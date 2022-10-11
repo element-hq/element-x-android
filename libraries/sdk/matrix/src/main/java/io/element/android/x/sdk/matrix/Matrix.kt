421e1f762eb98ac3e2519b311f4fef4287f9848c
Benoit Marty <benoit@matrix.org>
2022-10-11 17:16:27 +0200

    Store and restore session

--- libraries/sdk/matrix/src/main/java/io/element/android/x/sdk/matrix/Matrix.kt
+++ libraries/sdk/matrix/src/main/java/io/element/android/x/sdk/matrix/Matrix.kt
@@ -1,18 +1,35 @@
 package io.element.android.x.sdk.matrix
 
 import android.content.Context
+import io.element.android.x.sdk.matrix.store.SessionStore
 import org.matrix.rustcomponents.sdk.AuthenticationService
+import org.matrix.rustcomponents.sdk.ClientBuilder
 import java.io.File
 
 class Matrix(
     context: Context,
 ) {
     private val authFolder = File(context.filesDir, "auth")
+    private val sessionStore = SessionStore(context)
 
-    fun login(homeserver: String, username: String, password: String): MatrixClient {
+    suspend fun restoreSession(): MatrixClient? {
+        return sessionStore.getStoredData()
+            ?.let { sessionData ->
+                val client = ClientBuilder()
+                    .username(sessionData.userId)
+                    .build()
+                client.restoreLogin(sessionData.restoreToken)
+                client
+            }?.let {
+                MatrixClient(it)
+            }
+    }
+
+    suspend fun login(homeserver: String, username: String, password: String): MatrixClient {
         val authService = AuthenticationService(authFolder.absolutePath)
         authService.configureHomeserver(homeserver)
         val client = authService.login(username, password, "MatrixRustSDKSample", null)
+        sessionStore.storeData(SessionStore.SessionData(username, client.restoreToken()))
         return MatrixClient(client)
     }
 }
