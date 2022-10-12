d1c80438d5ca242bcc9e9b3bce0781bd3fbf06f6
Benoit Marty <benoit@matrix.org>
2022-10-12 18:10:44 +0200

    Add Logo

--- libraries/ui/screens/roomlist/src/main/java/io/element/android/x/ui/screen/roomlist/RoomListActivity.kt
+++ libraries/ui/screens/roomlist/src/main/java/io/element/android/x/ui/screen/roomlist/RoomListActivity.kt
@@ -1,21 +1,29 @@
 package io.element.android.x.ui.screen.roomlist
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.airbnb.mvrx.Success
 import com.airbnb.mvrx.compose.collectAsState
 import com.airbnb.mvrx.compose.mavericksViewModel
 import io.element.android.x.ui.theme.ElementXTheme
+import org.matrix.rustcomponents.sdk.Room
 
 class RoomListActivity : ComponentActivity() {
 
@@ -45,8 +53,16 @@ class RoomListActivity : ComponentActivity() {
                         OptionMenu(state.value.user, viewModel)
                         val rooms = state.value.rooms
                         if (rooms is Success) {
-                            rooms().forEach {
-                                Text(text = "Room: ${it.name() ?: it.id()}")
+                            LazyColumn {
+                                items(rooms()) { room ->
+                                    RoomCompose(room) {
+                                        Toast.makeText(
+                                            this@RoomListActivity,
+                                            "Room $it clicked!",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
                             }
                         }
                     }
@@ -55,6 +71,31 @@ class RoomListActivity : ComponentActivity() {
                     finish()
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun RoomCompose(room: Room, onClick: (String) -> Unit) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onClick.invoke(room.id()) },
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(model = room.avatarUrl()),
+                contentDescription = null,
+                modifier = Modifier.size(32.dp),
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(32.dp),
+            ) {
+                Text(text = "Room: ${room.name() ?: room.id()}")
+                Text(text = if (room.isDirect()) "Direct" else "Room")
+            }
+
         }
     }
 
@@ -69,8 +110,12 @@ class RoomListActivity : ComponentActivity() {
                     Image(
                         painter = rememberAsyncImagePainter(matrixUser.avatarUrl),
                         contentDescription = null,
-                        modifier = Modifier.size(48.dp)
+                        modifier = Modifier
+                            .size(48.dp)
+                            .clip(CircleShape)
+                            .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
                     )
+                    Spacer(modifier = Modifier.width(8.dp))
                     Text("${matrixUser.username}")
                 }
             },
