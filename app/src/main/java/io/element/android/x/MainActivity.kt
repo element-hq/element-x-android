6d1e549d958154781d574f13687a5396fe638bf6
ganfra <francoisg@element.io>
2022-11-02 20:03:31 +0100

    Fix theming dark/light

--- app/src/main/java/io/element/android/x/MainActivity.kt
+++ app/src/main/java/io/element/android/x/MainActivity.kt
@@ -6,10 +6,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import io.element.android.x.designsystem.ElementXTheme
@@ -23,7 +21,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ElementXTheme(darkTheme = false) {
+            ElementXTheme {
                 MainScreen(viewModel = viewModel)
             }
         }
