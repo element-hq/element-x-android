80065be58bcb6c019439f7e3cfd7249a1ac1c534
Benoit Marty <benoit@matrix.org>
2022-11-10 12:00:00 +0100

    Animate carousel

--- features/onboarding/src/main/java/io/element/android/x/features/onboarding/OnBoardingViewModel.kt
+++ features/onboarding/src/main/java/io/element/android/x/features/onboarding/OnBoardingViewModel.kt
@@ -5,15 +5,11 @@ import com.airbnb.mvrx.MavericksViewModel
 class OnBoardingViewModel(initialState: OnBoardingViewState) :
     MavericksViewModel<OnBoardingViewState>(initialState) {
 
-    fun handle(action: OnBoardingActions) {
-        when (action) {
-            is OnBoardingActions.GoToPage -> handleGoToPage(action)
-        }
-    }
-
-    private fun handleGoToPage(action: OnBoardingActions.GoToPage) {
+    fun onPageChanged(page: Int) {
         setState {
-            copy(currentPage = action.page)
+            copy(
+                currentPage = page,
+            )
         }
     }
 }
