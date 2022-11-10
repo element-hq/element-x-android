938a21c4bef3e84fc28849df1e37d9982b404c09
ganfra <francoisg@element.io>
2022-11-10 16:28:06 +0100

    Create coroutine application scope

--- app/src/main/java/io/element/android/x/ElementXApplication.kt
+++ app/src/main/java/io/element/android/x/ElementXApplication.kt
@@ -3,14 +3,19 @@ package io.element.android.x
 import android.app.Application
 import com.airbnb.mvrx.Mavericks
 import io.element.android.x.matrix.MatrixInstance
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.plus
 import timber.log.Timber
 
 class ElementXApplication : Application() {
 
+    private val applicationScope = MainScope() + CoroutineName("ElementX Scope")
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-        MatrixInstance.init(this)
+        MatrixInstance.init(this, applicationScope)
         Mavericks.initialize(this)
     }
 }
