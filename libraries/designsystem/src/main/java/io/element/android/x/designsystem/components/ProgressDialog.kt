ae17ff1b5860ec4d92cc5f8c78e8857e77111680
Benoit Marty <benoit@matrix.org>
2022-11-14 16:09:50 +0100

    Fix dialog theme

--- libraries/designsystem/src/main/java/io/element/android/x/designsystem/components/ProgressDialog.kt
+++ libraries/designsystem/src/main/java/io/element/android/x/designsystem/components/ProgressDialog.kt
@@ -1,7 +1,10 @@
 package io.element.android.x.designsystem.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -9,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
@@ -17,23 +21,36 @@ import androidx.compose.ui.window.DialogProperties
 fun ProgressDialog(text: String? = null, onDismiss: () -> Unit = {}) {
     Dialog(
         onDismissRequest = onDismiss,
-        DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)
+        properties = DialogProperties(dismissOnBackPress = false, dismissOnClickOutside = false)
     ) {
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
                 .fillMaxWidth()
                 .background(
-                    MaterialTheme.colorScheme.onBackground,
+                    color = MaterialTheme.colorScheme.onBackground,
                     shape = RoundedCornerShape(8.dp)
                 )
         ) {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                CircularProgressIndicator(modifier = Modifier.padding(16.dp), color = MaterialTheme.colorScheme.background)
+                CircularProgressIndicator(
+                    modifier = Modifier.padding(16.dp),
+                    color = MaterialTheme.colorScheme.background
+                )
                 if (!text.isNullOrBlank()) {
-                    Text(text = text, Modifier.padding(16.dp))
+                    Text(
+                        text = text,
+                        color = MaterialTheme.colorScheme.background,
+                        modifier = Modifier.padding(16.dp)
+                    )
                 }
             }
         }
     }
+}
+
+@Composable
+@Preview
+private fun ProgressDialogPreview() {
+    ProgressDialog("test dialog content")
 }
