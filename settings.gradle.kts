a43e66828da2f64cc485464189e838df309d218b
Benoit Marty <benoit@matrix.org>
2022-11-18 17:26:56 +0100

    Make the app compile in release
    Following instructions from https://stackoverflow.com/a/70074787/1472514

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@ dependencyResolutionManagement {
 rootProject.name = "ElementX"
 include(":app")
 include(":libraries:core")
+include(":libraries:rustSdk")
 include(":libraries:matrix")
 include(":libraries:textcomposer")
 include(":libraries:elementresources")
