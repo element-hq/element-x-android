aa77e7fe5aef490f22d76754288d7c1024deab3f
ganfra <francoisg@element.io>
2022-12-01 21:39:42 +0100

    Introduce ClickableLinkText

--- features/messages/src/main/java/io/element/android/x/features/messages/components/MessagesTimelineItemTextView.kt
+++ features/messages/src/main/java/io/element/android/x/features/messages/components/MessagesTimelineItemTextView.kt
@@ -1,10 +1,22 @@
 package io.element.android.x.features.messages.components
 
+import android.text.SpannableString
+import android.text.style.URLSpan
+import android.text.util.Linkify
+import android.text.util.Linkify.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.platform.UriHandler
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.core.text.util.LinkifyCompat
+import io.element.android.x.designsystem.components.ClickableLinkText
 import io.element.android.x.features.messages.components.html.HtmlDocument
 import io.element.android.x.features.messages.model.content.MessagesTimelineItemTextBasedContent
 
@@ -26,8 +38,44 @@ fun MessagesTimelineItemTextView(
             interactionSource = interactionSource
         )
     } else {
+        val uriHandler = LocalUriHandler.current
         Box(modifier) {
-            Text(text = content.body)
+            val linkStyle = SpanStyle(
+                color = Color.Blue,
+            )
+            val styledText = remember(content.body) { content.body.linkify(linkStyle) }
+            ClickableLinkText(
+                text = styledText,
+                linkAnnotationTag = "URL",
+                onClick = onTextClicked,
+                onLongClick = onTextLongClicked,
+                interactionSource = interactionSource
+            )
         }
+    }
+}
+
+private fun String.linkify(
+    linkStyle: SpanStyle,
+) = buildAnnotatedString {
+    append(this@linkify)
+    val spannable = SpannableString(this@linkify)
+    LinkifyCompat.addLinks(spannable, WEB_URLS or PHONE_NUMBERS)
+
+    val spans = spannable.getSpans(0, spannable.length, URLSpan::class.java)
+    for (span in spans) {
+        val start = spannable.getSpanStart(span)
+        val end = spannable.getSpanEnd(span)
+        addStyle(
+            start = start,
+            end = end,
+            style = linkStyle,
+        )
+        addStringAnnotation(
+            tag = "URL",
+            annotation = span.url,
+            start = start,
+            end = end
+        )
     }
 }
---
+++ libraries/designsystem/src/main/java/io/element/android/x/designsystem/components/ClickableLinkText.kt
@@ -0,0 +1,72 @@
+package io.element.android.x.designsystem.components
+
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun ClickableLinkText(
+    text: AnnotatedString,
+    linkAnnotationTag: String,
+    onClick: () -> Unit,
+    onLongClick: () -> Unit,
+    interactionSource: MutableInteractionSource,
+    modifier: Modifier = Modifier,
+    style: TextStyle = LocalTextStyle.current,
+    inlineContent: Map<String, InlineTextContent> = mapOf(),
+) {
+    val uriHandler = LocalUriHandler.current
+    val layoutResult = remember { mutableStateOf<TextLayoutResult?>(null) }
+    val pressIndicator = Modifier.pointerInput(onClick) {
+        detectTapGestures(
+            onPress = { offset: Offset ->
+                val pressInteraction = PressInteraction.Press(offset)
+                interactionSource.emit(pressInteraction)
+                val isReleased = tryAwaitRelease()
+                if (isReleased) {
+                    interactionSource.emit(PressInteraction.Release(pressInteraction))
+                } else {
+                    interactionSource.emit(PressInteraction.Cancel(pressInteraction))
+                }
+            },
+            onLongPress = {
+                onLongClick()
+            }
+        ) { offset ->
+            layoutResult.value?.let { layoutResult ->
+                val position = layoutResult.getOffsetForPosition(offset)
+                val linkAnnotations =
+                    text.getStringAnnotations(linkAnnotationTag, position, position)
+                if (linkAnnotations.isEmpty()) {
+                    onClick()
+                } else {
+                    uriHandler.openUri(linkAnnotations.first().item)
+                }
+            }
+
+        }
+    }
+    Text(
+        text = text,
+        modifier = modifier.then(pressIndicator),
+        style = style,
+        onTextLayout = {
+            layoutResult.value = it
+        },
+        inlineContent = inlineContent
+    )
+}
+
