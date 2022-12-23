01ae0ec4728ee63a3b1712c645dd490a7abb9fef
Benoit Marty <benoit@matrix.org>
2022-12-23 15:19:24 +0100

    Fix warning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+// TODO: Remove once https://youtrack.jetbrains.com/issue/KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("io.element.android-compose-application")
     alias(libs.plugins.kotlin.android)
