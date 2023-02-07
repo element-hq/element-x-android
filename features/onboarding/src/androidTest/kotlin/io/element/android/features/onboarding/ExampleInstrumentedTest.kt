11ec681945fa7da5b4c9144023194b9d2c694687
Martin Maartensson <mail@mama.sh>
2023-02-08 00:41:48 +0100

    Fixed naming of path

--- features/onboarding/src/androidTest/kotlin/io/element/android/features/onboarding/ExampleInstrumentedTest.kt
+++ features/onboarding/src/androidTest/kotlin/io/element/android/features/onboarding/ExampleInstrumentedTest.kt
@@ -14,19 +14,25 @@
  * limitations under the License.
  */
 
-package io.element.android.features.login
+package io.element.android.features.onboarding
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("io.element.android.features.login.test", appContext.packageName)
     }
 }
--- features/onboarding/src/test/kotlin/io/element/android/features/onboarding/ExampleUnitTest.kt
+++ features/onboarding/src/test/kotlin/io/element/android/features/onboarding/ExampleUnitTest.kt
@@ -14,25 +14,19 @@
  * limitations under the License.
  */
 
-package io.element.android.features.login
+package io.element.android.features.onboarding
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
 
 /**
- * Instrumented test, which will execute on an Android device.
+ * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class ExampleUnitTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("io.element.android.features.login.test", appContext.packageName)
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
     }
 }
