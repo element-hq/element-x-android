35eb0174b7115a06ab4b5ab601e52d8ef6b053f2
Benoit Marty <benoit@matrix.org>
2023-02-16 11:09:30 +0100

    Create `RoomListStateProvider`

--- features/roomlist/src/main/kotlin/io/element/android/features/roomlist/model/RoomListStateProvider.kt
+++ features/roomlist/src/main/kotlin/io/element/android/features/roomlist/model/RoomListStateProvider.kt
@@ -16,11 +16,45 @@
 
 package io.element.android.features.roomlist.model
 
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import io.element.android.libraries.designsystem.components.avatar.AvatarData
+import io.element.android.libraries.matrix.core.UserId
+import io.element.android.libraries.matrix.ui.model.MatrixUser
+import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
 
-fun aRoomListState() = RoomListState(
-    matrixUser = null,
-    roomList = persistentListOf(),
-    filter = "",
+open class RoomListStateProvider : PreviewParameterProvider<RoomListState> {
+    override val values: Sequence<RoomListState>
+        get() = sequenceOf(
+            aRoomListState(),
+        )
+}
+
+internal fun aRoomListState() = RoomListState(
+    matrixUser = MatrixUser(id = UserId("@id"), username = "User#1", avatarData = AvatarData("@id", "U")),
+    roomList = aRoomListRoomSummaryList(),
+    filter = "filter",
     eventSink = {}
 )
+
+internal fun aRoomListRoomSummaryList(): ImmutableList<RoomListRoomSummary> {
+    return persistentListOf(
+        RoomListRoomSummary(
+            name = "Room",
+            hasUnread = true,
+            timestamp = "14:18",
+            lastMessage = "A very very very very long message which suites on two lines",
+            avatarData = AvatarData("!id", "R"),
+            id = "roomId"
+        ),
+        RoomListRoomSummary(
+            name = "Room#2",
+            hasUnread = false,
+            timestamp = "14:16",
+            lastMessage = "A short message",
+            avatarData = AvatarData("!id", "Z"),
+            id = "roomId2"
+        ),
+        RoomListRoomSummaryPlaceholders.create("roomId2")
+    )
+}
