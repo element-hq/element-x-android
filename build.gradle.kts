3a73fc6214ae1661638d5a75263317ed19746bfe
Jorge Martín <jorgem@element.io>
2023-03-02 15:34:43 +0100

    Bump min coverage to 55, max to 60

--- build.gradle.kts
+++ build.gradle.kts
@@ -208,11 +208,11 @@ koverMerged {
             name = "Global minimum code coverage."
             target = kotlinx.kover.api.VerificationTarget.ALL
             bound {
-                minValue = 50
+                minValue = 55
                 // Setting a max value, so that if coverage is bigger, it means that we have to change minValue.
                 // For instance if we have minValue = 25 and maxValue = 30, and current code coverage is now 37.32%, update
                 // minValue to 35 and maxValue to 40.
-                maxValue = 55
+                maxValue = 60
                 counter = kotlinx.kover.api.CounterType.INSTRUCTION
                 valueType = kotlinx.kover.api.VerificationValueType.COVERED_PERCENTAGE
             }
