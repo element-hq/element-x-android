295def8d3b5cabe5bde31a1438353f07460ecded
ganfra <francoisg@element.io>
2023-03-09 17:54:35 +0100

    Add small comments

--- libraries/architecture/src/main/kotlin/io/element/android/libraries/architecture/FeatureEntryPoint.kt
+++ libraries/architecture/src/main/kotlin/io/element/android/libraries/architecture/FeatureEntryPoint.kt
@@ -19,8 +19,14 @@ package io.element.android.libraries.architecture
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 
+/**
+ * This interface represents an entrypoint to a feature. Should be used to return the entrypoint node of the feature without exposing the internal types.
+ */
 interface FeatureEntryPoint
 
+/**
+ * Can be used when the feature only exposes a simple node without the need of plugins.
+ */
 interface SimpleFeatureEntryPoint : FeatureEntryPoint {
     fun createNode(parentNode: Node, buildContext: BuildContext): Node
 }
