e838bcff3bc7eab96370fe1988564ded697ea353
Benoit Marty <benoit@matrix.org>
2023-03-13 14:37:25 +0100

    Setup Google services Gradle plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.cli.common.toBooleanLenient
 buildscript {
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("com.google.gms:google-services:4.3.15")
     }
 }
 
