1749f9e1cee0186edd7b1cf9d4ca51ea88997e70
Jorge Martín <jorgem@element.io>
2023-03-23 08:34:29 +0100

    Remove unneeded else clause

--- features/verifysession/impl/src/main/kotlin/io/element/android/features/verifysession/impl/VerifySelfSessionStateMachine.kt
+++ features/verifysession/impl/src/main/kotlin/io/element/android/features/verifysession/impl/VerifySelfSessionStateMachine.kt
@@ -103,7 +103,6 @@ class VerifySelfSessionStateMachine(
                 VerificationFlowState.Failed -> {
                     stateMachine.process(Event.DidFail)
                 }
-                else -> Unit
             }
         }.launchIn(coroutineScope)
     }
