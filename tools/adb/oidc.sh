4ecd7a241712952e9e6b96ed9761628cfef0597d
Benoit Marty <benoit@matrix.org>
2023-04-24 15:26:03 +0200

    Fix test script

--- tools/adb/oidc.sh
+++ tools/adb/oidc.sh
@@ -18,7 +18,7 @@
 # Format is:
 
 # Error
-# adb shell am start -a android.intent.action.VIEW -d io.element:/callback?error=access_denied&state=IFF1UETGye2ZA8pO
+# adb shell am start -a android.intent.action.VIEW -d "io.element:/callback?error=access_denied\\&state=IFF1UETGye2ZA8pO"
 
 # Success
-adb shell am start -a android.intent.action.VIEW -d io.element:/callback?state=IFF1UETGye2ZA8pO&code=y6X1GZeqA3xxOWcTeShgv8nkgFJXyzWB
+adb shell am start -a android.intent.action.VIEW -d "io.element:/callback?state=IFF1UETGye2ZA8pO\\&code=y6X1GZeqA3xxOWcTeShgv8nkgFJXyzWB"
