8c5e1c88ce9da2ec806bc8b25b795ee0850752d0
ganfra <francoisg@element.io>
2023-05-16 14:50:54 +0200

    Media: update doc on MediaLoader

--- libraries/matrix/api/src/main/kotlin/io/element/android/libraries/matrix/api/media/MatrixMediaLoader.kt
+++ libraries/matrix/api/src/main/kotlin/io/element/android/libraries/matrix/api/media/MatrixMediaLoader.kt
@@ -18,13 +18,13 @@ package io.element.android.libraries.matrix.api.media
 
 interface MatrixMediaLoader {
     /**
-     * @param url to fetch the content for.
+     * @param source to fetch the content for.
      * @return a [Result] of ByteArray. It contains the binary data for the media.
      */
     suspend fun loadMediaContent(source: MediaSource): Result<ByteArray>
 
     /**
-     * @param url to fetch the data for.
+     * @param source to fetch the data for.
      * @param width: the desired width for rescaling the media as thumbnail
      * @param height: the desired height for rescaling the media as thumbnail
      * @return a [Result] of ByteArray. It contains the binary data for the media.
@@ -32,7 +32,7 @@ interface MatrixMediaLoader {
     suspend fun loadMediaThumbnail(source: MediaSource, width: Long, height: Long): Result<ByteArray>
 
     /**
-     * @param url to fetch the data for.
+     * @param source to fetch the data for.
      * @param mimeType: optional mime type
      * @return a [Result] of [MediaFile]
      */
