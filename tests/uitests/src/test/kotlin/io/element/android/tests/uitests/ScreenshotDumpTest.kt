a6abf97fb1e4f82847734d245b6d2945c352fb2a
Jorge Martín <jorgem@element.io>
2023-05-30 08:37:28 +0200

    Add dump test

--- tests/uitests/src/test/kotlin/io/element/android/tests/uitests/ScreenshotDumpTest.kt
+++ tests/uitests/src/test/kotlin/io/element/android/tests/uitests/ScreenshotDumpTest.kt
@@ -1,12 +1,11 @@
 /*
- * Copyright 2022 The Android Open Source Project
- * Copyright (c) 2022 New Vector Ltd
+ * Copyright (c) 2023 New Vector Ltd
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,22 +23,37 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.semantics.SemanticsNode
 import androidx.compose.ui.test.isRoot
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onLast
-import androidx.compose.ui.test.onRoot
-import androidx.compose.ui.test.printToLog
-import androidx.compose.ui.test.printToString
 import androidx.compose.ui.unit.dp
 import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.airbnb.android.showkase.models.Showkase
 import com.github.takahirom.roborazzi.DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH
 import com.github.takahirom.roborazzi.RobolectricDeviceQualifiers
+import com.github.takahirom.roborazzi.RoborazziOptions
 import com.github.takahirom.roborazzi.captureRoboImage
 import com.google.testing.junit.testparameterinjector.TestParameter
+import io.element.android.features.leaveroom.api.LeaveRoomState
+import io.element.android.features.roomdetails.impl.members.details.RoomMemberDetailsState
+import io.element.android.features.roomdetails.impl.members.details.RoomMemberDetailsView
+import io.element.android.features.roomdetails.impl.members.details.aRoomMemberDetailsState
+import io.element.android.features.roomlist.impl.InvitesState
+import io.element.android.features.roomlist.impl.RoomListState
+import io.element.android.features.roomlist.impl.RoomListView
+import io.element.android.features.roomlist.impl.model.RoomListRoomSummary
+import io.element.android.features.roomlist.impl.model.RoomListRoomSummaryPlaceholders
+import io.element.android.libraries.designsystem.components.avatar.AvatarData
 import io.element.android.libraries.designsystem.theme.ElementTheme
+import io.element.android.libraries.matrix.api.core.RoomId
+import io.element.android.libraries.matrix.api.core.UserId
+import io.element.android.libraries.matrix.api.user.MatrixUser
 import io.element.android.tests.uitests.test.TestActivity
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -47,7 +61,6 @@ import org.robolectric.ParameterizedRobolectricTestRunner
 import org.robolectric.annotation.Config
 import org.robolectric.annotation.GraphicsMode
 import java.io.File
-import java.util.Locale
 
 /**
  * BMA: Inspired from https://github.com/airbnb/Showkase/blob/master/showkase-screenshot-testing-paparazzi-sample/src/test/java/com/airbnb/android/showkase/screenshot/testing/paparazzi/sample/PaparazziSampleScreenshotTest.kt
@@ -60,28 +73,9 @@ import java.util.Locale
  */
 //@RunWith(TestParameterInjector::class)
 @Config(sdk = [30], qualifiers = RobolectricDeviceQualifiers.Pixel5)
-@RunWith(ParameterizedRobolectricTestRunner::class)
+@RunWith(AndroidJUnit4::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
-class ScreenshotTest(
-    private val componentTestPreview: TestPreview,
-) {
-
-    companion object {
-        @JvmStatic
-        @ParameterizedRobolectricTestRunner.Parameters(name = "Input: {0}")
-        fun params() = PreviewProvider.provideValues()
-    }
-
-    object PreviewProvider : TestParameter.TestParameterValuesProvider {
-        override fun provideValues(): List<TestPreview> {
-            val metadata = Showkase.getMetadata()
-            val components = metadata.componentList.map(::ComponentTestPreview)
-            val colors = metadata.colorList.map(::ColorTestPreview)
-            val typography = metadata.typographyList.map(::TypographyTestPreview)
-
-            return components + colors + typography
-        }
-    }
+class ScreenshotDumpTest {
 
     @get:Rule
     val composeTestRule = createAndroidComposeRule<TestActivity>()
@@ -99,24 +93,20 @@ class ScreenshotTest(
                             .background(MaterialTheme.colorScheme.background)
                             .sizeIn(minWidth = 1.dp, minHeight = 1.dp, maxHeight = 1000.dp)
                     ) {
-                        componentTestPreview.Content()
+                        val state = aRoomMemberDetailsState().copy(displayConfirmationDialog = RoomMemberDetailsState.ConfirmationDialog.Block)
+                        RoomMemberDetailsView(state = state, onShareUser = { /*TODO*/ }, goBack = { /*TODO*/ })
                     }
                 }
             }
         }
         val onAllRootNodes = composeTestRule.onAllNodes(isRoot())
         val allNodes = onAllRootNodes.fetchSemanticsNodes()
-        allNodes.forEachIndexed { index, node -> println("Node #$index: ${node.boundsInWindow}") }
-        onAllRootNodes.onLast().captureRoboImage(File(DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH, componentTestPreview.toString() + ".png"))
-        scenario.close()
-    }
-}
 
-private fun String.toLocale(): Locale {
-    return when (this) {
-        "en" -> Locale.ENGLISH
-        "fr" -> Locale.FRANCE
-        "de" -> Locale.GERMAN
-        else -> Locale.Builder().setLanguage(this).build()
+        allNodes.forEachIndexed { index, node -> println("Node #$index: ${node.boundsInWindow}") }
+        onAllRootNodes.onFirst().captureRoboImage(
+            File(DEFAULT_ROBORAZZI_OUTPUT_DIR_PATH, "roborazzi_dump.png"),
+            RoborazziOptions(captureType = RoborazziOptions.CaptureType.Dump())
+        )
+        scenario.close()
     }
 }
