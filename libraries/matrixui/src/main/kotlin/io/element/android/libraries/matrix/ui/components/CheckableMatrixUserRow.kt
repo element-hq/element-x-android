8cb57c224bdb4af3f39eaf8cbdcfd6c6c081732a
Chris Smith <csmith@lunarian.uk>
2023-06-01 10:15:09 +0100

    Checkable user row: fix uneven padding

--- libraries/matrixui/src/main/kotlin/io/element/android/libraries/matrix/ui/components/CheckableMatrixUserRow.kt
+++ libraries/matrixui/src/main/kotlin/io/element/android/libraries/matrix/ui/components/CheckableMatrixUserRow.kt
@@ -20,12 +20,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
 import io.element.android.libraries.designsystem.components.avatar.AvatarData
 import io.element.android.libraries.designsystem.components.avatar.AvatarSize
 import io.element.android.libraries.designsystem.preview.ElementPreviewDark
@@ -79,6 +81,8 @@ fun CheckableUserRow(
         )
 
         Checkbox(
+            modifier = Modifier
+                .padding(end = 2.dp),
             checked = checked,
             onCheckedChange = onCheckedChange,
             enabled = enabled,
