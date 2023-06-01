1b48aef326fd0ceeb764776b9fd659a6c7734ccd
Jorge Martín <jorgem@element.io>
2023-06-01 09:31:30 +0200

    Remove testparameterinjector and other possible points of leaks

--- tests/uitests/src/test/kotlin/io/element/android/tests/uitests/ScreenshotTest.kt
+++ tests/uitests/src/test/kotlin/io/element/android/tests/uitests/ScreenshotTest.kt
@@ -39,13 +39,14 @@ import androidx.lifecycle.Lifecycle
 import app.cash.paparazzi.Paparazzi
 import com.airbnb.android.showkase.models.Showkase
 import com.android.ide.common.rendering.api.SessionParams
-import com.google.testing.junit.testparameterinjector.TestParameter
-import com.google.testing.junit.testparameterinjector.TestParameterInjector
 import io.element.android.libraries.designsystem.modifiers.applyIf
 import io.element.android.libraries.designsystem.theme.ElementTheme
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
 import java.util.Locale
 
 /**
@@ -57,11 +58,22 @@ import java.util.Locale
  * PR here - https://github.com/android/nowinandroid/pull/101. Modified the test from that PR to
  * my own needs for this sample.
  */
-@RunWith(TestParameterInjector::class)
-class ScreenshotTest {
+@RunWith(Parameterized::class)
+class ScreenshotTest(
+    private val componentTestPreview: TestPreview,
+) {
 
-    object ScreensPreviewProvider : TestParameter.TestParameterValuesProvider {
-        override fun provideValues(): List<TestPreview> {
+    companion object {
+
+        val previews = screensPreviews() + designPreviews()
+
+        @JvmStatic
+        @Parameters(name = "preview")
+        fun previews(): List<TestPreview> {
+            return previews
+        }
+
+        fun screensPreviews(): List<TestPreview> {
             val metadata = Showkase.getMetadata()
             val components = metadata.componentList.filterNot {
                 it.componentKey.startsWith("io.element.android.libraries.designsystem")
@@ -71,12 +83,10 @@ class ScreenshotTest {
 
             return components + colors + typography
         }
-    }
 
-    object DesignSystemPreviewProvider : TestParameter.TestParameterValuesProvider {
-        override fun provideValues(): List<TestPreview> {
+        fun designPreviews(): List<TestPreview> {
             val metadata = Showkase.getMetadata()
-            val components = metadata.componentList.filter {
+            val components = metadata.componentList.filterNot {
                 it.componentKey.startsWith("io.element.android.libraries.designsystem")
             }.map(::ComponentTestPreview)
             val colors = metadata.colorList.map(::ColorTestPreview)
@@ -84,47 +94,67 @@ class ScreenshotTest {
 
             return components + colors + typography
         }
+//
+//        @JvmStatic
+//        @Parameters(name = "ui_mode")
+//        fun mode(): List<Boolean> = listOf(false, true)
+    }
+
+    @Before
+    fun setup() {
+        System.gc()
+        val r = Runtime.getRuntime()
+        println("maxMemory  : " + r.maxMemory())
+        println("totalMemory: " + r.totalMemory())
     }
 
     @get:Rule
     val paparazzi = Paparazzi(
+        theme = "Theme.AppCompat.Light.NoActionBar",
         maxPercentDifference = 0.01,
         renderingMode = SessionParams.RenderingMode.SHRINK,
     )
 
     @Test
-    fun preview_tests_screens(
-        @TestParameter(valuesProvider = ScreensPreviewProvider::class) componentTestPreview: TestPreview,
-        @TestParameter baseDeviceConfig: BaseDeviceConfig,
-        @TestParameter(value = ["1.0"/*, "1.5"*/]) fontScale: Float,
-        @TestParameter(value = ["en" /*"fr", "de", "ru"*/]) localeStr: String,
-        @TestParameter(value = ["false", "true"]) darkMode: Boolean,
+    fun preview_tests_screens_light(
     ) {
         paparazzi.screenshotTest(
             componentTestPreview = componentTestPreview,
-            baseDeviceConfig = baseDeviceConfig,
-            fontScale = fontScale,
-            localeStr = localeStr,
-            darkMode = darkMode
+            baseDeviceConfig = BaseDeviceConfig.NEXUS_5,
+            fontScale = 1.0f,
+            localeStr = "en",
+            darkMode = false,
         )
     }
 
     @Test
-    fun preview_tests_design_system(
-        @TestParameter(valuesProvider = DesignSystemPreviewProvider::class) componentTestPreview: TestPreview,
-        @TestParameter baseDeviceConfig: BaseDeviceConfig,
-        @TestParameter(value = ["1.0"/*, "1.5"*/]) fontScale: Float,
-        @TestParameter(value = ["en" /*"fr", "de", "ru"*/]) localeStr: String,
-        @TestParameter(value = ["false"]) darkMode: Boolean,
+    fun preview_tests_screens_dark(
     ) {
         paparazzi.screenshotTest(
             componentTestPreview = componentTestPreview,
-            baseDeviceConfig = baseDeviceConfig,
-            fontScale = fontScale,
-            localeStr = localeStr,
-            darkMode = darkMode
+            baseDeviceConfig = BaseDeviceConfig.NEXUS_5,
+            fontScale = 1.0f,
+            localeStr = "en",
+            darkMode = true,
         )
     }
+
+//    @Test
+//    fun preview_tests_design_system(
+//        @TestParameter(valuesProvider = DesignSystemPreviewProvider::class) componentTestPreview: TestPreview,
+//        @TestParameter baseDeviceConfig: BaseDeviceConfig,
+//        @TestParameter(value = ["1.0"/*, "1.5"*/]) fontScale: Float,
+//        @TestParameter(value = ["en" /*"fr", "de", "ru"*/]) localeStr: String,
+//        @TestParameter(value = ["false"]) darkMode: Boolean,
+//    ) {
+//        paparazzi.screenshotTest(
+//            componentTestPreview = componentTestPreview,
+//            baseDeviceConfig = baseDeviceConfig,
+//            fontScale = fontScale,
+//            localeStr = localeStr,
+//            darkMode = darkMode
+//        )
+//    }
 }
 
 private fun Paparazzi.screenshotTest(
@@ -136,44 +166,26 @@ private fun Paparazzi.screenshotTest(
 ) {
     val needsScrolling = componentTestPreview.needsScroll
     val screenHeight = componentTestPreview.customHeightDp.takeIf { it != null }
-    unsafeUpdateConfig(
-        deviceConfig = baseDeviceConfig.deviceConfig.copy(
-            softButtons = false,
-            screenHeight = screenHeight ?: baseDeviceConfig.deviceConfig.screenHeight
-        ),
-        renderingMode = if (needsScrolling) SessionParams.RenderingMode.V_SCROLL else SessionParams.RenderingMode.SHRINK
-    )
+//    unsafeUpdateConfig(
+//        deviceConfig = baseDeviceConfig.deviceConfig.copy(
+//            softButtons = false,
+//            screenHeight = screenHeight ?: baseDeviceConfig.deviceConfig.screenHeight
+//        ),
+//        renderingMode = if (needsScrolling) SessionParams.RenderingMode.V_SCROLL else SessionParams.RenderingMode.SHRINK
+//    )
     snapshot {
-        val lifecycleOwner = LocalLifecycleOwner.current
-        CompositionLocalProvider(
-            LocalInspectionMode provides true,
-            LocalDensity provides Density(
-                density = LocalDensity.current.density,
-                fontScale = fontScale
-            ),
-            LocalConfiguration provides Configuration().apply {
-                setLocales(LocaleList(localeStr.toLocale()))
-                if (darkMode) uiMode = Configuration.UI_MODE_NIGHT_YES
-            },
-            // Needed so that UI that uses it don't crash during screenshot tests
-            LocalOnBackPressedDispatcherOwner provides object : OnBackPressedDispatcherOwner {
-                override val lifecycle: Lifecycle get() = lifecycleOwner.lifecycle
-                override val onBackPressedDispatcher: OnBackPressedDispatcher get() = OnBackPressedDispatcher()
-            }
-        ) {
-            ElementTheme {
+        ElementTheme {
                 Box(
                     modifier = Modifier
-                        .background(MaterialTheme.colorScheme.background)
+//                        .background(MaterialTheme.colorScheme.background)
                         .sizeIn(minWidth = 1.dp, minHeight = 1.dp)
-                        .applyIf(needsScrolling, ifTrue = {
-                            heightIn(max = 1000.dp)
-                        })
+//                        .applyIf(needsScrolling, ifTrue = {
+//                            heightIn(max = 1000.dp)
+//                        })
                 ) {
                     componentTestPreview.Content()
                 }
             }
-        }
     }
 }
 
