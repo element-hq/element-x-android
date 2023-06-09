0b18fceb39f53dcc01e98b1a334be00fbca075f3
Benoit Marty <benoit@matrix.org>
2023-06-09 17:58:36 +0200

    Use LaunchedEffect to avoid multiple calls.

--- features/login/impl/src/main/kotlin/io/element/android/features/login/impl/changeserver/ChangeServerView.kt
+++ features/login/impl/src/main/kotlin/io/element/android/features/login/impl/changeserver/ChangeServerView.kt
@@ -17,6 +17,7 @@
 package io.element.android.features.login.impl.changeserver
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -61,7 +62,9 @@ fun ChangeServerView(
             }
         }
         is Async.Loading -> ProgressDialog()
-        is Async.Success -> onDone()
+        is Async.Success -> LaunchedEffect(state.changeServerAction) {
+            onDone()
+        }
         Async.Uninitialized -> Unit
     }
 }
