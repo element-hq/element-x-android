223b19a40ebe0a4007bd9161204ac264d9bd0c1e
Jorge Martin Espinosa <jorgem@element.io>
2023-06-12 10:42:35 +0200

    Fix timeline item padding and corners (#573)
    * Try to fix timeline offsets and rounded corners.
    ---------
    
    Co-authored-by: ElementBot <benoitm+elementbot@element.io>

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/TimelineView.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/TimelineView.kt
@@ -20,6 +20,7 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -86,7 +87,8 @@ fun TimelineView(
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
             state = lazyListState,
-            reverseLayout = true
+            reverseLayout = true,
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
         ) {
             itemsIndexed(
                 items = state.timelineItems,
