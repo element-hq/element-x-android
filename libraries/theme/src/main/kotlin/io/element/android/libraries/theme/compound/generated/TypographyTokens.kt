f964571f43f639310372e3bfe4376b7b2cd13dda
Jorge Martín <jorgem@element.io>
2023-06-27 15:32:46 +0200

    Remove tab characters from `TypographyTokens`

--- libraries/theme/src/main/kotlin/io/element/android/libraries/theme/compound/generated/TypographyTokens.kt
+++ libraries/theme/src/main/kotlin/io/element/android/libraries/theme/compound/generated/TypographyTokens.kt
@@ -15,7 +15,7 @@
  */
 
 // Do not edit directly
-// Generated on Tue, 27 Jun 2023 11:49:05 GMT
+// Generated on Tue, 27 Jun 2023 13:31:52 GMT
 
 
 
@@ -29,116 +29,116 @@ import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
 object TypographyTokens {
-  val fontBodyLgMedium = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W500,
-		lineHeight = 22.sp,
-		fontSize = 16.sp,
-		letterSpacing = 0.015629999999999998.em,
-	)
-  val fontBodyLgRegular = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W400,
-		lineHeight = 22.sp,
-		fontSize = 16.sp,
-		letterSpacing = 0.015629999999999998.em,
-	)
-  val fontBodyMdMedium = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W500,
-		lineHeight = 20.sp,
-		fontSize = 14.sp,
-		letterSpacing = 0.01786.em,
-	)
-  val fontBodyMdRegular = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W400,
-		lineHeight = 20.sp,
-		fontSize = 14.sp,
-		letterSpacing = 0.01786.em,
-	)
-  val fontBodySmMedium = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W500,
-		lineHeight = 17.sp,
-		fontSize = 12.sp,
-		letterSpacing = 0.03333.em,
-	)
-  val fontBodySmRegular = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W400,
-		lineHeight = 17.sp,
-		fontSize = 12.sp,
-		letterSpacing = 0.03333.em,
-	)
-  val fontBodyXsMedium = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W500,
-		lineHeight = 15.sp,
-		fontSize = 11.sp,
-		letterSpacing = 0.04545.em,
-	)
-  val fontBodyXsRegular = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W400,
-		lineHeight = 15.sp,
-		fontSize = 11.sp,
-		letterSpacing = 0.04545.em,
-	)
-  val fontHeadingLgBold = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W700,
-		lineHeight = 34.sp,
-		fontSize = 28.sp,
-		letterSpacing = 0.em,
-	)
-  val fontHeadingLgRegular = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W400,
-		lineHeight = 34.sp,
-		fontSize = 28.sp,
-		letterSpacing = 0.em,
-	)
-  val fontHeadingMdBold = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W700,
-		lineHeight = 27.sp,
-		fontSize = 22.sp,
-		letterSpacing = 0.em,
-	)
-  val fontHeadingMdRegular = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W400,
-		lineHeight = 27.sp,
-		fontSize = 22.sp,
-		letterSpacing = 0.em,
-	)
-  val fontHeadingSmMedium = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W500,
-		lineHeight = 25.sp,
-		fontSize = 20.sp,
-		letterSpacing = 0.em,
-	)
-  val fontHeadingSmRegular = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W400,
-		lineHeight = 25.sp,
-		fontSize = 20.sp,
-		letterSpacing = 0.em,
-	)
-  val fontHeadingXlBold = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W700,
-		lineHeight = 41.sp,
-		fontSize = 34.sp,
-		letterSpacing = 0.em,
-	)
-  val fontHeadingXlRegular = TextStyle(
-		fontFamily = FontFamily.Default,
-		fontWeight = FontWeight.W400,
-		lineHeight = 41.sp,
-		fontSize = 34.sp,
-		letterSpacing = 0.em,
-	)
+    val fontBodyLgMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        lineHeight = 22.sp,
+        fontSize = 16.sp,
+        letterSpacing = 0.015629999999999998.em,
+    )
+    val fontBodyLgRegular = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W400,
+        lineHeight = 22.sp,
+        fontSize = 16.sp,
+        letterSpacing = 0.015629999999999998.em,
+    )
+    val fontBodyMdMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        lineHeight = 20.sp,
+        fontSize = 14.sp,
+        letterSpacing = 0.01786.em,
+    )
+    val fontBodyMdRegular = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W400,
+        lineHeight = 20.sp,
+        fontSize = 14.sp,
+        letterSpacing = 0.01786.em,
+    )
+    val fontBodySmMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        lineHeight = 17.sp,
+        fontSize = 12.sp,
+        letterSpacing = 0.03333.em,
+    )
+    val fontBodySmRegular = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W400,
+        lineHeight = 17.sp,
+        fontSize = 12.sp,
+        letterSpacing = 0.03333.em,
+    )
+    val fontBodyXsMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        lineHeight = 15.sp,
+        fontSize = 11.sp,
+        letterSpacing = 0.04545.em,
+    )
+    val fontBodyXsRegular = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W400,
+        lineHeight = 15.sp,
+        fontSize = 11.sp,
+        letterSpacing = 0.04545.em,
+    )
+    val fontHeadingLgBold = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W700,
+        lineHeight = 34.sp,
+        fontSize = 28.sp,
+        letterSpacing = 0.em,
+    )
+    val fontHeadingLgRegular = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W400,
+        lineHeight = 34.sp,
+        fontSize = 28.sp,
+        letterSpacing = 0.em,
+    )
+    val fontHeadingMdBold = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W700,
+        lineHeight = 27.sp,
+        fontSize = 22.sp,
+        letterSpacing = 0.em,
+    )
+    val fontHeadingMdRegular = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W400,
+        lineHeight = 27.sp,
+        fontSize = 22.sp,
+        letterSpacing = 0.em,
+    )
+    val fontHeadingSmMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        lineHeight = 25.sp,
+        fontSize = 20.sp,
+        letterSpacing = 0.em,
+    )
+    val fontHeadingSmRegular = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W400,
+        lineHeight = 25.sp,
+        fontSize = 20.sp,
+        letterSpacing = 0.em,
+    )
+    val fontHeadingXlBold = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W700,
+        lineHeight = 41.sp,
+        fontSize = 34.sp,
+        letterSpacing = 0.em,
+    )
+    val fontHeadingXlRegular = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W400,
+        lineHeight = 41.sp,
+        fontSize = 34.sp,
+        letterSpacing = 0.em,
+    )
 }
