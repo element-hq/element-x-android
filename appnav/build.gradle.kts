aab82ea054663598e4db22b6d11d71886810f427
yostyle <y.pintas@gmail.com>
2023-06-27 23:28:22 +0200

    Fix deps

--- appnav/build.gradle.kts
+++ appnav/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     testImplementation(libs.test.truth)
     testImplementation(libs.test.turbine)
     testImplementation(projects.libraries.matrix.test)
-    testImplementation(projects.features.analytics.test)
     testImplementation(projects.features.rageshake.test)
     testImplementation(projects.features.rageshake.impl)
     testImplementation(projects.services.appnavstate.test)
