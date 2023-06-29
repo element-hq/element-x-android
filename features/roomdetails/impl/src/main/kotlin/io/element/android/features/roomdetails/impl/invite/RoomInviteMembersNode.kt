7451d63ad18b170991340e199bacc73da5c3f1c9
Yoan Pintas <yoan@pintas.bzh>
2023-06-29 15:39:34 +0200

    Add analytics tracking for some screens (#698)

--- features/roomdetails/impl/src/main/kotlin/io/element/android/features/roomdetails/impl/invite/RoomInviteMembersNode.kt
+++ features/roomdetails/impl/src/main/kotlin/io/element/android/features/roomdetails/impl/invite/RoomInviteMembersNode.kt
@@ -19,16 +19,19 @@ package io.element.android.features.roomdetails.impl.invite
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import com.bumble.appyx.core.lifecycle.subscribe
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 import com.bumble.appyx.core.plugin.Plugin
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import im.vector.app.features.analytics.plan.MobileScreen
 import io.element.android.anvilannotations.ContributesNode
 import io.element.android.libraries.core.coroutine.CoroutineDispatchers
 import io.element.android.libraries.di.RoomScope
 import io.element.android.libraries.matrix.api.room.MatrixRoom
 import io.element.android.libraries.ui.strings.CommonStrings
+import io.element.android.services.analytics.api.AnalyticsService
 import io.element.android.services.apperror.api.AppErrorStateService
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
@@ -42,9 +45,18 @@ class RoomInviteMembersNode @AssistedInject constructor(
     private val room: MatrixRoom,
     private val presenter: RoomInviteMembersPresenter,
     private val appErrorStateService: AppErrorStateService,
+    private val analyticsService: AnalyticsService,
 ) : Node(buildContext, plugins = plugins) {
 
     private val coroutineScope = CoroutineScope(SupervisorJob() + coroutineDispatchers.io)
+
+    init {
+        lifecycle.subscribe(
+            onResume = {
+                analyticsService.screen(MobileScreen(screenName = MobileScreen.ScreenName.Invites))
+            }
+        )
+    }
 
     @Composable
     override fun View(modifier: Modifier) {
