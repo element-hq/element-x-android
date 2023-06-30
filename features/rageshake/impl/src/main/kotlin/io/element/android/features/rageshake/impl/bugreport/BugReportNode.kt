028faf2537a1dfc1d51a92e578ce7672925c6b1d
Benoit Marty <benoit@matrix.org>
2023-06-30 18:46:40 +0200

    Show Toast when bug report is submitted with success.

--- features/rageshake/impl/src/main/kotlin/io/element/android/features/rageshake/impl/bugreport/BugReportNode.kt
+++ features/rageshake/impl/src/main/kotlin/io/element/android/features/rageshake/impl/bugreport/BugReportNode.kt
@@ -16,8 +16,10 @@
 
 package io.element.android.features.rageshake.impl.bugreport
 
+import android.app.Activity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 import com.bumble.appyx.core.plugin.Plugin
@@ -26,7 +28,9 @@ import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import io.element.android.anvilannotations.ContributesNode
 import io.element.android.features.rageshake.api.bugreport.BugReportEntryPoint
+import io.element.android.libraries.androidutils.system.toast
 import io.element.android.libraries.di.AppScope
+import io.element.android.libraries.ui.strings.CommonStrings
 
 @ContributesNode(AppScope::class)
 class BugReportNode @AssistedInject constructor(
@@ -38,11 +42,15 @@ class BugReportNode @AssistedInject constructor(
     @Composable
     override fun View(modifier: Modifier) {
         val state = presenter.present()
+        val activity = LocalContext.current as? Activity
         BugReportView(
             state = state,
             modifier = modifier,
             onBackPressed = { navigateUp() },
-            onDone = this::onDone,
+            onDone = {
+                activity?.toast(CommonStrings.common_report_submitted)
+                onDone()
+            },
         )
     }
 
