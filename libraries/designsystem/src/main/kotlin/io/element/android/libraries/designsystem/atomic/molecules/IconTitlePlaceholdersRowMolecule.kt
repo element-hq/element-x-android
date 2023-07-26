9211a33ad8a4e97e5aa3fb045e5b7345ac091eaf
Benoit Marty <benoit@matrix.org>
2023-07-26 13:58:04 +0200

    Add missing preview.

--- libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/atomic/molecules/IconTitlePlaceholdersRowMolecule.kt
+++ libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/atomic/molecules/IconTitlePlaceholdersRowMolecule.kt
@@ -30,6 +30,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import io.element.android.libraries.designsystem.atomic.atoms.PlaceholderAtom
+import io.element.android.libraries.designsystem.components.avatar.AvatarSize
+import io.element.android.libraries.designsystem.preview.DayNightPreviews
+import io.element.android.libraries.designsystem.preview.ElementPreview
 import io.element.android.libraries.designsystem.theme.placeholderBackground
 import io.element.android.libraries.theme.ElementTheme
 
@@ -56,4 +59,12 @@ fun IconTitlePlaceholdersRowMolecule(
         Spacer(modifier = Modifier.width(7.dp))
         PlaceholderAtom(width = 45.dp, height = 7.dp)
     }
+}
+
+@DayNightPreviews
+@Composable
+internal fun IconTitlePlaceholdersRowMoleculePreview() = ElementPreview {
+    IconTitlePlaceholdersRowMolecule(
+        iconSize = AvatarSize.TimelineRoom.dp,
+    )
 }
