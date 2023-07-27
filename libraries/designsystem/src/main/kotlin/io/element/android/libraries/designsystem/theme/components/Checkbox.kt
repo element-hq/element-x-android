dd2432b37bb80768e5a77a27ee2c417f9ee696e7
Jorge Martín <jorgem@element.io>
2023-07-27 13:36:37 +0200

    Add `indeterminate` and `hasError` parameters to `CheckBox`.
    Improve previews.

--- libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/theme/components/Checkbox.kt
+++ libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/theme/components/Checkbox.kt
@@ -17,13 +17,20 @@
 package io.element.android.libraries.designsystem.theme.components
 
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.CheckboxColors
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.state.ToggleableState
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.element.android.libraries.designsystem.preview.ElementThemedPreview
 import io.element.android.libraries.designsystem.preview.PreviewGroup
 import io.element.android.libraries.theme.ElementTheme
@@ -36,12 +43,22 @@ fun Checkbox(
     onCheckedChange: ((Boolean) -> Unit)?,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    colors: CheckboxColors = compoundCheckBoxColors(),
+    hasError: Boolean = false,
+    indeterminate: Boolean = false,
+    colors: CheckboxColors = if (hasError) compoundErrorCheckBoxColors() else compoundCheckBoxColors(),
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
 ) {
-    androidx.compose.material3.Checkbox(
-        checked = checked,
-        onCheckedChange = onCheckedChange,
+    var indeterminate by remember { mutableStateOf(indeterminate) }
+    androidx.compose.material3.TriStateCheckbox(
+        state = if (!checked && indeterminate) ToggleableState.Indeterminate else ToggleableState(checked),
+        onClick = if (onCheckedChange != null) {
+            {
+                indeterminate = false
+                onCheckedChange(!checked)
+            }
+        } else {
+            null
+        },
         modifier = modifier,
         enabled = enabled,
         colors = colors,
@@ -50,9 +67,23 @@ fun Checkbox(
 }
 
 @Composable
-internal fun compoundCheckBoxColors(): CheckboxColors {
+private fun compoundCheckBoxColors(): CheckboxColors {
     return CheckboxDefaults.colors(
+        checkedColor = ElementTheme.materialColors.primary,
         uncheckedColor = ElementTheme.colors.borderInteractivePrimary,
+        checkmarkColor = ElementTheme.materialColors.onPrimary,
+        disabledUncheckedColor = ElementTheme.colors.borderDisabled,
+        disabledCheckedColor = ElementTheme.colors.iconDisabled,
+        disabledIndeterminateColor = ElementTheme.colors.iconDisabled,
+    )
+}
+
+@Composable
+private fun compoundErrorCheckBoxColors(): CheckboxColors {
+    return CheckboxDefaults.colors(
+        checkedColor = ElementTheme.materialColors.error,
+        uncheckedColor = ElementTheme.materialColors.error,
+        checkmarkColor = ElementTheme.materialColors.onPrimary,
         disabledUncheckedColor = ElementTheme.colors.borderDisabled,
         disabledCheckedColor = ElementTheme.colors.iconDisabled,
         disabledIndeterminateColor = ElementTheme.colors.iconDisabled,
@@ -66,9 +97,33 @@ internal fun CheckboxesPreview() = ElementThemedPreview(vertical = false) { Cont
 @Composable
 private fun ContentToPreview() {
     Column {
-        Checkbox(onCheckedChange = {}, enabled = true, checked = true)
-        Checkbox(onCheckedChange = {}, enabled = true, checked = false)
-        Checkbox(onCheckedChange = {}, enabled = false, checked = true)
-        Checkbox(onCheckedChange = {}, enabled = false, checked = false)
+        // Unchecked
+        Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+            Checkbox(onCheckedChange = {}, enabled = true, checked = false)
+            Checkbox(onCheckedChange = {}, enabled = false, checked = false)
+        }
+        // Checked
+        Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+            Checkbox(onCheckedChange = {}, enabled = true, checked = true)
+            Checkbox(onCheckedChange = {}, enabled = false, checked = true)
+        }
+        // Indeterminate
+        Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+            Checkbox(onCheckedChange = {}, enabled = true, checked = false, indeterminate = true)
+            Checkbox(onCheckedChange = {}, enabled = false, checked = false, indeterminate = true)
+        }
+        // Error
+        Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+            Checkbox(hasError = true, onCheckedChange = {}, checked = false)
+            Checkbox(hasError = true, onCheckedChange = {}, enabled = false, checked = false)
+        }
+        Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+            Checkbox(hasError = true, onCheckedChange = {}, enabled = true, checked = true)
+            Checkbox(hasError = true, onCheckedChange = {}, enabled = false, checked = true)
+        }
+        Row(horizontalArrangement = Arrangement.spacedBy(6.dp)) {
+            Checkbox(onCheckedChange = {}, enabled = true, checked = false, indeterminate = true, hasError = true)
+            Checkbox(onCheckedChange = {}, enabled = false, checked = false, indeterminate = true, hasError = true)
+        }
     }
 }
