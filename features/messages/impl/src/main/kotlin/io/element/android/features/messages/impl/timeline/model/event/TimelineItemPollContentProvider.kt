7cde04a815adbd0b3388c19e8055f63a980ed923
Florian Renaud <florianr@element.io>
2023-08-09 18:20:49 +0200

    Minor cleanup

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/model/event/TimelineItemPollContentProvider.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/model/event/TimelineItemPollContentProvider.kt
@@ -18,54 +18,22 @@ package io.element.android.features.messages.impl.timeline.model.event
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import io.element.android.features.poll.api.PollAnswerItem
+import io.element.android.features.poll.api.aPollAnswerItemList
 import io.element.android.libraries.architecture.Async
-import io.element.android.libraries.matrix.api.poll.PollAnswer
-import io.element.android.libraries.matrix.ui.components.aMatrixUserList
 
 open class TimelineItemPollContentProvider : PreviewParameterProvider<TimelineItemPollContent> {
     override val values: Sequence<TimelineItemPollContent>
         get() = sequenceOf(
             aTimelineItemPollContent(),
-            aTimelineItemPollContent().copy(isDisclosed = mutableStateOf(Async.Success(true)))
+            aTimelineItemPollContent().copy(isDisclosed = mutableStateOf(Async.Success(true))),
         )
 }
 
 fun aTimelineItemPollContent(): TimelineItemPollContent {
-    val aUserList = aMatrixUserList().map { it.userId }
     return TimelineItemPollContent(
         isDisclosed = mutableStateOf(Async.Uninitialized),
         question = "What type of food should we have at the party?",
-        answers = listOf(
-            PollAnswerItem(
-                answer = PollAnswer("option_1", "Italian \uD83C\uDDEE\uD83C\uDDF9"),
-                votesCount = 2,
-                progress = 0.2f
-            ),
-            PollAnswerItem(
-                answer = PollAnswer("option_2", "Chinese \uD83C\uDDE8\uD83C\uDDF3"),
-                votesCount = 5,
-                progress = 0.5f
-            ),
-            PollAnswerItem(
-                answer = PollAnswer("option_3", "Brazilian \uD83C\uDDE7\uD83C\uDDF7"),
-                votesCount = 3,
-                progress = 0.3f
-            ),
-            PollAnswerItem(
-                answer = PollAnswer(
-                    "option_4",
-                    "French \uD83C\uDDEB\uD83C\uDDF7 But make it a very very very long option then this should just keep expanding"
-                ),
-                votesCount = 2,
-                progress = 0.2f
-            ),
-        ),
-        votes = mapOf(
-            "option_1" to aUserList.take(2),
-            "option_2" to aUserList.take(5),
-            "option_3" to aUserList.take(3),
-            "option_4" to emptyList(),
-        ),
+        answers = aPollAnswerItemList(),
+        votes = emptyMap(),
     )
 }
