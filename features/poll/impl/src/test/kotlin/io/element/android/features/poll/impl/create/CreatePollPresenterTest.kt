f2e619f389a92b6d4b399e584d8d377be1f5ffaf
Marco Romano <marcor@element.io>
2023-08-28 15:29:18 +0200

    Complete tests

--- features/poll/impl/src/test/kotlin/io/element/android/features/poll/impl/create/CreatePollPresenterTest.kt
+++ features/poll/impl/src/test/kotlin/io/element/android/features/poll/impl/create/CreatePollPresenterTest.kt
@@ -20,22 +20,28 @@ import app.cash.molecule.RecompositionMode
 import app.cash.molecule.moleculeFlow
 import app.cash.turbine.test
 import com.google.common.truth.Truth
+import io.element.android.features.analytics.test.FakeAnalyticsService
 import io.element.android.libraries.matrix.api.poll.PollKind
+import io.element.android.libraries.matrix.test.room.CreatePollInvocation
 import io.element.android.libraries.matrix.test.room.FakeMatrixRoom
-import io.element.android.services.analytics.noop.NoopAnalyticsService
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
 
 class CreatePollPresenterTest {
 
-    val presenter = CreatePollPresenter(
-        room = FakeMatrixRoom(),
-        analyticsService = NoopAnalyticsService(),
-        backNavigator = {}, // TODO
+    private var navUpInvocationsCount = 0
+    private val fakeMatrixRoom = FakeMatrixRoom()
+    private val fakeAnalyticsService = FakeAnalyticsService()
+
+    private val presenter = CreatePollPresenter(
+        room = fakeMatrixRoom,
+        analyticsService = fakeAnalyticsService,
+        navigateUp = { navUpInvocationsCount++ },
     )
 
     @Test
-    fun `default state has empty question with 2 empty answers and disclosed kind`() = runTest {
+    fun `default state has proper default values`() = runTest {
         moleculeFlow(RecompositionMode.Immediate) {
             presenter.present()
         }.test {
@@ -52,45 +58,183 @@ class CreatePollPresenterTest {
 
     @Test
     fun `non blank question and 2 answers are required to create a poll`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            Truth.assertThat(initial.canCreate).isEqualTo(false)
+
+            initial.eventSink(CreatePollEvents.SetQuestion("A question?"))
+            val questionSet = awaitItem()
+            Truth.assertThat(questionSet.canCreate).isEqualTo(false)
+
+            questionSet.eventSink(CreatePollEvents.SetAnswer(0, "Answer 1"))
+            val answer1Set = awaitItem()
+            Truth.assertThat(answer1Set.canCreate).isEqualTo(false)
+
+            answer1Set.eventSink(CreatePollEvents.SetAnswer(1, "Answer 2"))
+            val answer2Set = awaitItem()
+            Truth.assertThat(answer2Set.canCreate).isEqualTo(true)
+        }
     }
 
     @Test
     fun `create polls sends a poll start event`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            initial.eventSink(CreatePollEvents.SetQuestion("A question?"))
+            initial.eventSink(CreatePollEvents.SetAnswer(0, "Answer 1"))
+            initial.eventSink(CreatePollEvents.SetAnswer(1, "Answer 2"))
+            skipItems(3)
+            initial.eventSink(CreatePollEvents.Create)
+            delay(1) // Wait for the coroutine to finish
+            Truth.assertThat(fakeMatrixRoom.createPollInvocations.size).isEqualTo(1)
+            Truth.assertThat(fakeMatrixRoom.createPollInvocations.last()).isEqualTo(
+                CreatePollInvocation(
+                    question = "A question?",
+                    answers = listOf("Answer 1", "Answer 2"),
+                    maxSelections = 1,
+                    pollKind = PollKind.Disclosed
+                )
+            )
+        }
     }
 
     @Test
-    fun `add answer button adds an empty answer`() = runTest {
-    }
+    fun `add answer button adds an empty answer and removing it removes it`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            Truth.assertThat(initial.answers.size).isEqualTo(2)
 
-    @Test
-    fun `delete answer button removes the given answer`() = runTest {
+            initial.eventSink(CreatePollEvents.AddAnswer)
+            val answerAdded = awaitItem()
+            Truth.assertThat(answerAdded.answers.size).isEqualTo(3)
+            Truth.assertThat(answerAdded.answers[2].text).isEqualTo("")
+
+            initial.eventSink(CreatePollEvents.RemoveAnswer(2))
+            val answerRemoved = awaitItem()
+            Truth.assertThat(answerRemoved.answers.size).isEqualTo(2)
+        }
     }
 
     @Test
     fun `set question sets the question`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            initial.eventSink(CreatePollEvents.SetQuestion("A question?"))
+            val questionSet = awaitItem()
+            Truth.assertThat(questionSet.question).isEqualTo("A question?")
+        }
     }
 
     @Test
     fun `set poll answer sets the given poll answer`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            initial.eventSink(CreatePollEvents.SetAnswer(0, "This is answer 1"))
+            val answerSet = awaitItem()
+            Truth.assertThat(answerSet.answers.first().text).isEqualTo("This is answer 1")
+        }
     }
 
     @Test
     fun `set poll kind sets the poll kind`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            initial.eventSink(CreatePollEvents.SetPollKind(PollKind.Undisclosed))
+            val kindSet = awaitItem()
+            Truth.assertThat(kindSet.pollKind).isEqualTo(PollKind.Undisclosed)
+        }
     }
 
     @Test
-    fun `can add options when between 2 and 20`() = runTest {
-    }
-
-    @Test
-    fun `cannot add option when there are already 20`() = runTest {
+    fun `can add options when between 2 and 20 and then no more`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            Truth.assertThat(initial.canAddAnswer).isEqualTo(true)
+            repeat(17) {
+                initial.eventSink(CreatePollEvents.AddAnswer)
+                Truth.assertThat(awaitItem().canAddAnswer).isEqualTo(true)
+            }
+            initial.eventSink(CreatePollEvents.AddAnswer)
+            Truth.assertThat(awaitItem().canAddAnswer).isEqualTo(false)
+        }
     }
 
     @Test
     fun `can delete option if there are more than 2`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            Truth.assertThat(initial.answers.all { it.canDelete }).isEqualTo(false)
+            initial.eventSink(CreatePollEvents.AddAnswer)
+            Truth.assertThat(awaitItem().answers.all { it.canDelete }).isEqualTo(true)
+        }
     }
 
     @Test
     fun `option with more than 240 char is truncated`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            initial.eventSink(CreatePollEvents.SetAnswer(0, "A".repeat(241)))
+            Truth.assertThat(awaitItem().answers.first().text.length).isEqualTo(240)
+        }
+    }
+
+    @Test
+    fun `navBack event calls navBack lambda`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            Truth.assertThat(navUpInvocationsCount).isEqualTo(0)
+            initial.eventSink(CreatePollEvents.NavBack)
+            Truth.assertThat(navUpInvocationsCount).isEqualTo(1)
+        }
+    }
+
+    @Test
+    fun `confirm nav back with blank fields calls nav back lambda`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            Truth.assertThat(navUpInvocationsCount).isEqualTo(0)
+            Truth.assertThat(initial.showConfirmation).isEqualTo(false)
+            initial.eventSink(CreatePollEvents.ConfirmNavBack)
+            Truth.assertThat(navUpInvocationsCount).isEqualTo(1)
+        }
+    }
+
+    @Test
+    fun `confirm nav back with non blank fields shows confirmation dialog and sending hide hids it`() = runTest {
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initial = awaitItem()
+            initial.eventSink(CreatePollEvents.SetQuestion("Non blank"))
+            Truth.assertThat(navUpInvocationsCount).isEqualTo(0)
+            Truth.assertThat(awaitItem().showConfirmation).isEqualTo(false)
+            initial.eventSink(CreatePollEvents.ConfirmNavBack)
+            Truth.assertThat(navUpInvocationsCount).isEqualTo(0)
+            Truth.assertThat(awaitItem().showConfirmation).isEqualTo(true)
+            initial.eventSink(CreatePollEvents.HideConfirmation)
+            Truth.assertThat(awaitItem().showConfirmation).isEqualTo(false)
+        }
     }
 }
