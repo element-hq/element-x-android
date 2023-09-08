6b04c6ff1bf914a516e37fe5b978754682f02d1e
jonnyandrew <jonnya@element.io>
2023-09-08 15:36:45 +0100

    Fix visual alignment of send icon

--- libraries/textcomposer/impl/src/main/kotlin/io/element/android/libraries/textcomposer/TextComposer.kt
+++ libraries/textcomposer/impl/src/main/kotlin/io/element/android/libraries/textcomposer/TextComposer.kt
@@ -30,6 +30,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -581,7 +582,7 @@ private fun SendButton(
         ) {
             Icon(
                 modifier = Modifier
-                    .size(16.dp.applyScaleUp())
+                    .height(18.dp.applyScaleUp())
                     .align(Alignment.Center),
                 resourceId = iconId,
                 contentDescription = contentDescription,
