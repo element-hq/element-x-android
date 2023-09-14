4933edb8eadcc1b0df966cb824ace98731a596f3
jonnyandrew <jonnya@element.io>
2023-09-14 18:41:35 +0100

    Ensure keyboard opens for reply and text formatting modes

--- libraries/androidutils/src/main/kotlin/io/element/android/libraries/androidutils/ui/View.kt
+++ libraries/androidutils/src/main/kotlin/io/element/android/libraries/androidutils/ui/View.kt
@@ -17,8 +17,11 @@
 package io.element.android.libraries.androidutils.ui
 
 import android.view.View
+import android.view.ViewTreeObserver
 import android.view.inputmethod.InputMethodManager
 import androidx.core.content.getSystemService
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 fun View.hideKeyboard() {
     val imm = context?.getSystemService<InputMethodManager>()
@@ -40,4 +43,25 @@ fun View.setHorizontalPadding(padding: Int) {
         padding,
         paddingBottom
     )
+}
+
+suspend fun View.awaitWindowFocus() = suspendCancellableCoroutine { continuation ->
+    if (hasWindowFocus()) {
+        continuation.resume(Unit)
+    } else {
+        val listener = object : ViewTreeObserver.OnWindowFocusChangeListener {
+            override fun onWindowFocusChanged(hasFocus: Boolean) {
+                if (hasFocus) {
+                    viewTreeObserver.removeOnWindowFocusChangeListener(this)
+                    continuation.resume(Unit)
+                }
+            }
+        }
+
+        viewTreeObserver.addOnWindowFocusChangeListener(listener)
+
+        continuation.invokeOnCancellation {
+            viewTreeObserver.removeOnWindowFocusChangeListener(listener)
+        }
+    }
 }
