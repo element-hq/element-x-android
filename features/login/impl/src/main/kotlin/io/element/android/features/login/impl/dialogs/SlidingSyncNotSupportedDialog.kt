53787489117bb1b379ae23d9b9be00ac1d4da038
Benoit Marty <benoit@matrix.org>
2023-09-29 17:06:32 +0200

    Add preview for SlidingSyncNotSupportedDialog

--- features/login/impl/src/main/kotlin/io/element/android/features/login/impl/dialogs/SlidingSyncNotSupportedDialog.kt
+++ features/login/impl/src/main/kotlin/io/element/android/features/login/impl/dialogs/SlidingSyncNotSupportedDialog.kt
@@ -21,6 +21,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import io.element.android.features.login.impl.R
 import io.element.android.libraries.designsystem.components.dialogs.ConfirmationDialog
+import io.element.android.libraries.designsystem.preview.ElementPreview
+import io.element.android.libraries.designsystem.preview.PreviewsDayNight
 import io.element.android.libraries.ui.strings.CommonStrings
 
 @Composable
@@ -37,5 +39,14 @@ internal fun SlidingSyncNotSupportedDialog(
         onCancelClicked = onDismiss,
         title = stringResource(CommonStrings.dialog_title_error),
         content = stringResource(R.string.screen_change_server_error_no_sliding_sync_message),
+    )
+}
+
+@PreviewsDayNight
+@Composable
+internal fun SlidingSyncNotSupportedDialogPreview() = ElementPreview {
+    SlidingSyncNotSupportedDialog(
+        onLearnMoreClicked = {},
+        onDismiss = {},
     )
 }
