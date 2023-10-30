0841a19d38f27b96f3d3ae98a7ee7fcea3795187
Marco Romano <marcor@element.io>
2023-10-30 11:13:35 +0100

    Add a11y label to voice messages

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/event/TimelineItemVoiceView.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/event/TimelineItemVoiceView.kt
@@ -34,6 +34,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
@@ -64,8 +66,11 @@ fun TimelineItemVoiceView(
         state.eventSink(VoiceMessageEvents.PlayPause)
     }
 
+    val a11y = stringResource(CommonStrings.common_voice_message)
     Row(
-        modifier = modifier,
+        modifier = modifier.semantics {
+            contentDescription = a11y
+        },
         verticalAlignment = Alignment.CenterVertically,
     ) {
         when (state.button) {
