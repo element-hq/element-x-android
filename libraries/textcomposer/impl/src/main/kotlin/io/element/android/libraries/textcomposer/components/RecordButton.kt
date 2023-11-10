d55eb57857539dc1e018dc8da827862cd5400921
jonnyandrew <jonnya@element.io>
2023-11-10 12:33:34 +0000

    Remove hold to record tooltip

--- libraries/textcomposer/impl/src/main/kotlin/io/element/android/libraries/textcomposer/components/RecordButton.kt
+++ libraries/textcomposer/impl/src/main/kotlin/io/element/android/libraries/textcomposer/components/RecordButton.kt
@@ -14,38 +14,25 @@
  * limitations under the License.
  */
 
-@file:OptIn(ExperimentalMaterial3Api::class)
 
 package io.element.android.libraries.textcomposer.components
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.TooltipState
-import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import io.element.android.libraries.designsystem.components.tooltip.ElementTooltipDefaults
-import io.element.android.libraries.designsystem.components.tooltip.PlainTooltip
-import io.element.android.libraries.designsystem.components.tooltip.TooltipBox
 import io.element.android.libraries.designsystem.preview.ElementPreview
 import io.element.android.libraries.designsystem.preview.PreviewsDayNight
 import io.element.android.libraries.designsystem.theme.components.Icon
 import io.element.android.libraries.designsystem.theme.components.IconButton
-import io.element.android.libraries.designsystem.theme.components.Text
 import io.element.android.libraries.designsystem.utils.CommonDrawables
-import io.element.android.libraries.textcomposer.R
 import io.element.android.libraries.textcomposer.utils.PressState
 import io.element.android.libraries.textcomposer.utils.PressStateEffects
 import io.element.android.libraries.textcomposer.utils.rememberPressState
@@ -53,11 +40,9 @@ import io.element.android.libraries.theme.ElementTheme
 import io.element.android.libraries.ui.strings.CommonStrings
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun RecordButton(
     modifier: Modifier = Modifier,
-    initialTooltipIsVisible: Boolean = false,
     onPressStart: () -> Unit = {},
     onLongPressEnd: () -> Unit = {},
     onTap: () -> Unit = {},
@@ -69,10 +54,6 @@ internal fun RecordButton(
     val performHapticFeedback = {
         hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
     }
-
-    val tooltipState = rememberTooltipState(
-        initialIsVisible = initialTooltipIsVisible
-    )
 
     PressStateEffects(
         pressState = pressState.value,
@@ -87,34 +68,25 @@ internal fun RecordButton(
         onTap = {
             onTap()
             performHapticFeedback()
-            coroutineScope.launch { tooltipState.show() }
         },
     )
-    Box(modifier = modifier) {
-        HoldToRecordTooltip(
-            tooltipState = tooltipState,
-            spacingBetweenTooltipAndAnchor = 0.dp, // Accounts for the 48.dp size of the record button
-            anchor = {
-                RecordButtonView(
-                    isPressed = pressState.value is PressState.Pressing,
-                    modifier = Modifier
-                        .pointerInput(Unit) {
-                            awaitPointerEventScope {
-                                while (true) {
-                                    val event = awaitPointerEvent()
-                                    coroutineScope.launch {
-                                        when (event.type) {
-                                            PointerEventType.Press -> pressState.press()
-                                            PointerEventType.Release -> pressState.release()
-                                        }
-                                    }
-                                }
+    RecordButtonView(
+        isPressed = pressState.value is PressState.Pressing,
+        modifier = modifier
+            .pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        val event = awaitPointerEvent()
+                        coroutineScope.launch {
+                            when (event.type) {
+                                PointerEventType.Press -> pressState.press()
+                                PointerEventType.Release -> pressState.release()
                             }
                         }
-                )
+                    }
+                }
             }
-        )
-    }
+    )
 }
 
 @Composable
@@ -140,50 +112,11 @@ private fun RecordButtonView(
     }
 }
 
-@Composable
-private fun HoldToRecordTooltip(
-    tooltipState: TooltipState,
-    spacingBetweenTooltipAndAnchor: Dp,
-    modifier: Modifier = Modifier,
-    anchor: @Composable () -> Unit,
-) {
-    TooltipBox(
-        positionProvider = ElementTooltipDefaults.rememberPlainTooltipPositionProvider(
-            spacingBetweenTooltipAndAnchor = spacingBetweenTooltipAndAnchor,
-        ),
-        tooltip = {
-            PlainTooltip {
-                Text(
-                    text = stringResource(R.string.screen_room_voice_message_tooltip),
-                    color = ElementTheme.colors.textOnSolidPrimary,
-                    style = ElementTheme.typography.fontBodySmMedium,
-                )
-            }
-        },
-        state = tooltipState,
-        modifier = modifier,
-        focusable = false,
-        enableUserInput = false,
-        content = anchor,
-    )
-}
-
 @PreviewsDayNight
 @Composable
 internal fun RecordButtonPreview() = ElementPreview {
     Row {
         RecordButtonView(isPressed = false)
         RecordButtonView(isPressed = true)
-    }
-}
-
-@PreviewsDayNight
-@Composable
-internal fun HoldToRecordTooltipPreview() = ElementPreview {
-    Box(modifier = Modifier.fillMaxSize()) {
-        RecordButton(
-            modifier = Modifier.align(Alignment.BottomEnd),
-            initialTooltipIsVisible = true,
-        )
     }
 }
