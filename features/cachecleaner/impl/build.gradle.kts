7dc4ff5b8ca30af8bc770bc65aac968b41906fe7
Marco Romano <marcor@element.io>
2023-11-17 16:23:59 +0100

    Remove unused dependencies/plugins

--- features/cachecleaner/impl/build.gradle.kts
+++ features/cachecleaner/impl/build.gradle.kts
@@ -17,8 +17,6 @@
 plugins {
     id("io.element.android-compose-library")
     alias(libs.plugins.anvil)
-    alias(libs.plugins.ksp)
-    id("kotlin-parcelize")
 }
 
 android {
@@ -35,17 +33,9 @@ dependencies {
     api(projects.features.cachecleaner.api)
     implementation(projects.libraries.core)
     implementation(projects.libraries.architecture)
-    implementation(projects.libraries.matrix.api)
-    implementation(projects.libraries.matrixui)
-    implementation(projects.libraries.designsystem)
 
     testImplementation(libs.test.junit)
     testImplementation(libs.coroutines.test)
-    testImplementation(libs.molecule.runtime)
     testImplementation(libs.test.truth)
-    testImplementation(libs.test.turbine)
-    testImplementation(projects.libraries.matrix.test)
     testImplementation(projects.tests.testutils)
-
-    ksp(libs.showkase.processor)
 }
