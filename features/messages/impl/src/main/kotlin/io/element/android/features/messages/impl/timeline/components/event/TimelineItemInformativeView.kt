9ec67e3cbb58fab804f8c3f238dc1d1f589d5e59
Jorge Martin Espinosa <jorgem@element.io>
2023-12-20 14:37:28 +0100

    Rework timestamp positioning (#2075)
    * Rework timestamp positioning
    
    ---------
    
    Co-authored-by: ElementBot <benoitm+elementbot@element.io>

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/event/TimelineItemInformativeView.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/event/TimelineItemInformativeView.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import io.element.android.compound.theme.ElementTheme
 import io.element.android.libraries.designsystem.icons.CompoundDrawables
 import io.element.android.libraries.designsystem.preview.ElementPreview
@@ -58,7 +57,7 @@ fun TimelineItemInformativeView(
             fontStyle = FontStyle.Italic,
             color = MaterialTheme.colorScheme.secondary,
             style = ElementTheme.typography.fontBodyMdRegular,
-            text = text + extraPadding.getStr(14.sp)
+            text = text + extraPadding.getStr(textStyle = ElementTheme.typography.fontBodyMdRegular)
         )
     }
 }
