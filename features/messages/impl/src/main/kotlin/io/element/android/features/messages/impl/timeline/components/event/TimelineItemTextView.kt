e71e2e8658d3e62734db064772daccb99262e700
Jorge Martín <jorgem@element.io>
2023-12-30 10:35:06 +0100

    Add screenshot diff

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/event/TimelineItemTextView.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/event/TimelineItemTextView.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import androidx.core.text.buildSpannedString
@@ -49,7 +50,7 @@ fun TimelineItemTextView(
 
         val formattedBody = content.formattedBody
         val body = SpannableString(formattedBody ?: content.body)
-        val extraPaddingText = extraPadding.getStr()
+        val extraPaddingText = "test"
 
         Box(modifier) {
             val textWithPadding = remember(body) {
