14d5274d22eb8bd925663295c5c785ca0b44b853
Benoit Marty <benoit@matrix.org>
2024-01-12 15:56:47 +0100

    Display name disambiguation #2215.
    Applied to:
    - timeline message
    - detail of timeline message
    - reply preview of timeline message
    - rendering of state Event
    Not applied to:
    - room last message
    - room member list (we display the MatrixId here)
    - room member detail page

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/factories/event/TimelineItemEventFactory.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/factories/event/TimelineItemEventFactory.kt
@@ -33,6 +33,7 @@ import io.element.android.libraries.matrix.api.MatrixClient
 import io.element.android.libraries.matrix.api.room.RoomMember
 import io.element.android.libraries.matrix.api.timeline.MatrixTimelineItem
 import io.element.android.libraries.matrix.api.timeline.item.event.ProfileTimelineDetails
+import io.element.android.libraries.matrix.api.timeline.item.event.getDisambiguatedDisplayName
 import kotlinx.collections.immutable.toImmutableList
 import java.text.DateFormat
 import java.util.Date
@@ -63,7 +64,7 @@ class TimelineItemEventFactory @Inject constructor(
                 senderAvatarUrl = null
             }
             is ProfileTimelineDetails.Ready -> {
-                senderDisplayName = senderProfile.displayName
+                senderDisplayName = senderProfile.getDisambiguatedDisplayName(currentSender)
                 senderAvatarUrl = senderProfile.avatarUrl
             }
         }
