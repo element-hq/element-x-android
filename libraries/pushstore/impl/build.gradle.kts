4b37ea1a2d38c5c7ae28de160c075561c3b9bad8
Benoit Marty <benoit@matrix.org>
2024-01-16 14:34:06 +0100

    Ensure client secret is deleted when the user signs out.

--- libraries/pushstore/impl/build.gradle.kts
+++ libraries/pushstore/impl/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     testImplementation(libs.coroutines.test)
     testImplementation(projects.libraries.matrix.test)
     testImplementation(projects.services.appnavstate.test)
+    testImplementation(projects.libraries.sessionStorage.test)
 
     androidTestImplementation(libs.coroutines.test)
     androidTestImplementation(libs.test.core)
