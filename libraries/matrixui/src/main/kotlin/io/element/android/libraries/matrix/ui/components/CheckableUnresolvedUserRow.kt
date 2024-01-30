7c794f5330058aacfefebed3108f060ecb44ff3d
Jorge Martín <jorgem@element.io>
2024-01-30 10:16:25 +0100

    Fix padding in `CheckableUnresolvedUserRow`

--- libraries/matrixui/src/main/kotlin/io/element/android/libraries/matrix/ui/components/CheckableUnresolvedUserRow.kt
+++ libraries/matrixui/src/main/kotlin/io/element/android/libraries/matrix/ui/components/CheckableUnresolvedUserRow.kt
@@ -20,13 +20,11 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import io.element.android.libraries.designsystem.components.avatar.AvatarData
 import io.element.android.libraries.designsystem.components.avatar.AvatarSize
 import io.element.android.libraries.designsystem.preview.ElementThemedPreview
@@ -58,7 +56,6 @@ fun CheckableUnresolvedUserRow(
         )
 
         Checkbox(
-            modifier = Modifier.padding(end = 16.dp),
             checked = checked,
             onCheckedChange = null,
             enabled = enabled,
