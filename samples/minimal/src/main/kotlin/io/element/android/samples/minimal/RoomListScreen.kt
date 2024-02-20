448acd3d491c193c2f4e0e95fb0e5038e581ed1e
ganfra <francoisg@matrix.org>
2024-02-20 21:18:17 +0100

    RoomListFilters : fix sample compilation

--- samples/minimal/src/main/kotlin/io/element/android/samples/minimal/RoomListScreen.kt
+++ samples/minimal/src/main/kotlin/io/element/android/samples/minimal/RoomListScreen.kt
@@ -28,6 +28,7 @@ import io.element.android.features.roomlist.impl.RoomListView
 import io.element.android.features.roomlist.impl.datasource.DefaultInviteStateDataSource
 import io.element.android.features.roomlist.impl.datasource.RoomListDataSource
 import io.element.android.features.roomlist.impl.datasource.RoomListRoomSummaryFactory
+import io.element.android.features.roomlist.impl.filters.RoomListFiltersPresenter
 import io.element.android.features.roomlist.impl.migration.MigrationScreenPresenter
 import io.element.android.features.roomlist.impl.migration.SharedPrefsMigrationScreenStore
 import io.element.android.features.roomlist.impl.search.RoomListSearchPresenter
@@ -120,6 +121,10 @@ class RoomListScreen(
             context = context,
             sessionId = matrixClient.sessionId,
             sessionCoroutineScope = Singleton.appScope
+        ),
+        filtersPresenter = RoomListFiltersPresenter(
+            roomListService = matrixClient.roomListService,
+            featureFlagService = featureFlagService,
         ),
     )
 
