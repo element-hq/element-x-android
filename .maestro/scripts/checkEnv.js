15728d6b5890ce312812bad06890029edf73d77a
Benoit Marty <benoit@matrix.org>
2024-02-22 09:29:05 +0100

    Add prefix `MAESTRO_` to Maestro env variable.

--- .maestro/scripts/checkEnv.js
+++ .maestro/scripts/checkEnv.js
@@ -1,10 +1,10 @@
 // This array contains all the required environment variable. When adding a variable, add it here also.
 // If a variable is missing, an error will occur.
 
-if (APP_ID == null) throw "Fatal: missing env variable APP_ID"
-if (USERNAME == null) throw "Fatal: missing env variable USERNAME"
-if (PASSWORD == null) throw "Fatal: missing env variable PASSWORD"
-if (RECOVERY_KEY == null) throw "Fatal: missing env variable RECOVERY_KEY"
-if (ROOM_NAME == null) throw "Fatal: missing env variable ROOM_NAME"
-if (INVITEE1_MXID == null) throw "Fatal: missing env variable INVITEE1_MXID"
-if (INVITEE2_MXID == null) throw "Fatal: missing env variable INVITEE2_MXID"
+if (MAESTRO_APP_ID == null) throw "Fatal: missing env variable MAESTRO_APP_ID"
+if (MAESTRO_USERNAME == null) throw "Fatal: missing env variable MAESTRO_USERNAME"
+if (MAESTRO_PASSWORD == null) throw "Fatal: missing env variable MAESTRO_PASSWORD"
+if (MAESTRO_RECOVERY_KEY == null) throw "Fatal: missing env variable MAESTRO_RECOVERY_KEY"
+if (MAESTRO_ROOM_NAME == null) throw "Fatal: missing env variable MAESTRO_ROOM_NAME"
+if (MAESTRO_INVITEE1_MXID == null) throw "Fatal: missing env variable MAESTRO_INVITEE1_MXID"
+if (MAESTRO_INVITEE2_MXID == null) throw "Fatal: missing env variable MAESTRO_INVITEE2_MXID"
