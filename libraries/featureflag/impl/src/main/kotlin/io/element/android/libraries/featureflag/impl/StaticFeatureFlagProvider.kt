bb84050c91d4feee23de32b4ac85e675f43e4f5f
Jorge Martín <jorgem@element.io>
2024-04-09 13:55:33 +0200

    Remove the feature flag

--- libraries/featureflag/impl/src/main/kotlin/io/element/android/libraries/featureflag/impl/StaticFeatureFlagProvider.kt
+++ libraries/featureflag/impl/src/main/kotlin/io/element/android/libraries/featureflag/impl/StaticFeatureFlagProvider.kt
@@ -41,7 +41,6 @@ class StaticFeatureFlagProvider @Inject constructor() :
                 FeatureFlags.Mentions -> true
                 FeatureFlags.MarkAsUnread -> true
                 FeatureFlags.RoomListFilters -> true
-                FeatureFlags.RoomModeration -> true
                 FeatureFlags.RoomDirectorySearch -> false
             }
         } else {
