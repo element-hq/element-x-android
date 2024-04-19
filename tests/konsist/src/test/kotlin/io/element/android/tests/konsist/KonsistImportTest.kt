94930bae80b1cb34e8c1dc4eb4653cb30ced9e54
Benoit Marty <benoit@matrix.org>
2024-04-19 11:40:16 +0200

    Add Konsist check to ensure that 'org.jetbrains.annotations.VisibleForTesting' is not used.

---
+++ tests/konsist/src/test/kotlin/io/element/android/tests/konsist/KonsistImportTest.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2024 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.element.android.tests.konsist
+
+import com.lemonappdev.konsist.api.Konsist
+import com.lemonappdev.konsist.api.verify.assertFalse
+import org.junit.Test
+
+class KonsistImportTest {
+    @Test
+    fun `Functions with '@VisibleForTesting' annotation should use 'androidx' version`() {
+        Konsist
+            .scopeFromProject()
+            .imports
+            .assertFalse(
+                additionalMessage = "Please use 'androidx.annotation.VisibleForTesting' instead of 'org.jetbrains.annotations.VisibleForTesting' (project convention).",
+            ) {
+                it.name == "org.jetbrains.annotations.VisibleForTesting"
+            }
+    }
+}
