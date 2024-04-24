124638c7a73a1ca3f17308e07ace0fd0493acc97
Benoit Marty <benoit@matrix.org>
2024-04-24 13:26:27 +0200

    Add KDoc.

--- features/migration/impl/src/main/kotlin/io/element/android/features/migration/impl/MigrationStore.kt
+++ features/migration/impl/src/main/kotlin/io/element/android/features/migration/impl/MigrationStore.kt
@@ -19,6 +19,16 @@ package io.element.android.features.migration.impl
 import kotlinx.coroutines.flow.Flow
 
 interface MigrationStore {
-    suspend fun setApplicationMigrationVersion(version: Int)
+    /**
+     * Return of flow of the current value for application migration version.
+     * If the value is not set, it will emit 0.
+     * If the emitted value is lower than the current application migration version, it means
+     * that a migration should occur, and at the end [setApplicationMigrationVersion] should be called.
+     */
     fun applicationMigrationVersion(): Flow<Int>
+
+    /**
+     * Set the application migration version, typically after a migration has been done.
+     */
+    suspend fun setApplicationMigrationVersion(version: Int)
 }
