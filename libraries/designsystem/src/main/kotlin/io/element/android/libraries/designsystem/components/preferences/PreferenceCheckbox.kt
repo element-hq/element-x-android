370702f125a64a967e8b7d4f16006ed697b18410
Benoit Marty <benoit@matrix.org>
2024-05-24 16:26:04 +0200

    Let PreferenceCheckbox use ListItem and add missing previews.

--- libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/components/preferences/PreferenceCheckbox.kt
+++ libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/components/preferences/PreferenceCheckbox.kt
@@ -17,25 +17,18 @@
 package io.element.android.libraries.designsystem.components.preferences
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import io.element.android.compound.theme.ElementTheme
+import io.element.android.libraries.designsystem.components.list.ListItemContent
 import io.element.android.libraries.designsystem.components.preferences.components.PreferenceIcon
 import io.element.android.libraries.designsystem.icons.CompoundDrawables
 import io.element.android.libraries.designsystem.preview.ElementThemedPreview
 import io.element.android.libraries.designsystem.preview.PreviewGroup
-import io.element.android.libraries.designsystem.theme.components.Checkbox
+import io.element.android.libraries.designsystem.theme.components.ListItem
 import io.element.android.libraries.designsystem.theme.components.Text
 import io.element.android.libraries.designsystem.toEnabledColor
 import io.element.android.libraries.designsystem.toSecondaryEnabledColor
@@ -52,29 +45,28 @@ fun PreferenceCheckbox(
     @DrawableRes iconResourceId: Int? = null,
     showIconAreaIfNoIcon: Boolean = false,
 ) {
-    Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .defaultMinSize(minHeight = preferenceMinHeight)
-            .clickable { onCheckedChange(!isChecked) }
-            .padding(vertical = 4.dp, horizontal = preferencePaddingHorizontal),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        PreferenceIcon(
-            icon = icon,
-            iconResourceId = iconResourceId,
-            enabled = enabled,
-            isVisible = showIconAreaIfNoIcon
-        )
-        Column(
-            modifier = Modifier.weight(1f),
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-        ) {
+    ListItem(
+        modifier = modifier,
+        leadingContent = if (iconResourceId != null || icon != null || showIconAreaIfNoIcon) {
+            ListItemContent.Custom {
+                PreferenceIcon(
+                    icon = icon,
+                    iconResourceId = iconResourceId,
+                    enabled = enabled,
+                    isVisible = showIconAreaIfNoIcon
+                )
+            }
+        } else {
+            null
+        },
+        headlineContent = {
             Text(
                 style = ElementTheme.typography.fontBodyLgRegular,
                 text = title,
                 color = enabled.toEnabledColor(),
             )
+        },
+        supportingContent = {
             if (supportingText != null) {
                 Text(
                     style = ElementTheme.typography.fontBodyMdRegular,
@@ -82,15 +74,13 @@ fun PreferenceCheckbox(
                     color = enabled.toSecondaryEnabledColor(),
                 )
             }
-        }
-        Checkbox(
-            modifier = Modifier
-                .align(Alignment.CenterVertically),
+        },
+        trailingContent = ListItemContent.Checkbox(
             checked = isChecked,
+            onChange = onCheckedChange,
             enabled = enabled,
-            onCheckedChange = onCheckedChange
-        )
-    }
+        ),
+    )
 }
 
 @Preview(group = PreviewGroup.Preferences)
@@ -108,6 +98,24 @@ internal fun PreferenceCheckboxPreview() = ElementThemedPreview {
             title = "Checkbox with supporting text",
             supportingText = "Supporting text",
             iconResourceId = CompoundDrawables.ic_compound_threads,
+            enabled = true,
+            isChecked = true,
+            onCheckedChange = {},
+        )
+        PreferenceCheckbox(
+            title = "Checkbox with supporting text",
+            supportingText = "Supporting text",
+            iconResourceId = null,
+            showIconAreaIfNoIcon = true,
+            enabled = true,
+            isChecked = true,
+            onCheckedChange = {},
+        )
+        PreferenceCheckbox(
+            title = "Checkbox with supporting text",
+            supportingText = "Supporting text",
+            iconResourceId = null,
+            showIconAreaIfNoIcon = false,
             enabled = true,
             isChecked = true,
             onCheckedChange = {},
