2fae4a52adbfa06fc42e34622534985684cb6e24
Benoit Marty <benoit@matrix.org>
2024-06-13 17:50:32 +0200

    Add documentation back.

--- appconfig/src/main/kotlin/io/element/android/appconfig/RageshakeConfig.kt
+++ appconfig/src/main/kotlin/io/element/android/appconfig/RageshakeConfig.kt
@@ -17,8 +17,18 @@
 package io.element.android.appconfig
 
 object RageshakeConfig {
+    /**
+     * The URL to submit bug reports to.
+     */
     const val BUG_REPORT_URL = "https://riot.im/bugreports/submit"
+
+    /**
+     * As per https://github.com/matrix-org/rageshake:
+     * Identifier for the application (eg 'riot-web').
+     * Should correspond to a mapping configured in the configuration file for github issue reporting to work.
+     */
     const val BUG_REPORT_APP_NAME = "element-x-android"
+
     /**
      * The maximum size of the upload request. Default value is just below CloudFlare's max request size.
      */
