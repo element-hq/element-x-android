efa7a2d0466ecf21ca1b526ccce578c58b726909
Benoit Marty <benoit@matrix.org>
2024-06-17 15:26:50 +0200

    Move InMemoryMigrationScreenStore to a test module.

--- features/roomlist/impl/build.gradle.kts
+++ features/roomlist/impl/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     testImplementation(projects.libraries.preferences.test)
     testImplementation(projects.services.analytics.test)
     testImplementation(projects.features.networkmonitor.test)
+    testImplementation(projects.features.roomlist.test)
     testImplementation(projects.tests.testutils)
     testImplementation(projects.features.leaveroom.test)
 }
