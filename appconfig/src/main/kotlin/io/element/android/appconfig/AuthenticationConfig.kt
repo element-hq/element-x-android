ab237eb89774f38a2a836eee89c4c3821056f830
Jorge Martín <jorgem@element.io>
2024-06-27 10:31:57 +0200

    Add docs to `AuthenticationConfig`

--- appconfig/src/main/kotlin/io/element/android/appconfig/AuthenticationConfig.kt
+++ appconfig/src/main/kotlin/io/element/android/appconfig/AuthenticationConfig.kt
@@ -19,8 +19,18 @@ package io.element.android.appconfig
 object AuthenticationConfig {
     const val MATRIX_ORG_URL = "https://matrix.org"
 
+    /**
+     * Default homeserver url to sign in with, unless the user selects a different one.
+     */
     const val DEFAULT_HOMESERVER_URL = MATRIX_ORG_URL
+
+    /**
+     * URL with some docs that explain what's sliding sync and how to add it to your home server.
+     */
     const val SLIDING_SYNC_READ_MORE_URL = "https://github.com/matrix-org/sliding-sync/blob/main/docs/Landing.md"
 
+    /**
+     * Force a sliding sync proxy url, if not null, the proxy url in the .well-known file will be ignored.
+     */
     val SLIDING_SYNC_PROXY_URL: String? = null
 }
