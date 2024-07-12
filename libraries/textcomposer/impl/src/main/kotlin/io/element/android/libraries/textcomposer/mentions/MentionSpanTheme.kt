2c879b020eabe21656173e5efffb4a5f00e8762a
Jorge Martín <jorgem@element.io>
2024-07-12 17:52:40 +0200

    Fix theme color preview

--- libraries/textcomposer/impl/src/main/kotlin/io/element/android/libraries/textcomposer/mentions/MentionSpanTheme.kt
+++ libraries/textcomposer/impl/src/main/kotlin/io/element/android/libraries/textcomposer/mentions/MentionSpanTheme.kt
@@ -19,8 +19,11 @@ package io.element.android.libraries.textcomposer.mentions
 import android.graphics.Color
 import android.graphics.Typeface
 import android.text.Spanned
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,13 +32,22 @@ import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.buildSpannedString
 import io.element.android.compound.theme.ElementTheme
+import io.element.android.libraries.designsystem.preview.ElementPreview
+import io.element.android.libraries.designsystem.preview.PreviewsDayNight
 import io.element.android.libraries.designsystem.text.rememberTypeface
 import io.element.android.libraries.designsystem.theme.currentUserMentionPillBackground
 import io.element.android.libraries.designsystem.theme.currentUserMentionPillText
 import io.element.android.libraries.designsystem.theme.mentionPillBackground
 import io.element.android.libraries.designsystem.theme.mentionPillText
+import io.element.android.libraries.matrix.api.core.RoomAlias
 import io.element.android.libraries.matrix.api.core.UserId
+import io.element.android.libraries.matrix.api.core.toRoomIdOrAlias
+import io.element.android.libraries.matrix.api.permalink.PermalinkData
+import io.element.android.libraries.matrix.api.permalink.PermalinkParser
+import kotlinx.collections.immutable.persistentListOf
 import javax.inject.Inject
 
 /**
@@ -99,3 +111,62 @@ fun MentionSpanTheme.updateMentionStyles(charSequence: CharSequence) {
 val LocalMentionSpanTheme = staticCompositionLocalOf {
     MentionSpanTheme()
 }
+
+ @PreviewsDayNight
+ @Composable
+ internal fun MentionSpanThemePreview() {
+    ElementPreview {
+        val mentionSpanTheme = remember { MentionSpanTheme() }
+        val provider = remember {
+            MentionSpanProvider(
+                permalinkParser = object : PermalinkParser {
+                    override fun parse(uriString: String): PermalinkData {
+                        return when (uriString) {
+                            "https://matrix.to/#/@me:matrix.org" -> PermalinkData.UserLink(UserId("@me:matrix.org"))
+                            "https://matrix.to/#/@other:matrix.org" -> PermalinkData.UserLink(UserId("@other:matrix.org"))
+                            "https://matrix.to/#/#room:matrix.org" -> PermalinkData.RoomLink(
+                                roomIdOrAlias = RoomAlias("#room:matrix.org").toRoomIdOrAlias(),
+                                eventId = null,
+                                viaParameters = persistentListOf(),
+                            )
+                            else -> throw AssertionError("Unexpected value $uriString")
+                        }
+                    }
+                },
+            )
+        }
+
+        val textColor = ElementTheme.colors.textPrimary.toArgb()
+        fun mentionSpanMe() = provider.getMentionSpanFor("mention", "https://matrix.to/#/@me:matrix.org")
+        fun mentionSpanOther() = provider.getMentionSpanFor("mention", "https://matrix.to/#/@other:matrix.org")
+        fun mentionSpanRoom() = provider.getMentionSpanFor("room", "https://matrix.to/#/#room:matrix.org")
+        mentionSpanTheme.updateStyles(currentUserId = UserId("@me:matrix.org"))
+
+        CompositionLocalProvider(
+            LocalMentionSpanTheme provides mentionSpanTheme
+        ) {
+            AndroidView(factory = { context ->
+                TextView(context).apply {
+                    includeFontPadding = false
+                    layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+                    text = buildSpannedString {
+                        append("This is a ")
+                        append("@mention", mentionSpanMe(), 0)
+                        append(" to the current user and this is a ")
+                        append("@mention", mentionSpanOther(), 0)
+                        append(" to other user. This one is for a room: ")
+                        append("#room:matrix.org", mentionSpanRoom(), 0)
+                        append("\n\n")
+                        append("This ")
+                        append("mention", mentionSpanMe(), 0)
+                        append(" didn't have an '@' and it was automatically added, same as this ")
+                        append("room:matrix.org", mentionSpanRoom(), 0)
+                        append(" one, which had no leading '#'.")
+                    }
+                    mentionSpanTheme.updateMentionStyles(text)
+                    setTextColor(textColor)
+                }
+            })
+        }
+    }
+ }
