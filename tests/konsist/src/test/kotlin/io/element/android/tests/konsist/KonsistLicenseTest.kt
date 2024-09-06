49fcba138177b3cc35ce8dfeb1c3bdfd3c5cf9c8
Benoit Marty <benoit@matrix.org>
2024-09-06 18:02:35 +0200

    Migrate license to AGPL - Update KonsistLicenseTest

--- tests/konsist/src/test/kotlin/io/element/android/tests/konsist/KonsistLicenseTest.kt
+++ tests/konsist/src/test/kotlin/io/element/android/tests/konsist/KonsistLicenseTest.kt
@@ -14,19 +14,10 @@ import org.junit.Test
 class KonsistLicenseTest {
     private val publicLicense = """
         /\*
-        (?:.*\n)* \* Copyright \(c\) 20\d\d New Vector Ltd
+        (?:.*\n)* \* Copyright 20\d\d((, |-)20\d\d)? New Vector Ltd.
         (?:.*\n)* \*
-         \* Licensed under the Apache License, Version 2\.0 \(the "License"\);
-         \* you may not use this file except in compliance with the License\.
-         \* You may obtain a copy of the License at
-         \*
-         \* {5}https?://www\.apache\.org/licenses/LICENSE-2\.0
-         \*
-         \* Unless required by applicable law or agreed to in writing, software
-         \* distributed under the License is distributed on an "AS IS" BASIS,
-         \* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\.
-         \* See the License for the specific language governing permissions and
-         \* limitations under the License\.
+         \* SPDX-License-Identifier: AGPL-3.0-only
+         \* Please see LICENSE in the repository root for full details.
          \*/
         """.trimIndent().toRegex()
 
