de7d93e37d3b3fee3bb4519cf3f28ba96f92d76d
Benoit Marty <benoit@matrix.org>
2024-09-19 16:41:19 +0200

    Add test on RustRoomListService

--- libraries/matrix/impl/src/test/kotlin/io/element/android/libraries/matrix/impl/fixtures/fakes/FakeRustRoomListService.kt
+++ libraries/matrix/impl/src/test/kotlin/io/element/android/libraries/matrix/impl/fixtures/fakes/FakeRustRoomListService.kt
@@ -10,9 +10,27 @@ package io.element.android.libraries.matrix.impl.fixtures.fakes
 import org.matrix.rustcomponents.sdk.NoPointer
 import org.matrix.rustcomponents.sdk.RoomList
 import org.matrix.rustcomponents.sdk.RoomListService
+import org.matrix.rustcomponents.sdk.RoomListServiceSyncIndicator
+import org.matrix.rustcomponents.sdk.RoomListServiceSyncIndicatorListener
+import org.matrix.rustcomponents.sdk.TaskHandle
 
 class FakeRustRoomListService : RoomListService(NoPointer) {
+
     override suspend fun allRooms(): RoomList {
         return FakeRustRoomList()
+    }
+
+    private var listener: RoomListServiceSyncIndicatorListener? = null
+    override fun syncIndicator(
+        delayBeforeShowingInMs: UInt,
+        delayBeforeHidingInMs: UInt,
+        listener: RoomListServiceSyncIndicatorListener,
+    ): TaskHandle {
+        this.listener = listener
+        return FakeRustTaskHandle()
+    }
+
+    fun emitRoomListServiceSyncIndicator(syncIndicator: RoomListServiceSyncIndicator) {
+        listener?.onUpdate(syncIndicator)
     }
 }
