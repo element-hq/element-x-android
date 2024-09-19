d1be4349f077e73c08277fb248fffa6e7a04dffc
Benoit Marty <benoit@matrix.org>
2024-09-19 15:40:25 +0200

    Add test on RustRoomDirectoryList

--- libraries/matrix/impl/src/test/kotlin/io/element/android/libraries/matrix/impl/fixtures/fakes/FakeRoomDirectorySearch.kt
+++ libraries/matrix/impl/src/test/kotlin/io/element/android/libraries/matrix/impl/fixtures/fakes/FakeRoomDirectorySearch.kt
@@ -7,7 +7,31 @@
 
 package io.element.android.libraries.matrix.impl.fixtures.fakes
 
+import io.element.android.tests.testutils.simulateLongTask
 import org.matrix.rustcomponents.sdk.NoPointer
 import org.matrix.rustcomponents.sdk.RoomDirectorySearch
+import org.matrix.rustcomponents.sdk.RoomDirectorySearchEntriesListener
+import org.matrix.rustcomponents.sdk.RoomDirectorySearchEntryUpdate
+import org.matrix.rustcomponents.sdk.TaskHandle
 
-class FakeRoomDirectorySearch : RoomDirectorySearch(NoPointer)
+class FakeRoomDirectorySearch(
+    var isAtLastPage: Boolean = false,
+) : RoomDirectorySearch(NoPointer) {
+    override suspend fun isAtLastPage(): Boolean {
+        return isAtLastPage
+    }
+
+    override suspend fun search(filter: String?, batchSize: UInt) = simulateLongTask { }
+    override suspend fun nextPage() = simulateLongTask { }
+
+    private var listener: RoomDirectorySearchEntriesListener? = null
+
+    override suspend fun results(listener: RoomDirectorySearchEntriesListener): TaskHandle {
+        this.listener = listener
+        return FakeRustTaskHandle()
+    }
+
+    fun emitResult(roomEntriesUpdate: List<RoomDirectorySearchEntryUpdate>) {
+        listener?.onUpdate(roomEntriesUpdate)
+    }
+}
