84465b3e3c9953118d560e34082056c542079786
Jorge Martín <jorgem@element.io>
2024-09-30 10:28:10 +0200

    Improve the helper to include dagger code generation.
    Add it to `app` and `appnav` modules.

--- plugins/src/main/kotlin/extension/AnvilExtensions.kt
+++ plugins/src/main/kotlin/extension/AnvilExtensions.kt
@@ -9,24 +9,48 @@ package extension
 
 import org.gradle.api.Project
 import com.squareup.anvil.plugin.AnvilExtension
+import org.gradle.accessors.dm.LibrariesForLibs
+import org.gradle.kotlin.dsl.the
 
 /**
  * Setup Anvil plugin with the given configuration.
- * @param generateDaggerFactories whether to generate Dagger factories using Anvil
+ * @param generateDaggerCode whether to enable general Dagger code generation using Kapt
+ * @param generateDaggerFactoriesUsingAnvil whether to generate Dagger factories using Anvil instead of Kapt
  */
-fun Project.setupAnvil(generateDaggerFactories: Boolean = true) {
-    plugins.apply("com.squareup.anvil")
-    project.pluginManager.withPlugin("com.squareup.anvil") {
-        extensions.configure(AnvilExtension::class.java) {
-            this.generateDaggerFactories.set(generateDaggerFactories)
+fun Project.setupAnvil(
+    generateDaggerCode: Boolean = false,
+    generateDaggerFactoriesUsingAnvil: Boolean = true,
+) {
+    val libs = the<LibrariesForLibs>()
+    // Apply plugins and dependencies
+    applyPluginIfNeeded("com.squareup.anvil")
 
-            // These dependencies are only needed for compose libraries
-            if (project.pluginManager.hasPlugin("io.element.android-compose-library")) {
-                // Annotations to generate DI code for Appyx nodes
-                dependencies.add("implementation", project.project(":anvilannotations"))
-                // Code generator for the annotations above
-                dependencies.add("anvil", project.project(":anvilcodegen"))
-            }
+    if (generateDaggerCode) {
+        applyPluginIfNeeded("org.jetbrains.kotlin.kapt")
+        // Needed at the top level since dagger code should be generated at a single point for performance
+        dependencies.add("implementation", libs.dagger)
+        dependencies.add("kapt", libs.dagger.compiler)
+    }
+
+    // These dependencies are only needed for compose library or application modules
+    if (project.pluginManager.hasPlugin("io.element.android-compose-library")
+        || project.pluginManager.hasPlugin("io.element.android-compose-application")) {
+        // Annotations to generate DI code for Appyx nodes
+        dependencies.add("implementation", project.project(":anvilannotations"))
+        // Code generator for the annotations above
+        dependencies.add("anvil", project.project(":anvilcodegen"))
+    }
+
+    project.pluginManager.withPlugin("com.squareup.anvil") {
+        // Setup extension
+        extensions.configure(AnvilExtension::class.java) {
+            this.generateDaggerFactories.set(generateDaggerFactoriesUsingAnvil)
         }
+    }
+}
+
+private fun Project.applyPluginIfNeeded(pluginId: String) {
+    if (!pluginManager.hasPlugin(pluginId)) {
+        pluginManager.apply(pluginId)
     }
 }
