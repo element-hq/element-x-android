0b67dfdc3dcf574822d7a84461b8dd25eeb9e9ca
Joe Groocock <me@frebib.net>
2024-10-05 15:54:06 +0000

    Use CircleShape instead of 50% RoundedCornerShape
    This is a builtin alias for the same thing.
    
    Signed-off-by: Joe Groocock <me@frebib.net>

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/protection/ProtectedView.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/protection/ProtectedView.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -59,7 +60,7 @@ fun ProtectedView(
                         .border(
                             width = 1.dp,
                             color = ElementTheme.colors.borderInteractiveSecondary,
-                            shape = RoundedCornerShape(percent = 50),
+                            shape = CircleShape,
                         )
                         .padding(
                             horizontal = 16.dp,
