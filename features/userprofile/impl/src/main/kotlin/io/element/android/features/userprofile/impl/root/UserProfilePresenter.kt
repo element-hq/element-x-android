77e874b906e8ff5c1afe54c6c4535a116542f9db
Benoit Marty <benoit@matrix.org>
2024-10-17 12:21:15 +0200

    Rework: use @AssistedFactory and comment out the UserProfileModule.

--- features/userprofile/impl/src/main/kotlin/io/element/android/features/userprofile/impl/root/UserProfilePresenter.kt
+++ features/userprofile/impl/src/main/kotlin/io/element/android/features/userprofile/impl/root/UserProfilePresenter.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import io.element.android.features.createroom.api.StartDMAction
 import io.element.android.features.userprofile.shared.UserProfileEvents
@@ -41,6 +42,7 @@ class UserProfilePresenter @AssistedInject constructor(
     private val client: MatrixClient,
     private val startDMAction: StartDMAction,
 ) : Presenter<UserProfileState> {
+    @AssistedFactory
     interface Factory {
         fun create(userId: UserId): UserProfilePresenter
     }
