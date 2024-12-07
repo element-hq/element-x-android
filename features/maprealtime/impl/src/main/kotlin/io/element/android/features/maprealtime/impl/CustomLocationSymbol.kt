3d683e36eee55b0b3f4373fa76a323e75f5764c9
torrybr <16907963+torrybr@users.noreply.github.com>
2024-12-07 13:11:01 -0500

    working on broken map crash

--- features/maprealtime/impl/src/main/kotlin/io/element/android/features/maprealtime/impl/CustomLocationSymbol.kt
+++ features/maprealtime/impl/src/main/kotlin/io/element/android/features/maprealtime/impl/CustomLocationSymbol.kt
@@ -11,23 +11,36 @@ import androidx.compose.runtime.Composable
 import io.element.android.features.location.api.Location
 import io.element.android.libraries.matrix.api.location.LiveLocationShare
 import org.maplibre.android.geometry.LatLng
+import org.ramani.compose.CircleWithItem
 
 @Composable
 fun LocationSymbol(item: LiveLocationShare, textColor: String) {
     val location = Location.fromGeoUri(item.lastLocation.location.geoUri) ?: return
     val latLng = LatLng(location.lat, location.lon)
 
-    CustomCircleWithItem(
+//    CustomCircleWithItem(
+//        center = latLng,
+//        radius = 10.0F,
+//        isDraggable = false,
+//        color = "#F6993A",
+//        text = item.userId.toString(),
+//        zIndex = 1,
+//        itemSize = 12F,
+//        borderColor = "#4A4A4A",
+//        borderWidth = 3.0F,
+//        imageId = null,
+//        textColor = textColor
+//    )
+    CircleWithItem(
         center = latLng,
         radius = 10.0F,
         isDraggable = false,
         color = "#F6993A",
-        text = item.userId.toString(),
+        text = "HE",
         zIndex = 1,
         itemSize = 12F,
         borderColor = "#4A4A4A",
         borderWidth = 3.0F,
         imageId = null,
-        textColor = textColor
     )
 }
