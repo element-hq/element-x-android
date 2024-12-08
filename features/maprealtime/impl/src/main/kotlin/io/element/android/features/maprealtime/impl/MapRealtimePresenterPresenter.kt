f6f7a06a3353ef560a5e4e3dc8008c38f6b7c39e
torrybr <16907963+torrybr@users.noreply.github.com>
2024-12-08 10:04:26 -0500

    stop foreground service

--- features/maprealtime/impl/src/main/kotlin/io/element/android/features/maprealtime/impl/MapRealtimePresenterPresenter.kt
+++ features/maprealtime/impl/src/main/kotlin/io/element/android/features/maprealtime/impl/MapRealtimePresenterPresenter.kt
@@ -143,6 +143,7 @@ class MapRealtimePresenterPresenter @Inject constructor(
                     scope.launch {
                         stopLiveLocationShare()
                     }
+                    LocationForegroundService.stop(context)
                 }
             }
         }
