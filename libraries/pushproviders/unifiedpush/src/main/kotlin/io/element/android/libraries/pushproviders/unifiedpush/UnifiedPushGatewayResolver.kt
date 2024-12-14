175a1234e5f63f65f488e49d69e598ea4bf6f73a
SpiritCroc <dev@spiritcroc.de>
2024-12-14 09:55:58 +0100

    Properly handle UnifiedPush distributors without matrix gateway again
    Fixes https://github.com/element-hq/element-x-android/issues/3571
    
    Co-authored-by: sim <git@sgougeon.fr>

--- libraries/pushproviders/unifiedpush/src/main/kotlin/io/element/android/libraries/pushproviders/unifiedpush/UnifiedPushGatewayResolver.kt
+++ libraries/pushproviders/unifiedpush/src/main/kotlin/io/element/android/libraries/pushproviders/unifiedpush/UnifiedPushGatewayResolver.kt
@@ -12,12 +12,13 @@ import io.element.android.libraries.core.coroutine.CoroutineDispatchers
 import io.element.android.libraries.core.data.tryOrNull
 import io.element.android.libraries.di.AppScope
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import timber.log.Timber
 import java.net.URL
 import javax.inject.Inject
 
 interface UnifiedPushGatewayResolver {
-    suspend fun getGateway(endpoint: String): String
+    suspend fun getGateway(endpoint: String, previousGateway: String?): String
 }
 
 @ContributesBinding(AppScope::class)
@@ -27,7 +28,7 @@ class DefaultUnifiedPushGatewayResolver @Inject constructor(
 ) : UnifiedPushGatewayResolver {
     private val logger = Timber.tag("DefaultUnifiedPushGatewayResolver")
 
-    override suspend fun getGateway(endpoint: String): String {
+    override suspend fun getGateway(endpoint: String, previousGateway: String?): String {
         val url = tryOrNull(
             onError = { logger.d(it, "Cannot parse endpoint as an URL") }
         ) {
@@ -47,14 +48,24 @@ class DefaultUnifiedPushGatewayResolver @Inject constructor(
                     val discoveryResponse = api.discover()
                     if (discoveryResponse.unifiedpush.gateway == "matrix") {
                         logger.d("The endpoint seems to be a valid UnifiedPush gateway")
+                        customUrl
                     } else {
-                        logger.e("The endpoint does not seem to be a valid UnifiedPush gateway")
+                        // The endpoint returned a 200 OK but didn't promote an actual matrix gateway, which means it doesn't have any
+                        logger.w("The endpoint does not seem to be a valid UnifiedPush gateway, using fallback")
+                        UnifiedPushConfig.DEFAULT_PUSH_GATEWAY_HTTP_URL
+                    }
+                } catch (exception: HttpException) {
+                    if (exception.code() == 404) {
+                        logger.i("Checking for UnifiedPush endpoint yielded 404, using fallback")
+                        UnifiedPushConfig.DEFAULT_PUSH_GATEWAY_HTTP_URL
+                    } else {
+                        logger.e(exception, "Error checking for UnifiedPush endpoint")
+                        previousGateway ?: customUrl
                     }
                 } catch (throwable: Throwable) {
                     logger.e(throwable, "Error checking for UnifiedPush endpoint")
+                    previousGateway ?: customUrl
                 }
-                // Always return the custom url.
-                customUrl
             }
         }
     }
