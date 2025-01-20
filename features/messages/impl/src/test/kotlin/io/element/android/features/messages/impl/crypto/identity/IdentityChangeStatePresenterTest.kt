e5c99e778bd501e3821b69a1c08b977516274bcb
Valere <bill.carson@valrsoft.com>
2025-01-20 08:56:49 +0100

    Quick test for verification violation event

--- features/messages/impl/src/test/kotlin/io/element/android/features/messages/impl/crypto/identity/IdentityChangeStatePresenterTest.kt
+++ features/messages/impl/src/test/kotlin/io/element/android/features/messages/impl/crypto/identity/IdentityChangeStatePresenterTest.kt
@@ -143,7 +143,22 @@ class IdentityChangeStatePresenterTest {
             val presenter = createIdentityChangeStatePresenter(encryptionService = encryptionService)
             presenter.test {
                 val initialState = awaitItem()
-                initialState.eventSink(IdentityChangeEvent.Submit(A_USER_ID))
+                initialState.eventSink(IdentityChangeEvent.PinViolation(A_USER_ID))
+                lambda.assertions().isCalledOnce().with(value(A_USER_ID))
+            }
+        }
+
+    @Test
+    fun `present - when the user withdraw the identity, the presenter invokes the encryption service api`() =
+        runTest {
+            val lambda = lambdaRecorder<UserId, Result<Unit>> { Result.success(Unit) }
+            val encryptionService = FakeEncryptionService(
+                withdrawVerificationResult = lambda,
+            )
+            val presenter = createIdentityChangeStatePresenter(encryptionService = encryptionService)
+            presenter.test {
+                val initialState = awaitItem()
+                initialState.eventSink(IdentityChangeEvent.VerificationViolation(A_USER_ID))
                 lambda.assertions().isCalledOnce().with(value(A_USER_ID))
             }
         }
