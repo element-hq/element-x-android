e5ed0a0641367ed5a45dae8cb9d8e899d850f409
Benoit Marty <benoit@matrix.org>
2025-02-19 17:10:30 +0100

    Move build parameters to a common file

---
+++ plugins/src/main/kotlin/config/BuildTimeConfig.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2025 New Vector Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package config
+
+object BuildTimeConfig {
+    const val APPLICATION_ID = "io.element.android.x"
+    const val APPLICATION_NAME = "Element X"
+    const val GOOGLE_APP_ID_RELEASE = "1:912726360885:android:d097de99a4c23d2700427c"
+    const val GOOGLE_APP_ID_DEBUG = "1:912726360885:android:def0a4e454042e9b00427c"
+    const val GOOGLE_APP_ID_NIGHTLY = "1:912726360885:android:e17435e0beb0303000427c"
+}
