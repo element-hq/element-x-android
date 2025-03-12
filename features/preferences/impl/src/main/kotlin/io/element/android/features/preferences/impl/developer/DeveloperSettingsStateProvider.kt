0df0d9f7641da21cc948c412f1301a38b2c3c50d
Jorge Martín <jorgem@element.io>
2025-03-12 11:42:11 +0100

    Fix EC base url developer settings item and its tests

--- features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/developer/DeveloperSettingsStateProvider.kt
+++ features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/developer/DeveloperSettingsStateProvider.kt
@@ -47,10 +47,8 @@ fun aDeveloperSettingsState(
 
 fun aCustomElementCallBaseUrlState(
     baseUrl: String? = null,
-    defaultUrl: String = "https://call.element.io",
     validator: (String?) -> Boolean = { true },
 ) = CustomElementCallBaseUrlState(
     baseUrl = baseUrl,
-    defaultUrl = defaultUrl,
     validator = validator,
 )
