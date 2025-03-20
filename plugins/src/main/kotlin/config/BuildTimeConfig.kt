7c668f5035ac27454a46ddca37d952b04dd04559
Benoit Marty <benoit@matrix.org>
2025-03-20 15:09:46 +0100

    Let element enterprise configure the ids for maptiler service.

--- plugins/src/main/kotlin/config/BuildTimeConfig.kt
+++ plugins/src/main/kotlin/config/BuildTimeConfig.kt
@@ -13,4 +13,8 @@ object BuildTimeConfig {
     const val GOOGLE_APP_ID_RELEASE = "1:912726360885:android:d097de99a4c23d2700427c"
     const val GOOGLE_APP_ID_DEBUG = "1:912726360885:android:def0a4e454042e9b00427c"
     const val GOOGLE_APP_ID_NIGHTLY = "1:912726360885:android:e17435e0beb0303000427c"
+
+    val SERVICES_MAPTILER_APIKEY: String? = null
+    val SERVICES_MAPTILER_LIGHT_MAPID: String? = null
+    val SERVICES_MAPTILER_DARK_MAPID: String? = null
 }
