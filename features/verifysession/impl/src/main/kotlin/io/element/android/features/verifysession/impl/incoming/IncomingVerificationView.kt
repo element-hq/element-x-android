2a228add6c9ee895ddcac092b54d089f2cea7171
Jorge Martín <jorgem@element.io>
2025-03-21 09:00:20 +0100

    Apply the same changes to `IncomingVerificationView`

--- features/verifysession/impl/src/main/kotlin/io/element/android/features/verifysession/impl/incoming/IncomingVerificationView.kt
+++ features/verifysession/impl/src/main/kotlin/io/element/android/features/verifysession/impl/incoming/IncomingVerificationView.kt
@@ -13,9 +13,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -67,7 +69,8 @@ fun IncomingVerificationView(
                         step is Step.Completed -> Unit
                         else -> BackButton(onClick = { state.eventSink(IncomingVerificationViewEvents.GoBack) })
                     }
-                }
+                },
+                colors = topAppBarColors(containerColor = Color.Transparent),
             )
         },
         header = {
@@ -77,7 +80,8 @@ fun IncomingVerificationView(
             IncomingVerificationBottomMenu(
                 state = state,
             )
-        }
+        },
+        isScrollable = true,
     ) {
         IncomingVerificationContent(
             step = step,
