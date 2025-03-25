4c4c74ec4d0ec03f95a0f4e38eb644aa5961dcea
Benoit Marty <benoit@matrix.org>
2025-03-25 14:54:24 +0100

    Add handy extension "VariantDimension.buildConfigFieldStr"

---
+++ plugins/src/main/kotlin/extension/VariantDimensionExtension.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2025 New Vector Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package extension
+
+import com.android.build.api.dsl.VariantDimension
+
+fun VariantDimension.buildConfigFieldStr(
+    name: String,
+    value: String,
+) {
+    buildConfigField(
+        type = "String",
+        name = name,
+        value = "\"$value\""
+    )
+}
