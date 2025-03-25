39d40a9da82704d553d003213ece3b980b5677de
Benoit Marty <benoit@matrix.org>
2025-03-25 15:09:15 +0100

    Build AnalyticsConfig depending on analytics configuration.

--- plugins/src/main/kotlin/ModulesConfig.kt
+++ plugins/src/main/kotlin/ModulesConfig.kt
@@ -6,6 +6,7 @@
  */
 
 import config.AnalyticsConfig
+import config.BuildTimeConfig
 import config.PushProvidersConfig
 
 object ModulesConfig {
@@ -14,8 +15,24 @@ object ModulesConfig {
         includeUnifiedPush = true,
     )
 
-    val analyticsConfig: AnalyticsConfig = AnalyticsConfig.Enabled(
-        withPosthog = true,
-        withSentry = true,
-    )
+    val analyticsConfig: AnalyticsConfig = if (isEnterpriseBuild) {
+        // Is Posthog configuration available?
+        val withPosthog = BuildTimeConfig.SERVICES_POSTHOG_APIKEY.isNullOrEmpty().not() &&
+            BuildTimeConfig.SERVICES_POSTHOG_HOST.isNullOrEmpty().not()
+        // Is Sentry configuration available?
+        val withSentry = BuildTimeConfig.SERVICES_SENTRY_DSN.isNullOrEmpty().not()
+        if (withPosthog || withSentry) {
+            AnalyticsConfig.Enabled(
+                withPosthog = withPosthog,
+                withSentry = withSentry,
+            )
+        } else {
+            AnalyticsConfig.Disabled
+        }
+    } else {
+        AnalyticsConfig.Enabled(
+            withPosthog = true,
+            withSentry = true,
+        )
+    }
 }
