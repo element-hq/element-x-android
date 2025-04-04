589b25dc784f64c211c22c7e95d050e817f8e749
Benoit Marty <benoit@matrix.org>
2025-04-04 15:22:38 +0200

    Update icon preview code.

--- appicon/element/src/main/kotlin/io/element/android/appicon/element/IconPreview.kt
+++ appicon/element/src/main/kotlin/io/element/android/appicon/element/IconPreview.kt
@@ -10,25 +10,28 @@ package io.element.android.appicon.element
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 internal fun IconPreview() {
     Box {
-        Image(painter = painterResource(id = R.mipmap.ic_launcher_background), contentDescription = null)
-        Image(painter = painterResource(id = R.mipmap.ic_launcher_foreground), contentDescription = null)
+        Image(
+            modifier = Modifier.matchParentSize(),
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+        )
+        Image(
+            painter = painterResource(id = R.mipmap.ic_launcher_foreground),
+            contentDescription = null,
+        )
     }
 }
 
@@ -36,8 +39,15 @@ internal fun IconPreview() {
 @Composable
 internal fun RoundIconPreview() {
     Box(modifier = Modifier.clip(shape = CircleShape)) {
-        Image(painter = painterResource(id = R.mipmap.ic_launcher_background), contentDescription = null)
-        Image(painter = painterResource(id = R.mipmap.ic_launcher_foreground), contentDescription = null)
+        Image(
+            modifier = Modifier.matchParentSize(),
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+        )
+        Image(
+            painter = painterResource(id = R.mipmap.ic_launcher_foreground),
+            contentDescription = null,
+        )
     }
 }
 
@@ -46,10 +56,7 @@ internal fun RoundIconPreview() {
 internal fun MonochromeIconPreview() {
     Box(
         modifier = Modifier
-            .size(108.dp)
-            .background(Color(0xFF2F3133))
-            .clip(shape = RoundedCornerShape(32.dp)),
-        contentAlignment = Alignment.Center
+            .background(Color(0xFF2F3133)),
     ) {
         Image(
             painter = painterResource(id = R.mipmap.ic_launcher_monochrome),
