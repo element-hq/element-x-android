d16a4160d9b14a84c119e20a546bfb7f956564d7
Benoit Marty <benoit@matrix.org>
2025-04-04 12:14:13 +0200

    Simplify test

--- tests/konsist/src/test/kotlin/io/element/android/tests/konsist/KonsistFlowTest.kt
+++ tests/konsist/src/test/kotlin/io/element/android/tests/konsist/KonsistFlowTest.kt
@@ -10,7 +10,7 @@ package io.element.android.tests.konsist
 import androidx.compose.runtime.Composable
 import com.lemonappdev.konsist.api.Konsist
 import com.lemonappdev.konsist.api.ext.list.withAnnotationOf
-import com.lemonappdev.konsist.api.verify.assertTrue
+import com.lemonappdev.konsist.api.verify.assertFalse
 import org.junit.Test
 
 class KonsistFlowTest {
@@ -23,18 +23,15 @@ class KonsistFlowTest {
         //     .collectAsState```
         val regex = "(.*)\\)(\n\\s*)*\\.collectAsState".toRegex()
 
-        val allowedMethods = emptyList<String>()
         Konsist
             .scopeFromProject()
             .functions()
             .withAnnotationOf(Composable::class)
-            .assertTrue(
+            .assertFalse(
                 additionalMessage = "Please check that the flow is remembered when it is collected as state." +
-                    " If the returned flow is backed up by a val, you can add an exception to this test.",
+                    " Only val flows can be not remembered.",
             ) { function ->
-                val result = regex.find(function.text) ?: return@assertTrue true
-                val functionLine = result.groups[0]?.value
-                allowedMethods.any { functionLine?.contains(it) == true }
+                regex.matches(function.text)
             }
     }
 }
