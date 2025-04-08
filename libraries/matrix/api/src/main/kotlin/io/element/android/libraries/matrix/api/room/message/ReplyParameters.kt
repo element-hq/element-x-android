a0c677e4b6e47568e9b5faaa1f41e9286f11be16
Jorge Martín <jorgem@element.io>
2025-04-08 11:04:55 +0200

    Try to improve the usage of `ReplyParameters`

--- libraries/matrix/api/src/main/kotlin/io/element/android/libraries/matrix/api/room/message/ReplyParameters.kt
+++ libraries/matrix/api/src/main/kotlin/io/element/android/libraries/matrix/api/room/message/ReplyParameters.kt
@@ -9,25 +9,14 @@ package io.element.android.libraries.matrix.api.room.message
 
 import io.element.android.libraries.matrix.api.core.EventId
 
-/*
- * Copyright 2025 New Vector Ltd.
- *
- * SPDX-License-Identifier: AGPL-3.0-only
- * Please see LICENSE in the repository root for full details.
- */
-
 data class ReplyParameters(
     val inReplyToEventId: EventId,
     val enforceThreadReply: Boolean,
     val replyWithinThread: Boolean,
 )
 
-fun inReplyTo(
-    eventId: EventId,
-    enforceThreadReply: Boolean = false,
-    replyWithinThread: Boolean = false,
-): ReplyParameters = ReplyParameters(
+fun replyInThread(eventId: EventId, explicitReply: Boolean = false) = ReplyParameters(
     inReplyToEventId = eventId,
-    enforceThreadReply = enforceThreadReply,
-    replyWithinThread = replyWithinThread,
+    enforceThreadReply = true,
+    replyWithinThread = explicitReply,
 )
