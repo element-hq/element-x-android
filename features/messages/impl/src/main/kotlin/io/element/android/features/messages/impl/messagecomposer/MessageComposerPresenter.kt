dbd76cd1f00510bc9823cb0d8476836008cb69d4
Jorge Martín <jorgem@element.io>
2025-04-08 11:42:34 +0200

    Use the new function to reply to messages in the room

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/messagecomposer/MessageComposerPresenter.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/messagecomposer/MessageComposerPresenter.kt
@@ -53,7 +53,7 @@ import io.element.android.libraries.matrix.api.room.MatrixRoom
 import io.element.android.libraries.matrix.api.room.draft.ComposerDraft
 import io.element.android.libraries.matrix.api.room.draft.ComposerDraftType
 import io.element.android.libraries.matrix.api.room.isDm
-import io.element.android.libraries.matrix.api.room.message.inReplyTo
+import io.element.android.libraries.matrix.api.room.message.ReplyParameters
 import io.element.android.libraries.matrix.api.timeline.TimelineException
 import io.element.android.libraries.matrix.api.timeline.item.event.toEventOrTransactionId
 import io.element.android.libraries.matrix.ui.messages.reply.InReplyToDetails
@@ -458,7 +458,12 @@ class MessageComposerPresenter @AssistedInject constructor(
                             body = message.markdown,
                             htmlBody = message.html,
                             intentionalMentions = message.intentionalMentions,
-                            replyParameters = inReplyTo(eventId = eventId, enforceThreadReply = inThread, replyWithinThread = inThread),
+                            replyParameters = ReplyParameters(
+                                inReplyToEventId = eventId,
+                                enforceThreadReply = inThread,
+                                // This should be false until we add a way to make a reply in a thread an explicit reply to the provided eventId
+                                replyWithinThread = false,
+                            ),
                         )
                     }
                 }
