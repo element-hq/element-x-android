0628b780e11b30d6970265264d54621fae78eee6
Jorge Martín <jorgem@element.io>
2025-05-06 15:57:37 +0200

    Setting version for the release 25.05.0

--- plugins/src/main/kotlin/Versions.kt
+++ plugins/src/main/kotlin/Versions.kt
@@ -29,10 +29,10 @@ import org.gradle.jvm.toolchain.JavaLanguageVersion
  */
 
 private const val versionYear = 25
-private const val versionMonth = 4
+private const val versionMonth = 5
 
 // Note: must be in [0,99]
-private const val versionReleaseNumber = 3
+private const val versionReleaseNumber = 0
 
 object Versions {
     const val VERSION_CODE = (2000 + versionYear) * 10_000 + versionMonth * 100 + versionReleaseNumber
