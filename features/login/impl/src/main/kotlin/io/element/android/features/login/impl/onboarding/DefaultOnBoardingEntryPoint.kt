4d0f8947b161edbfce3895b76601f9a9cb55bdb1
Benoit Marty <benoit@matrix.org>
2025-05-19 20:37:37 +0200

    Move onboarding code to the login module.

---
+++ features/login/impl/src/main/kotlin/io/element/android/features/login/impl/onboarding/DefaultOnBoardingEntryPoint.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2023, 2024 New Vector Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.features.login.impl.onboarding
+
+import com.bumble.appyx.core.modality.BuildContext
+import com.bumble.appyx.core.node.Node
+import com.bumble.appyx.core.plugin.Plugin
+import com.squareup.anvil.annotations.ContributesBinding
+import io.element.android.features.login.api.OnBoardingEntryPoint
+import io.element.android.libraries.architecture.createNode
+import io.element.android.libraries.di.AppScope
+import javax.inject.Inject
+
+@ContributesBinding(AppScope::class)
+class DefaultOnBoardingEntryPoint @Inject constructor() : OnBoardingEntryPoint {
+    override fun nodeBuilder(parentNode: Node, buildContext: BuildContext): OnBoardingEntryPoint.NodeBuilder {
+        return object : OnBoardingEntryPoint.NodeBuilder {
+            val plugins = ArrayList<Plugin>()
+
+            override fun callback(callback: OnBoardingEntryPoint.Callback): OnBoardingEntryPoint.NodeBuilder {
+                plugins += callback
+                return this
+            }
+
+            override fun build(): Node {
+                return parentNode.createNode<OnBoardingNode>(buildContext, plugins)
+            }
+        }
+    }
+}
---
+++ features/login/impl/src/test/kotlin/io/element/android/features/login/impl/onboarding/OnBoardingPresenterTest.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2023, 2024 New Vector Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.features.login.impl.onboarding
+
+import app.cash.molecule.RecompositionMode
+import app.cash.molecule.moleculeFlow
+import app.cash.turbine.test
+import com.google.common.truth.Truth.assertThat
+import io.element.android.appconfig.OnBoardingConfig
+import io.element.android.libraries.featureflag.api.FeatureFlags
+import io.element.android.libraries.featureflag.test.FakeFeatureFlagService
+import io.element.android.libraries.matrix.test.core.aBuildMeta
+import io.element.android.tests.testutils.WarmUpRule
+import io.element.android.tests.testutils.test
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
+import org.junit.Test
+
+class OnBoardingPresenterTest {
+    @get:Rule
+    val warmUpRule = WarmUpRule()
+
+    @Test
+    fun `present - initial state`() = runTest {
+        val buildMeta = aBuildMeta(
+            applicationName = "A",
+            productionApplicationName = "B",
+            desktopApplicationName = "C",
+        )
+        val featureFlagService = FakeFeatureFlagService(
+            initialState = mapOf(FeatureFlags.QrCodeLogin.key to true),
+            buildMeta = buildMeta,
+        )
+        val presenter = OnBoardingPresenter(
+            buildMeta = buildMeta,
+            featureFlagService = featureFlagService,
+            rageshakeFeatureAvailability = { true },
+        )
+        moleculeFlow(RecompositionMode.Immediate) {
+            presenter.present()
+        }.test {
+            val initialState = awaitItem()
+            assertThat(initialState.canLoginWithQrCode).isFalse()
+            assertThat(initialState.productionApplicationName).isEqualTo("B")
+            assertThat(initialState.canCreateAccount).isEqualTo(OnBoardingConfig.CAN_CREATE_ACCOUNT)
+            assertThat(initialState.canReportBug).isTrue()
+            assertThat(awaitItem().canLoginWithQrCode).isTrue()
+        }
+    }
+
+    @Test
+    fun `present - rageshake not available`() = runTest {
+        val presenter = OnBoardingPresenter(
+            buildMeta = aBuildMeta(),
+            featureFlagService = FakeFeatureFlagService(),
+            rageshakeFeatureAvailability = { false },
+        )
+        presenter.test {
+            skipItems(1)
+            assertThat(awaitItem().canReportBug).isFalse()
+        }
+    }
+}
