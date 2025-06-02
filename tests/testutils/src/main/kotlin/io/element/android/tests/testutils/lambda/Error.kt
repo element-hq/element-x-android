2c20f17220f25e7689a5bc408b055a393247a7aa
Jorge Martín <jorgem@element.io>
2025-06-02 12:54:58 +0200

    Remove unnecessary comment on `lambdaError`

--- tests/testutils/src/main/kotlin/io/element/android/tests/testutils/lambda/Error.kt
+++ tests/testutils/src/main/kotlin/io/element/android/tests/testutils/lambda/Error.kt
@@ -10,10 +10,5 @@ package io.element.android.tests.testutils.lambda
 fun lambdaError(
     message: String = "This lambda should never be called."
 ): Nothing {
-    // Throwing an exception here is not enough, it can be caught.
-    // Instead exit the process to make sure the test fails.
-    // The error will be:
-    // "Could not stop all services."
-    // In this case, put a breakpoint here and run the test in debug mode to identify which lambda is failing.
     throw AssertionError(message)
 }
