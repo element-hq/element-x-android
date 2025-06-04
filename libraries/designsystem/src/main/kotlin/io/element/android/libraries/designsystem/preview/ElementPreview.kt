68d3de7c98d1ebc65ac4c567e3014c46a91bc707
Benoit Marty <benoit@matrix.org>
2025-06-04 17:34:37 +0200

    Fix compilation issue. Need to provide an image for preview.
    https://coil-kt.github.io/coil/compose/#previews

--- libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/preview/ElementPreview.kt
+++ libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/preview/ElementPreview.kt
@@ -10,10 +10,14 @@ package io.element.android.libraries.designsystem.preview
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.res.ResourcesCompat
 import coil3.annotation.ExperimentalCoilApi
+import coil3.asImage
 import coil3.compose.AsyncImagePreviewHandler
 import coil3.compose.LocalAsyncImagePreviewHandler
 import io.element.android.compound.theme.ElementTheme
+import io.element.android.libraries.designsystem.R
 import io.element.android.libraries.designsystem.theme.components.Surface
 
 @OptIn(ExperimentalCoilApi::class)
@@ -24,7 +28,12 @@ fun ElementPreview(
     showBackground: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    CompositionLocalProvider(LocalAsyncImagePreviewHandler provides AsyncImagePreviewHandler { null }) {
+    val context = LocalContext.current
+    CompositionLocalProvider(
+        LocalAsyncImagePreviewHandler provides AsyncImagePreviewHandler {
+            ResourcesCompat.getDrawable(context.resources, R.drawable.sample_background, null)!!.asImage()
+        }
+    ) {
         ElementTheme(darkTheme = darkTheme) {
             if (showBackground) {
                 // If we have a proper contentColor applied we need a Surface instead of a Box
