12fdb8e875a517a6b7342cf2183d0c2e06f3d232
Jorge Martin Espinosa <jorgem@element.io>
2025-06-20 14:19:33 +0200

    Add scripts to enable and disable talkback (#4906)

---
+++ tools/adb/enable_talkback.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Copyright 2025 New Vector Ltd.
+#
+# SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+# Please see LICENSE files in the repository root for full details.
+
+adb shell settings put secure enabled_accessibility_services com.google.android.marvin.talkback/com.google.android.marvin.talkback.TalkBackService
