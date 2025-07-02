ab51ddda1c3e928680af1f10f163259274f10e2a
Benoit Marty <benoit@matrix.org>
2025-07-02 13:29:22 +0200

    Improve test coverage of PinnedMessagesBannerState

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/pinned/banner/PinnedMessagesBannerState.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/pinned/banner/PinnedMessagesBannerState.kt
@@ -10,14 +10,30 @@ package io.element.android.features.messages.impl.pinned.banner
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import io.element.android.libraries.designsystem.text.toAnnotatedString
 import io.element.android.libraries.ui.strings.CommonStrings
 
 @Immutable
 sealed interface PinnedMessagesBannerState {
     data object Hidden : PinnedMessagesBannerState
-    sealed interface Visible : PinnedMessagesBannerState
+    sealed interface Visible : PinnedMessagesBannerState {
+        fun pinnedMessagesCount() = when (this) {
+            is Loading -> expectedPinnedMessagesCount
+            is Loaded -> loadedPinnedMessagesCount
+        }
+
+        fun currentPinnedMessageIndex() = when (this) {
+            is Loading -> expectedPinnedMessagesCount - 1
+            is Loaded -> currentPinnedMessageIndex
+        }
+
+        @Composable
+        fun formattedMessage() = when (this) {
+            is Loading -> stringResource(id = CommonStrings.screen_room_pinned_banner_loading_description).toAnnotatedString()
+            is Loaded -> currentPinnedMessage.formatted
+        }
+    }
+
     data class Loading(val expectedPinnedMessagesCount: Int) : Visible
     data class Loaded(
         val currentPinnedMessage: PinnedMessagesBannerItem,
@@ -25,23 +41,4 @@ sealed interface PinnedMessagesBannerState {
         val loadedPinnedMessagesCount: Int,
         val eventSink: (PinnedMessagesBannerEvents) -> Unit
     ) : Visible
-
-    fun pinnedMessagesCount() = when (this) {
-        is Hidden -> 0
-        is Loading -> expectedPinnedMessagesCount
-        is Loaded -> loadedPinnedMessagesCount
-    }
-
-    fun currentPinnedMessageIndex() = when (this) {
-        is Hidden -> 0
-        is Loading -> expectedPinnedMessagesCount - 1
-        is Loaded -> currentPinnedMessageIndex
-    }
-
-    @Composable
-    fun formattedMessage() = when (this) {
-        is Hidden -> AnnotatedString("")
-        is Loading -> stringResource(id = CommonStrings.screen_room_pinned_banner_loading_description).toAnnotatedString()
-        is Loaded -> currentPinnedMessage.formatted
-    }
 }
