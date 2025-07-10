28e90d6fd547ed6caec4261ca4aa5fb5ed832e1a
Jorge Martín <jorgem@element.io>
2025-07-10 15:12:55 +0200

    Improve the title of the feature flag

--- libraries/featureflag/api/src/main/kotlin/io/element/android/libraries/featureflag/api/FeatureFlags.kt
+++ libraries/featureflag/api/src/main/kotlin/io/element/android/libraries/featureflag/api/FeatureFlags.kt
@@ -177,7 +177,7 @@ enum class FeatureFlags(
     ),
     SharePos(
         key = "feature.share_pos",
-        title = "Share pos",
+        title = "Share pos in sliding sync",
         description = "Keep the sliding sync pos to make initial syncs faster. Requires an app restart to take effect." +
             "\n\nWARNING: this may cause issues with syncs.",
         defaultValue = { false },
