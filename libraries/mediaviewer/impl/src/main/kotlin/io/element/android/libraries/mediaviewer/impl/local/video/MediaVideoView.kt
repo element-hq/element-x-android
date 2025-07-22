0cbcfc7e7883cca6a5c1ce35b53eb77b99d7ad3e
Jorge Martín <jorgem@element.io>
2025-07-22 14:19:34 +0200

    Remove `onVideoSizeChanged` completely

--- libraries/mediaviewer/impl/src/main/kotlin/io/element/android/libraries/mediaviewer/impl/local/video/MediaVideoView.kt
+++ libraries/mediaviewer/impl/src/main/kotlin/io/element/android/libraries/mediaviewer/impl/local/video/MediaVideoView.kt
@@ -33,7 +33,6 @@ import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.common.Player.STATE_READY
 import androidx.media3.common.Timeline
-import androidx.media3.common.VideoSize
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
@@ -55,7 +54,6 @@ import io.element.android.libraries.mediaviewer.impl.local.player.seekToEnsurePl
 import io.element.android.libraries.mediaviewer.impl.local.player.togglePlay
 import io.element.android.libraries.mediaviewer.impl.local.rememberLocalMediaViewState
 import kotlinx.coroutines.delay
-import me.saket.telephoto.zoomable.ZoomableContentLocation
 import me.saket.telephoto.zoomable.zoomable
 import kotlin.time.Duration.Companion.seconds
 
@@ -151,13 +149,6 @@ private fun ExoPlayerMediaVideoView(
             override fun onPlaybackStateChanged(playbackState: Int) {
                 mediaPlayerControllerState = mediaPlayerControllerState.copy(
                     isReady = playbackState == STATE_READY,
-                )
-            }
-
-            override fun onVideoSizeChanged(videoSize: VideoSize) {
-                // Ensure that the user cannot zoom/move outside of the video bounds
-                localMediaViewState.zoomableState.setContentLocation(
-                    ZoomableContentLocation.SameAsLayoutBounds
                 )
             }
         }
