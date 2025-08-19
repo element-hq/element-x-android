81d82fb0de86bc57c47d121441ac6e72def75cc0
Benoit Marty <benoit@matrix.org>
2025-08-19 19:06:05 +0200

    Space List
    Extract InviteButtonsRowMolecule
    Take into account seenSpaceInvites

--- libraries/matrix/impl/src/main/kotlin/io/element/android/libraries/matrix/impl/RustMatrixClient.kt
+++ libraries/matrix/impl/src/main/kotlin/io/element/android/libraries/matrix/impl/RustMatrixClient.kt
@@ -42,6 +42,7 @@ import io.element.android.libraries.matrix.api.room.join.JoinRule
 import io.element.android.libraries.matrix.api.roomdirectory.RoomDirectoryService
 import io.element.android.libraries.matrix.api.roomdirectory.RoomVisibility
 import io.element.android.libraries.matrix.api.roomlist.RoomListService
+import io.element.android.libraries.matrix.api.spaces.SpaceService
 import io.element.android.libraries.matrix.api.sync.SlidingSyncVersion
 import io.element.android.libraries.matrix.api.sync.SyncService
 import io.element.android.libraries.matrix.api.sync.SyncState
@@ -71,6 +72,7 @@ import io.element.android.libraries.matrix.impl.roomdirectory.map
 import io.element.android.libraries.matrix.impl.roomlist.RoomListFactory
 import io.element.android.libraries.matrix.impl.roomlist.RustRoomListService
 import io.element.android.libraries.matrix.impl.roomlist.roomOrNull
+import io.element.android.libraries.matrix.impl.spaces.RustSpaceService
 import io.element.android.libraries.matrix.impl.sync.RustSyncService
 import io.element.android.libraries.matrix.impl.sync.map
 import io.element.android.libraries.matrix.impl.usersearch.UserProfileMapper
@@ -143,6 +145,7 @@ class RustMatrixClient(
     private val sessionDispatcher = dispatchers.io.limitedParallelism(64)
 
     private val innerRoomListService = innerSyncService.roomListService()
+    private val innerSpaceService = innerClient.spaceService()
 
     private val rustSyncService = RustSyncService(
         inner = innerSyncService,
@@ -182,6 +185,17 @@ class RustMatrixClient(
             sessionCoroutineScope = sessionCoroutineScope,
         ),
         roomSyncSubscriber = roomSyncSubscriber,
+    )
+
+    override val spaceService: SpaceService = RustSpaceService(
+        innerSpaceService = innerSpaceService,
+        sessionCoroutineScope = sessionCoroutineScope,
+        sessionDispatcher = sessionDispatcher,
+        //roomListFactory = RoomListFactory(
+        //    innerRoomListService = innerRoomListService,
+        //    sessionCoroutineScope = sessionCoroutineScope,
+        //),
+        //roomSyncSubscriber = roomSyncSubscriber,
     )
 
     private val verificationService = RustSessionVerificationService(
@@ -540,6 +554,7 @@ class RustMatrixClient(
 
         sessionDelegate.clearCurrentClient()
         innerRoomListService.close()
+        innerSpaceService.close()
         notificationService.close()
         encryptionService.close()
         innerClient.close()
