629776b9a3bac0df3ca21c2b47d5aa8daae8af6f
Jorge Martín <jorgem@element.io>
2025-08-25 09:17:34 +0200

    Fix module still using dagger annotation

--- libraries/featureflag/impl/src/main/kotlin/io/element/android/libraries/featureflag/impl/di/FeatureFlagModule.kt
+++ libraries/featureflag/impl/src/main/kotlin/io/element/android/libraries/featureflag/impl/di/FeatureFlagModule.kt
@@ -7,10 +7,10 @@
 
 package io.element.android.libraries.featureflag.impl.di
 
-import dagger.multibindings.ElementsIntoSet
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.BindingContainer
 import dev.zacsweers.metro.ContributesTo
+import dev.zacsweers.metro.ElementsIntoSet
 import dev.zacsweers.metro.Provides
 import io.element.android.libraries.featureflag.impl.FeatureFlagProvider
 import io.element.android.libraries.featureflag.impl.PreferencesFeatureFlagProvider
