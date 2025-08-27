0859fc3fcdde9add688746268e86ca5c734d0843
Benoit Marty <benoit@matrix.org>
2025-08-27 10:40:35 +0200

    Multi accounts - ignore automatic GoBack in case of error.

--- features/login/impl/src/main/kotlin/io/element/android/features/login/impl/login/LoginHelper.kt
+++ features/login/impl/src/main/kotlin/io/element/android/features/login/impl/login/LoginHelper.kt
@@ -94,6 +94,11 @@ class LoginHelper(
     }
 
     private suspend fun onOidcAction(oidcAction: OidcAction) {
+        if (oidcAction is OidcAction.GoBack && loginModeState.value !is AsyncData.Loading) {
+            // Ignore GoBack action if the current state is not Loading. This GoBack action is coming from LoginFlowNode.
+            // This can happen if there is an error, for instance attempt to login again on the same account.
+            return
+        }
         loginModeState.value = AsyncData.Loading()
         when (oidcAction) {
             OidcAction.GoBack -> {
