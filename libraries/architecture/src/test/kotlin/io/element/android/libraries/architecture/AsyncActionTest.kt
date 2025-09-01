150d0790bc5f0d5ceea5611e8e56b8a01f378664
João Breno <joao.breno.rodrigues07@aluno.ifce.edu.br>
2025-09-01 10:07:32 -0300

    Update state in runUpdatingState when CancellationException occurs (#5243)

---
+++ libraries/architecture/src/test/kotlin/io/element/android/libraries/architecture/AsyncActionTest.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 New Vector Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.libraries.architecture
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withTimeout
+import org.junit.Assert.assertSame
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+import org.junit.Test
+import kotlin.time.Duration.Companion.milliseconds
+
+class AsyncActionTest {
+    @Test
+    fun `updates state on timeout`() = runTest {
+        val state: MutableState<AsyncAction<Int>> = mutableStateOf(AsyncAction.Uninitialized)
+        val timeoutMillis = 500L
+        val operationTimeMillis = 1000L
+
+        try {
+            runUpdatingState(state = state) {
+                withTimeout(timeoutMillis.milliseconds) {
+                    delay(operationTimeMillis)
+                }
+                Result.success(0)
+            }
+            fail("Expected TimeoutCancellationException, but nothing was thrown")
+        } catch (e: TimeoutCancellationException) {
+            assertTrue(state.value.isFailure())
+            assertSame(e, state.value.errorOrNull())
+        }
+    }
+}
