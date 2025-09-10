e2278853457f0a3be57ddd5503b2965cce47e4e0
Benoit Marty <benoit@matrix.org>
2025-09-10 14:58:18 +0200

    More compilation fixes.

--- features/space/impl/src/main/kotlin/io/element/android/features/space/impl/DefaultSpaceEntryPoint.kt
+++ features/space/impl/src/main/kotlin/io/element/android/features/space/impl/DefaultSpaceEntryPoint.kt
@@ -11,13 +11,14 @@ import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 import com.bumble.appyx.core.plugin.Plugin
 import dev.zacsweers.metro.ContributesBinding
+import dev.zacsweers.metro.Inject
 import io.element.android.features.space.api.SpaceEntryPoint
 import io.element.android.libraries.architecture.createNode
 import io.element.android.libraries.di.SessionScope
-import javax.inject.Inject
 
 @ContributesBinding(SessionScope::class)
-class DefaultSpaceEntryPoint @Inject constructor() : SpaceEntryPoint {
+@Inject
+class DefaultSpaceEntryPoint : SpaceEntryPoint {
     override fun nodeBuilder(parentNode: Node, buildContext: BuildContext): SpaceEntryPoint.NodeBuilder {
         val plugins = mutableSetOf<Plugin>()
         return object : SpaceEntryPoint.NodeBuilder {
--- features/space/impl/src/main/kotlin/io/element/android/features/space/impl/SpaceNode.kt
+++ features/space/impl/src/main/kotlin/io/element/android/features/space/impl/SpaceNode.kt
@@ -12,14 +12,15 @@ import androidx.compose.ui.Modifier
 import com.bumble.appyx.core.modality.BuildContext
 import com.bumble.appyx.core.node.Node
 import com.bumble.appyx.core.plugin.Plugin
-import dagger.assisted.Assisted
-import dagger.assisted.AssistedInject
-import io.element.android.anvilannotations.ContributesNode
+import dev.zacsweers.metro.Assisted
+import dev.zacsweers.metro.Inject
+import io.element.android.annotations.ContributesNode
 import io.element.android.features.space.api.SpaceEntryPoint
 import io.element.android.libraries.di.SessionScope
 
 @ContributesNode(SessionScope::class)
-class SpaceNode @AssistedInject constructor(
+@Inject
+class SpaceNode(
     @Assisted buildContext: BuildContext,
     @Assisted plugins: List<Plugin>,
     presenterFactory: SpacePresenter.Factory,
