386f07a06d55a0e8895668950c2c323410ecb56e
Richard van der Hoff <richard@matrix.org>
2025-09-11 18:16:43 +0100

    Show progress dialog while invites are being sent

--- features/roomdetails/impl/src/main/kotlin/io/element/android/features/roomdetails/impl/invite/RoomInviteMembersView.kt
+++ features/roomdetails/impl/src/main/kotlin/io/element/android/features/roomdetails/impl/invite/RoomInviteMembersView.kt
@@ -8,22 +8,29 @@
 package io.element.android.features.roomdetails.impl.invite
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import io.element.android.compound.theme.ElementTheme
 import io.element.android.features.invitepeople.api.InvitePeopleEvents
 import io.element.android.features.invitepeople.api.InvitePeopleState
 import io.element.android.features.invitepeople.api.InvitePeopleStateProvider
 import io.element.android.features.roomdetails.impl.R
+import io.element.android.libraries.designsystem.components.ProgressDialog
 import io.element.android.libraries.designsystem.components.button.BackButton
 import io.element.android.libraries.designsystem.preview.ElementPreview
 import io.element.android.libraries.designsystem.preview.PreviewsDayNight
 import io.element.android.libraries.designsystem.theme.components.Scaffold
+import io.element.android.libraries.designsystem.theme.components.Text
 import io.element.android.libraries.designsystem.theme.components.TextButton
 import io.element.android.libraries.designsystem.theme.components.TopAppBar
 import io.element.android.libraries.ui.strings.CommonStrings
@@ -62,6 +69,10 @@ fun RoomInviteMembersView(
             invitePeopleView()
         }
     }
+
+    if (state.sendInvitesAction.isLoading()) {
+        InviteProgressDialog()
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -82,6 +93,24 @@ private fun RoomInviteMembersTopBar(
             )
         }
     )
+}
+
+@Composable
+private fun InviteProgressDialog() {
+    ProgressDialog {
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            text = stringResource(R.string.screen_room_details_invite_people_preparing),
+            color = ElementTheme.colors.textPrimary,
+            style = ElementTheme.typography.fontHeadingSmMedium,
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.screen_room_details_invite_people_dont_close),
+            color = ElementTheme.colors.textSecondary,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
 }
 
 @PreviewsDayNight
