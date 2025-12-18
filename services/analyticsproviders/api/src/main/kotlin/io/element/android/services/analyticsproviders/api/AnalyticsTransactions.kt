3e3531e33c13b3c47fc0085484b9f099e8c537c9
Jorge Martín <jorgem@element.io>
2025-12-18 17:42:26 +0100

    Add `AnalyticsTransactions` with a set of `TransactionDefinition` items matching those in the user story

---
+++ services/analyticsproviders/api/src/main/kotlin/io/element/android/services/analyticsproviders/api/AnalyticsTransactions.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2025 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.services.analyticsproviders.api
+
+object AnalyticsTransactions {
+    val coldStart = TransactionDefinition(
+        name = "Cold start",
+        operation = "ux",
+        description = "Cold start until the cached room list is displayed",
+    )
+
+    val catchUp = TransactionDefinition(
+        name = "Catch-up",
+        operation = "ux",
+        description = "The app syncs and the room list becomes up-to-date",
+    )
+
+    val notificationToMessage = TransactionDefinition(
+        name = "Notification to message",
+        operation = "ux",
+        description = "A notification was tapped and it opened a timeline",
+    )
+
+    val openRoom = TransactionDefinition(
+        name = "Open a room",
+        operation = "ux",
+        description = "Open a room and see loaded items in the timeline",
+    )
+
+    val sendMessage = TransactionDefinition(
+        name = "Send a message",
+        operation = "ux",
+        description = "Send to sent state in timeline",
+    )
+}
+
+data class TransactionDefinition(
+    val name: String,
+    val operation: String? = null,
+    val description: String?,
+)
