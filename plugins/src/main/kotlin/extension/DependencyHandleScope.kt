dc3c77ac31d2e02dce3d6fb61cf1984c9e9fef3c
Jorge Martín <jorgem@element.io>
2026-02-13 10:16:12 +0100

    Remove explicit dependency for `androix.compose.material` for compose library modules

--- plugins/src/main/kotlin/extension/DependencyHandleScope.kt
+++ plugins/src/main/kotlin/extension/DependencyHandleScope.kt
@@ -74,7 +74,6 @@ fun DependencyHandlerScope.composeDependencies(libs: LibrariesForLibs) {
     implementation(composeBom)
     androidTestImplementation(composeBom)
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons)
     implementation(libs.androidx.compose.ui.tooling.preview)
