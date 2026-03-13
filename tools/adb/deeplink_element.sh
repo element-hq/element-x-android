39bfd3a2617f672a9d386f2434d2952479a86f21
Benoit Marty <benoit@matrix.org>
2026-03-13 18:15:07 +0100

    Fix deeplink_matrixto.sh content, it did not contain a matrix.to link.

--- tools/adb/deeplink_element.sh
+++ tools/adb/deeplink_element.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-# Copyright (c) 2025 Element Creations Ltd.
-# Copyright 2024 New Vector Ltd.
-#
+# Copyright (c) 2026 Element Creations Ltd.
+#Fix
 # SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
 # Please see LICENSE files in the repository root for full details.
 
 adb shell am start -a android.intent.action.VIEW \
-   -d "https://matrix.to/#/#matrix:matrix.org"
+   -d "element://room/%23element-android%3Amatrix.org"
