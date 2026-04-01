547d70617471bdae4ad8975c9d529a0c3a45925f
Benoit Marty <benoit@matrix.org>
2026-04-01 10:53:31 +0200

    Format file and add parameter names, add default values and cleanup

--- libraries/slashcommands/impl/src/main/kotlin/io/element/android/libraries/slashcommands/impl/Command.kt
+++ libraries/slashcommands/impl/src/main/kotlin/io/element/android/libraries/slashcommands/impl/Command.kt
@@ -16,47 +16,189 @@ import androidx.annotation.StringRes
  */
 enum class Command(
     val command: String,
-    val aliases: List<String>?,
-    val parameters: String?,
+    val aliases: List<String>? = null,
+    val parameters: String? = null,
     @StringRes val description: Int,
-    val isDevCommand: Boolean,
-    val isAllowedInThread: Boolean
+    val isAllowedInThread: Boolean = true,
+    val isDevCommand: Boolean = false,
 ) {
-    CRASH_APP("/crash", null, null, R.string.slash_command_description_crash_application, true, true),
-    EMOTE("/me", null, "<message>", R.string.slash_command_description_emote, false, true),
-    BAN_USER("/ban", null, "<user-id> [reason]", R.string.slash_command_description_ban_user, false, true),
-    UNBAN_USER("/unban", null, "<user-id> [reason]", R.string.slash_command_description_unban_user, false, true),
-    IGNORE_USER("/ignore", null, "<user-id> [reason]", R.string.slash_command_description_ignore_user, false, true),
-    UNIGNORE_USER("/unignore", null, "<user-id>", R.string.slash_command_description_unignore_user, false, true),
-    SET_USER_POWER_LEVEL("/op", null, "<user-id> [<power-level>]", R.string.slash_command_description_op_user, false, false),
-    RESET_USER_POWER_LEVEL("/deop", null, "<user-id>", R.string.slash_command_description_deop_user, false, false),
-    ROOM_NAME("/roomname", null, "<name>", R.string.slash_command_description_room_name, false, false),
-    INVITE("/invite", null, "<user-id> [reason]", R.string.slash_command_description_invite_user, false, true),
-    JOIN_ROOM("/join", listOf("/j", "/goto"), "<room-address> [reason]", R.string.slash_command_description_join_room, false, false),
-    TOPIC("/topic", null, "<topic>", R.string.slash_command_description_topic, false, false),
-    REMOVE_USER("/remove", listOf("/kick"), "<user-id> [reason]", R.string.slash_command_description_remove_user, false, true),
-    CHANGE_DISPLAY_NAME("/nick", null, "<display-name>", R.string.slash_command_description_nick, false, true),
-    CHANGE_DISPLAY_NAME_FOR_ROOM("/myroomnick", listOf("/roomnick"), "<display-name>", R.string.slash_command_description_nick_for_room, false, false),
+    CRASH_APP(
+        command = "/crash",
+        description = R.string.slash_command_description_crash_application,
+        isDevCommand = true,
+    ),
+    EMOTE(
+        command = "/me",
+        parameters = "<message>",
+        description = R.string.slash_command_description_emote,
+    ),
+    BAN_USER(
+        command = "/ban",
+        parameters = "<user-id> [reason]",
+        description = R.string.slash_command_description_ban_user,
+    ),
+    UNBAN_USER(
+        command = "/unban",
+        parameters = "<user-id> [reason]",
+        description = R.string.slash_command_description_unban_user,
+    ),
+    IGNORE_USER(
+        command = "/ignore",
+        parameters = "<user-id> [reason]",
+        description = R.string.slash_command_description_ignore_user,
+    ),
+    UNIGNORE_USER(
+        command = "/unignore",
+        parameters = "<user-id>",
+        description = R.string.slash_command_description_unignore_user,
+    ),
+    SET_USER_POWER_LEVEL(
+        command = "/op",
+        parameters = "<user-id> [<power-level>]",
+        description = R.string.slash_command_description_op_user,
+        isAllowedInThread = false,
+    ),
+    RESET_USER_POWER_LEVEL(
+        command = "/deop",
+        parameters = "<user-id>",
+        description = R.string.slash_command_description_deop_user,
+        isAllowedInThread = false,
+    ),
+    ROOM_NAME(
+        command = "/roomname",
+        parameters = "<name>",
+        description = R.string.slash_command_description_room_name,
+        isAllowedInThread = false,
+    ),
+    INVITE(
+        command = "/invite",
+        parameters = "<user-id> [reason]",
+        description = R.string.slash_command_description_invite_user,
+    ),
+    JOIN_ROOM(
+        command = "/join",
+        aliases = listOf("/j", "/goto"),
+        parameters = "<room-address> [reason]",
+        description = R.string.slash_command_description_join_room,
+        isAllowedInThread = false,
+    ),
+    TOPIC(
+        command = "/topic",
+        parameters = "<topic>",
+        description = R.string.slash_command_description_topic,
+        isAllowedInThread = false,
+    ),
+    REMOVE_USER(
+        command = "/remove",
+        aliases = listOf("/kick"),
+        parameters = "<user-id> [reason]",
+        description = R.string.slash_command_description_remove_user,
+    ),
+    CHANGE_DISPLAY_NAME(
+        command = "/nick",
+        parameters = "<display-name>",
+        description = R.string.slash_command_description_nick,
+    ),
+    CHANGE_DISPLAY_NAME_FOR_ROOM(
+        command = "/myroomnick",
+        aliases = listOf("/roomnick"),
+        parameters = "<display-name>",
+        description = R.string.slash_command_description_nick_for_room,
+        isAllowedInThread = false,
+    ),
+    ROOM_AVATAR(
+        command = "/roomavatar",
+        parameters = "<mxc_url>",
+        description = R.string.slash_command_description_room_avatar,
+        isAllowedInThread = false,
+        // Dev command since user has to know the mxc url
+        isDevCommand = true,
+    ),
+    CHANGE_AVATAR_FOR_ROOM(
+        command = "/myroomavatar",
+        parameters = "<mxc_url>",
+        description = R.string.slash_command_description_avatar_for_room,
+        isAllowedInThread = false,
+        // Dev command since user has to know the mxc url
+        isDevCommand = true,
+    ),
+    RAINBOW(
+        command = "/rainbow",
+        parameters = "<message>",
+        description = R.string.slash_command_description_rainbow,
+    ),
+    RAINBOW_EMOTE(
+        command = "/rainbowme",
 
-    // Dev command since user has to know the mxc url
-    ROOM_AVATAR("/roomavatar", null, "<mxc_url>", R.string.slash_command_description_room_avatar, true, false),
-
-    // Dev command since user has to know the mxc url
-    CHANGE_AVATAR_FOR_ROOM("/myroomavatar", null, "<mxc_url>", R.string.slash_command_description_avatar_for_room, true, false),
-    RAINBOW("/rainbow", null, "<message>", R.string.slash_command_description_rainbow, false, true),
-    RAINBOW_EMOTE("/rainbowme", null, "<message>", R.string.slash_command_description_rainbow_emote, false, true),
-    DEVTOOLS("/devtools", null, null, R.string.slash_command_description_devtools, true, true),
-    SPOILER("/spoiler", null, "<message>", R.string.slash_command_description_spoiler, false, true),
-    SHRUG("/shrug", null, "<message>", R.string.slash_command_description_shrug, false, true),
-    LENNY("/lenny", null, "<message>", R.string.slash_command_description_lenny, false, true),
-    PLAIN("/plain", null, "<message>", R.string.slash_command_description_plain, false, true),
-    WHOIS("/whois", null, "<user-id>", R.string.slash_command_description_whois, false, true),
-    DISCARD_SESSION("/discardsession", null, null, R.string.slash_command_description_discard_session, false, false),
-    CONFETTI("/confetti", null, "<message>", R.string.slash_command_confetti, false, false),
-    SNOWFALL("/snowfall", null, "<message>", R.string.slash_command_snow, false, false),
-    LEAVE_ROOM("/leave", listOf("/part"), null, R.string.slash_command_description_leave_room, true, false),
-    UPGRADE_ROOM("/upgraderoom", null, "newVersion", R.string.slash_command_description_upgrade_room, true, false),
-    TABLE_FLIP("/tableflip", null, "<message>", R.string.slash_command_description_table_flip, false, true);
+        parameters = "<message>",
+        description = R.string.slash_command_description_rainbow_emote,
+    ),
+    DEVTOOLS(
+        command = "/devtools",
+        description = R.string.slash_command_description_devtools,
+        isDevCommand = true,
+    ),
+    SPOILER(
+        command = "/spoiler",
+        parameters = "<message>",
+        description = R.string.slash_command_description_spoiler,
+    ),
+    SHRUG(
+        command = "/shrug",
+        parameters = "<message>",
+        description = R.string.slash_command_description_shrug,
+    ),
+    LENNY(
+        command = "/lenny",
+        parameters = "<message>",
+        description = R.string.slash_command_description_lenny,
+    ),
+    PLAIN(
+        command = "/plain",
+        parameters = "<message>",
+        description = R.string.slash_command_description_plain,
+    ),
+    WHOIS(
+        command = "/whois",
+        parameters = "<user-id>",
+        description = R.string.slash_command_description_whois,
+    ),
+    DISCARD_SESSION(
+        command = "/discardsession",
+        description = R.string.slash_command_description_discard_session,
+        isAllowedInThread = false,
+    ),
+    CONFETTI(
+        command = "/confetti",
+        parameters = "<message>",
+        description = R.string.slash_command_confetti,
+        isAllowedInThread = false,
+    ),
+    SNOWFALL(
+        command = "/snowfall",
+        parameters = "<message>",
+        description = R.string.slash_command_snow,
+        isAllowedInThread = false,
+    ),
+    LEAVE_ROOM(
+        command = "/leave",
+        aliases = listOf("/part"),
+        description = R.string.slash_command_description_leave_room,
+        isAllowedInThread = false,
+        isDevCommand = true,
+    ),
+    UPGRADE_ROOM(
+        command = "/upgraderoom",
+        parameters = "newVersion",
+        description = R.string.slash_command_description_upgrade_room,
+        isAllowedInThread = false,
+        isDevCommand = true,
+    ),
+    TABLE_FLIP(
+        command = "/tableflip",
+        parameters = "<message>",
+        description = R.string.slash_command_description_table_flip,
+    );
 
     val allAliases = listOf(command) + aliases.orEmpty()
     fun matches(inputCommand: CharSequence) = allAliases.any { it.contentEquals(inputCommand, true) }
