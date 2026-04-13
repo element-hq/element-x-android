llm-lab-org/fanoos-android

Sign in with Classic

features/login/impl/src/main/kotlin/io/element/android/features/login/impl/LoginFlowNode.kt
@@ -22,6 +22,7 @@ import com.bumble.appyx.core.plugin.Plugin
 import com.bumble.appyx.navmodel.backstack.BackStack
 import com.bumble.appyx.navmodel.backstack.operation.pop
 import com.bumble.appyx.navmodel.backstack.operation.push
+import com.bumble.appyx.navmodel.backstack.operation.replace
 import com.bumble.appyx.navmodel.backstack.operation.singleTop
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.Assisted
@@ -30,12 +31,15 @@ import io.element.android.annotations.ContributesNode
 import io.element.android.compound.theme.ElementTheme
 import io.element.android.features.login.api.LoginEntryPoint
 import io.element.android.features.login.impl.accountprovider.AccountProviderDataSource
+import io.element.android.features.login.impl.classic.ElementClassicConnection
 import io.element.android.features.login.impl.qrcode.QrCodeLoginFlowNode
 import io.element.android.features.login.impl.screens.changeaccountprovider.ChangeAccountProviderNode
+import io.element.android.features.login.impl.screens.checkclassic.CheckClassicNode
 import io.element.android.features.login.impl.screens.chooseaccountprovider.ChooseAccountProviderNode
 import io.element.android.features.login.impl.screens.confirmaccountprovider.ConfirmAccountProviderNode
 import io.element.android.features.login.impl.screens.createaccount.CreateAccountNode
 import io.element.android.features.login.impl.screens.loginpassword.LoginPasswordNode
+import io.element.android.features.login.impl.screens.loginwithclassic.LoginWithClassicNode
 import io.element.android.features.login.impl.screens.onboarding.OnBoardingNode
 import io.element.android.features.login.impl.screens.searchaccountprovider.SearchAccountProviderNode
 import io.element.android.libraries.androidutils.browser.openUrlInChromeCustomTab
@@ -46,7 +50,9 @@ import io.element.android.libraries.architecture.callback
 import io.element.android.libraries.architecture.createNode
 import io.element.android.libraries.architecture.inputs
 import io.element.android.libraries.di.annotations.AppCoroutineScope
+import io.element.android.libraries.featureflag.api.FeatureFlagService
 import io.element.android.libraries.matrix.api.auth.OidcDetails
+import io.element.android.libraries.matrix.api.core.UserId
 import io.element.android.libraries.oidc.api.OidcAction
 import io.element.android.libraries.oidc.api.OidcActionFlow
 import kotlinx.coroutines.CoroutineScope
@@ -63,9 +69,11 @@ class LoginFlowNode(
     private val oidcActionFlow: OidcActionFlow,
     @AppCoroutineScope
     private val appCoroutineScope: CoroutineScope,
+    private val elementClassicConnection: ElementClassicConnection,
+    private val featureFlagService: FeatureFlagService,
 ) : BaseFlowNode<LoginFlowNode.NavTarget>(
     backstack = BackStack(
-        initialElement = NavTarget.OnBoarding,
+        initialElement = NavTarget.CheckClassic,
         savedStateMap = buildContext.savedStateMap,
     ),
     buildContext = buildContext,
@@ -103,10 +111,20 @@ class LoginFlowNode(
 
     sealed interface NavTarget : Parcelable {
         @Parcelize
-        data object OnBoarding : NavTarget
+        data object CheckClassic : NavTarget
+
+        @Parcelize
+        data class OnBoarding(
+            val showBackButton: Boolean,
+        ) : NavTarget
 
         @Parcelize
         data object QrCode : NavTarget
+
+        @Parcelize
+        data class Classic(
+            val userId: UserId,
+        ) : NavTarget
 
         @Parcelize
         data class ConfirmAccountProvider(
@@ -131,7 +149,19 @@ class LoginFlowNode(
 
     override fun resolve(navTarget: NavTarget, buildContext: BuildContext): Node {
         return when (navTarget) {
-            NavTarget.OnBoarding -> {
+            NavTarget.CheckClassic -> {
+                val callback = object : CheckClassicNode.Callback {
+                    override fun navigateToClassic(userId: UserId) {
+                        backstack.replace(NavTarget.Classic(userId))
+                    }
+
+                    override fun navigateToOnBoarding() {
+                        backstack.replace(NavTarget.OnBoarding(showBackButton = false))
+                    }
+                }
+                createNode<CheckClassicNode>(buildContext, listOf(callback))
+            }
+            is NavTarget.OnBoarding -> {
                 val callback = object : OnBoardingNode.Callback {
                     override fun navigateToSignUpFlow() {
                         backstack.push(
@@ -170,13 +200,18 @@ class LoginFlowNode(
                     }
 
                     override fun onDone() {
-                        callback.onDone()
+                        if (navTarget.showBackButton) {
+                            backstack.pop()
+                        } else {
+                            callback.onDone()
+                        }
                     }
                 }
                 val params = inputs<Params>()
                 val inputs = OnBoardingNode.Params(
                     accountProvider = params.accountProvider,
                     loginHint = params.loginHint,
+                    showBackButton = navTarget.showBackButton,
                 )
                 createNode<OnBoardingNode>(buildContext, listOf(callback, inputs))
             }
@@ -226,6 +261,29 @@ class LoginFlowNode(
                     }
                 }
                 createNode<ConfirmAccountProviderNode>(buildContext, plugins = listOf(inputs, callback))
+            }
+            is NavTarget.Classic -> {
+                val callback = object : LoginWithClassicNode.Callback {
+                    override fun navigateToOtherOptions() {
+                        backstack.push(NavTarget.OnBoarding(showBackButton = true))
+                    }
+
+                    override fun navigateToLoginPassword() {
+                        backstack.push(NavTarget.LoginPassword)
+                    }
+
+                    override fun navigateToOidc(oidcDetails: OidcDetails) {
+                        navigateToMas(oidcDetails)
+                    }
+
+                    override fun navigateToCreateAccount(url: String) {
+                        backstack.push(NavTarget.CreateAccount(url))
+                    }
+                }
+                val inputs = LoginWithClassicNode.Inputs(
+                    userId = navTarget.userId,
+                )
+                createNode<LoginWithClassicNode>(buildContext, plugins = listOf(inputs, callback))
             }
             NavTarget.ChangeAccountProvider -> {
                 val callback = object : ChangeAccountProviderNode.Callback {
@@ -280,6 +338,14 @@ class LoginFlowNode(
     override fun View(modifier: Modifier) {
         activity = requireNotNull(LocalActivity.current)
         darkTheme = !ElementTheme.isLightTheme
+
+        DisposableEffect(Unit) {
+            elementClassicConnection.start()
+            onDispose {
+                elementClassicConnection.stop()
+            }
+        }
+
         DisposableEffect(Unit) {
             onDispose {
                 activity = null
@@ -288,6 +354,6 @@ class LoginFlowNode(
                 }
             }
         }
-        BackstackView()
+        BackstackView(transitionHandler = rememberLoginFlowTransitionHandler(backstack))
     }
 }

features/login/impl/src/test/kotlin/io/element/android/features/login/impl/classic/FakeElementClassicConnection.kt
@@ -5,8 +5,9 @@
  * Please see LICENSE files in the repository root for full details.
  */
 
-package io.element.android.features.login.impl.screens.onboarding.classic
+package io.element.android.features.login.impl.classic
 
+import io.element.android.libraries.matrix.api.core.UserId
 import io.element.android.tests.testutils.lambda.lambdaError
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,12 +16,14 @@ import kotlinx.coroutines.flow.asStateFlow
 class FakeElementClassicConnection(
     private val startResult: () -> Unit = { lambdaError() },
     private val stopResult: () -> Unit = { lambdaError() },
-    private val requestDataResult: () -> Unit = { lambdaError() },
+    private val requestVerifiedUserIdResult: () -> Unit = { lambdaError() },
+    private val requestProfileResult: (UserId) -> Unit = { lambdaError() },
     initialState: ElementClassicConnectionState = ElementClassicConnectionState.Idle
 ) : ElementClassicConnection {
     override fun start() = startResult()
     override fun stop() = stopResult()
-    override fun requestData() = requestDataResult()
+    override fun requestSession() = requestVerifiedUserIdResult()
+    override fun requestAvatar(userId: UserId) = requestProfileResult(userId)
     private val mutableStateFlow = MutableStateFlow(initialState)
     override val stateFlow: StateFlow<ElementClassicConnectionState> = mutableStateFlow.asStateFlow()
     suspend fun emitState(state: ElementClassicConnectionState) {

element-hq/element-x-android

features/login/impl/src/main/kotlin/io/element/android/features/login/impl/LoginFlowNode.kt
@@ -22,6 +22,7 @@ import com.bumble.appyx.core.plugin.Plugin
 import com.bumble.appyx.navmodel.backstack.BackStack
 import com.bumble.appyx.navmodel.backstack.operation.pop
 import com.bumble.appyx.navmodel.backstack.operation.push
+import com.bumble.appyx.navmodel.backstack.operation.replace
 import com.bumble.appyx.navmodel.backstack.operation.singleTop
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.Assisted
@@ -30,9 +31,11 @@ import io.element.android.annotations.ContributesNode
 import io.element.android.compound.theme.ElementTheme
 import io.element.android.features.login.api.LoginEntryPoint
 import io.element.android.features.login.impl.accountprovider.AccountProviderDataSource
+import io.element.android.features.login.impl.classic.ElementClassicConnection
 import io.element.android.features.login.impl.qrcode.QrCodeLoginFlowNode
 import io.element.android.features.login.impl.screens.changeaccountprovider.ChangeAccountProviderNode
 import io.element.android.features.login.impl.screens.chooseaccountprovider.ChooseAccountProviderNode
+import io.element.android.features.login.impl.screens.classic.ClassicFlowNode
 import io.element.android.features.login.impl.screens.confirmaccountprovider.ConfirmAccountProviderNode
 import io.element.android.features.login.impl.screens.createaccount.CreateAccountNode
 import io.element.android.features.login.impl.screens.loginpassword.LoginPasswordNode
@@ -63,9 +66,10 @@ class LoginFlowNode(
     private val oidcActionFlow: OidcActionFlow,
     @AppCoroutineScope
     private val appCoroutineScope: CoroutineScope,
+    private val elementClassicConnection: ElementClassicConnection,
 ) : BaseFlowNode<LoginFlowNode.NavTarget>(
     backstack = BackStack(
-        initialElement = NavTarget.OnBoarding,
+        initialElement = NavTarget.CheckClassicFlow,
         savedStateMap = buildContext.savedStateMap,
     ),
     buildContext = buildContext,
@@ -103,7 +107,12 @@ class LoginFlowNode(
 
     sealed interface NavTarget : Parcelable {
         @Parcelize
-        data object OnBoarding : NavTarget
+        data object CheckClassicFlow : NavTarget
+
+        @Parcelize
+        data class OnBoarding(
+            val showBackButton: Boolean,
+        ) : NavTarget
 
         @Parcelize
         data object QrCode : NavTarget
@@ -123,7 +132,9 @@ class LoginFlowNode(
         data object SearchAccountProvider : NavTarget
 
         @Parcelize
-        data object LoginPassword : NavTarget
+        data class LoginPassword(
+            val initialLogin: String = "",
+        ) : NavTarget
 
         @Parcelize
         data class CreateAccount(val url: String) : NavTarget
@@ -131,7 +142,31 @@ class LoginFlowNode(
 
     override fun resolve(navTarget: NavTarget, buildContext: BuildContext): Node {
         return when (navTarget) {
-            NavTarget.OnBoarding -> {
+            NavTarget.CheckClassicFlow -> {
+                val callback = object : ClassicFlowNode.Callback {
+                    override fun navigateToOnBoarding(allowBackNavigation: Boolean) {
+                        if (allowBackNavigation) {
+                            backstack.push(NavTarget.OnBoarding(showBackButton = true))
+                        } else {
+                            backstack.replace(NavTarget.OnBoarding(showBackButton = false))
+                        }
+                    }
+
+                    override fun navigateToLoginPassword() {
+                        backstack.push(NavTarget.LoginPassword())
+                    }
+
+                    override fun navigateToOidc(oidcDetails: OidcDetails) {
+                        navigateToMas(oidcDetails)
+                    }
+
+                    override fun navigateToCreateAccount(url: String) {
+                        backstack.push(NavTarget.CreateAccount(url))
+                    }
+                }
+                createNode<ClassicFlowNode>(buildContext, listOf(callback))
+            }
+            is NavTarget.OnBoarding -> {
                 val callback = object : OnBoardingNode.Callback {
                     override fun navigateToSignUpFlow() {
                         backstack.push(
@@ -166,17 +201,22 @@ class LoginFlowNode(
                     }
 
                     override fun navigateToLoginPassword() {
-                        backstack.push(NavTarget.LoginPassword)
+                        backstack.push(NavTarget.LoginPassword())
                     }
 
                     override fun onDone() {
-                        callback.onDone()
+                        if (navTarget.showBackButton) {
+                            backstack.pop()
+                        } else {
+                            callback.onDone()
+                        }
                     }
                 }
                 val params = inputs<Params>()
                 val inputs = OnBoardingNode.Params(
                     accountProvider = params.accountProvider,
                     loginHint = params.loginHint,
+                    showBackButton = navTarget.showBackButton,
                 )
                 createNode<OnBoardingNode>(buildContext, listOf(callback, inputs))
             }
@@ -191,7 +231,7 @@ class LoginFlowNode(
                     }
 
                     override fun navigateToLoginPassword() {
-                        backstack.push(NavTarget.LoginPassword)
+                        backstack.push(NavTarget.LoginPassword())
                     }
                 }
                 createNode<ChooseAccountProviderNode>(buildContext, listOf(callback))
@@ -218,7 +258,7 @@ class LoginFlowNode(
                     }
 
                     override fun navigateToLoginPassword() {
-                        backstack.push(NavTarget.LoginPassword)
+                        backstack.push(NavTarget.LoginPassword())
                     }
 
                     override fun navigateToChangeAccountProvider() {
@@ -257,8 +297,11 @@ class LoginFlowNode(
 
                 createNode<SearchAccountProviderNode>(buildContext, plugins = listOf(callback))
             }
-            NavTarget.LoginPassword -> {
-                createNode<LoginPasswordNode>(buildContext)
+            is NavTarget.LoginPassword -> {
+                val inputs = LoginPasswordNode.Inputs(
+                    initialLogin = navTarget.initialLogin,
+                )
+                createNode<LoginPasswordNode>(buildContext, plugins = listOf(inputs))
             }
             is NavTarget.CreateAccount -> {
                 val inputs = CreateAccountNode.Inputs(
@@ -280,6 +323,14 @@ class LoginFlowNode(
     override fun View(modifier: Modifier) {
         activity = requireNotNull(LocalActivity.current)
         darkTheme = !ElementTheme.isLightTheme
+
+        DisposableEffect(Unit) {
+            elementClassicConnection.start()
+            onDispose {
+                elementClassicConnection.stop()
+            }
+        }
+
         DisposableEffect(Unit) {
             onDispose {
                 activity = null
@@ -288,6 +339,6 @@ class LoginFlowNode(
                 }
             }
         }
-        BackstackView()
+        BackstackView(transitionHandler = rememberLoginFlowTransitionHandler())
     }
 }

features/login/impl/src/test/kotlin/io/element/android/features/login/impl/classic/FakeElementClassicConnection.kt
@@ -5,8 +5,9 @@
  * Please see LICENSE files in the repository root for full details.
  */
 
-package io.element.android.features.login.impl.screens.onboarding.classic
+package io.element.android.features.login.impl.classic
 
+import io.element.android.libraries.matrix.api.core.UserId
 import io.element.android.tests.testutils.lambda.lambdaError
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,12 +16,14 @@ import kotlinx.coroutines.flow.asStateFlow
 class FakeElementClassicConnection(
     private val startResult: () -> Unit = { lambdaError() },
     private val stopResult: () -> Unit = { lambdaError() },
-    private val requestDataResult: () -> Unit = { lambdaError() },
+    private val requestSessionResult: () -> Unit = { lambdaError() },
+    private val requestAvatarResult: (UserId) -> Unit = { lambdaError() },
     initialState: ElementClassicConnectionState = ElementClassicConnectionState.Idle
 ) : ElementClassicConnection {
     override fun start() = startResult()
     override fun stop() = stopResult()
-    override fun requestData() = requestDataResult()
+    override fun requestSession() = requestSessionResult()
+    override fun requestAvatar(userId: UserId) = requestAvatarResult(userId)
     private val mutableStateFlow = MutableStateFlow(initialState)
     override val stateFlow: StateFlow<ElementClassicConnectionState> = mutableStateFlow.asStateFlow()
     suspend fun emitState(state: ElementClassicConnectionState) {