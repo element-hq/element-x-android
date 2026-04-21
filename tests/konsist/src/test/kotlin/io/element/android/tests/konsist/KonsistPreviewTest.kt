025f9b2c8caaf103d0851cba769490f6771cabd9
Jorge Martín <jorgem@element.io>
2026-04-21 18:05:58 +0200

    Fix Konsist issues

--- tests/konsist/src/test/kotlin/io/element/android/tests/konsist/KonsistPreviewTest.kt
+++ tests/konsist/src/test/kotlin/io/element/android/tests/konsist/KonsistPreviewTest.kt
@@ -139,7 +139,6 @@ class KonsistPreviewTest {
         "TextComposerLinkDialogCreateLinkWithoutTextPreview",
         "TextComposerLinkDialogEditLinkPreview",
         "TextComposerReplyPreview",
-        "TextComposerScaledDensityWithReplyPreview",
         "TextComposerSimpleNotEncryptedPreview",
         "TextComposerSimplePreview",
         "TextComposerVoiceNotEncryptedPreview",
