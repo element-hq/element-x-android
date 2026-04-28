5c9d8d917c6721a21395d6eadef3cbb26d97da79
Hugh Nimmo-Smith <hughns@element.io>
2026-04-28 11:22:25 +0100

    Update error mappings for Link new device

--- features/linknewdevice/impl/src/main/kotlin/io/element/android/features/linknewdevice/impl/LinkNewDeviceFlowNode.kt
+++ features/linknewdevice/impl/src/main/kotlin/io/element/android/features/linknewdevice/impl/LinkNewDeviceFlowNode.kt
@@ -183,20 +183,15 @@ class LinkNewDeviceFlowNode(
 
     private fun navigateToError(errorType: ErrorType) {
         // Map the error to an error screen
-        // TODO Update this mapping
         val error = when (errorType) {
-            is ErrorType.DeviceIdAlreadyInUse -> ErrorScreenType.UnknownError
-            is ErrorType.InvalidCheckCode -> ErrorScreenType.InsecureChannelDetected
-            is ErrorType.MissingSecretsBackup -> ErrorScreenType.UnknownError
-            is ErrorType.NotFound -> ErrorScreenType.Expired
-            is ErrorType.DeviceNotFound -> ErrorScreenType.UnknownError
-            is ErrorType.Unknown -> ErrorScreenType.UnknownError
-            is ErrorType.UnsupportedProtocol -> ErrorScreenType.UnknownError
-            is ErrorType.Cancelled -> ErrorScreenType.UnknownError
+            is ErrorType.InvalidCheckCode -> ErrorScreenType.Mismatch2Digits
+            is ErrorType.UnsupportedProtocol -> ErrorScreenType.ProtocolNotSupported
+            is ErrorType.Cancelled -> ErrorScreenType.Cancelled
             is ErrorType.ConnectionInsecure -> ErrorScreenType.InsecureChannelDetected
-            is ErrorType.Expired -> ErrorScreenType.Expired
-            is ErrorType.OtherDeviceAlreadySignedIn -> ErrorScreenType.UnknownError
-            is ErrorType.UnsupportedQrCodeType -> ErrorScreenType.UnknownError
+            is ErrorType.Expired, is ErrorType.NotFound, is ErrorType.DeviceNotFound -> ErrorScreenType.Expired
+            is ErrorType.OtherDeviceAlreadySignedIn -> ErrorScreenType.UnknownError // TODO: should show other_device_already_signed_in screen with checkmark when available
+            is ErrorType.UnsupportedQrCodeType -> ErrorScreenType.UnknownError // TODO: check if we expect to hit this here or if it should be caught earlier on
+            is ErrorType.MissingSecretsBackup, is ErrorType.DeviceIdAlreadyInUse, is ErrorType.Unknown -> ErrorScreenType.UnknownError
         }
         // It is OK to push on backstack, since when user leaves the error screen, a new root will be set,
         // or the whole flow will be popped.
