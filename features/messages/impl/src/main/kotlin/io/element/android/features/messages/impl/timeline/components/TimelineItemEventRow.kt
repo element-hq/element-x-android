781f47549025e7270c9ffa7332e3c3204a701877
Jorge Martín <jorgem@element.io>
2026-06-10 13:52:21 +0200

    Try improving screenshot tests yet again

--- features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/TimelineItemEventRow.kt
+++ features/messages/impl/src/main/kotlin/io/element/android/features/messages/impl/timeline/components/TimelineItemEventRow.kt
@@ -913,8 +913,7 @@ internal fun TimelineItemEventRowRtlContentPreview() = ElementPreview {
                     senderDisplayName = "Sender with a super long name that should ellipsize",
                     isMine = isMine,
                     content = aTimelineItemTextContent(
-                        body = "ظَة وَدَاع يَسْتَغْرِب فِيهَا اَلشَّاعِر أَنْ لَا يَبْكِي مِنْ أَلَم اَلْفِرَاق،" +
-                            " وَيَصِف حَالَة اَلْمُودِعِينَ وَبَعْضهم"
+                        body = "ظَة وَدَاع يَسْتَغْرِب فِيهَ"
                     ),
                     groupPosition = TimelineItemGroupPosition.First,
                     threadInfo = null,
@@ -930,7 +929,7 @@ internal fun TimelineItemEventRowRtlContentPreview() = ElementPreview {
                         isMine = isMine,
                         content = aTimelineItemTextContent(
                             body = "ظَة وَدَاع يَسْتَغْرِب فِيهَا اَلشَّاعِر أَنْ لَا يَبْكِي مِنْ أَلَم اَلْفِرَاق،" +
-                                " وَيَصِف حَالَة اَلْمُودِعِينَ وَبَعْضهم"
+                                " وَيَصِف حَالَة اَلْمُودِعِينَ"
                         ),
                         groupPosition = TimelineItemGroupPosition.First,
                         threadInfo = null,
