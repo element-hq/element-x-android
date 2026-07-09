ea202921df894ed3701383dc9751b1bdf0a134fd
Skye Elliot <actuallyori@gmail.com>
2026-07-09 10:34:50 +0100

    Add uniffi-free X.509 interfaces to matrix.api
    Co-Authored-By: Richard van der Hoff <richard@matrix.org>

---
+++ libraries/matrix/api/src/main/kotlin/io/element/android/libraries/matrix/api/x509/RawX509Signature.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2026 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.libraries.matrix.api.x509
+
+/**
+ * The object we receive from [`X509Sign`], and pass to
+ * [`X509Verify`].
+ *
+ * A simplified representation of the data in the signature object.
+ */
+data class RawX509Signature(
+    /**
+     * The PEM-encoded certificate chain, starting with the device's own
+     * certificate, followed by intermediate certificates.
+     */
+    val certificateChain: String,
+    /** The raw bytes of the signature. */
+    val signatureBytes: ByteArray,
+    /** The algorithm that the signer used to construct the signature. */
+    val signatureScheme: X509SignatureScheme,
+) {
+    // Overridden because the [signatureBytes] array must be compared by content, not by reference.
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is RawX509Signature) return false
+
+        return certificateChain == other.certificateChain &&
+            signatureBytes.contentEquals(other.signatureBytes) &&
+            signatureScheme == other.signatureScheme
+    }
+
+    override fun hashCode(): Int {
+        var result = certificateChain.hashCode()
+        result = 31 * result + signatureBytes.contentHashCode()
+        result = 31 * result + signatureScheme.hashCode()
+        return result
+    }
+}
+
+enum class X509SignatureScheme {
+    /**
+     * SHA-512 message digest, with RSASSA-PSS signature scheme (aka RSA-PSS),
+     * per [RFC 8017 § 8.1](https://www.rfc-editor.org/info/rfc8017/#section-8.1).
+     *
+     * Not to be confused with RSA-PKCS1, which is incompatible.
+     */
+    RSA_PSS_SHA512,
+}
