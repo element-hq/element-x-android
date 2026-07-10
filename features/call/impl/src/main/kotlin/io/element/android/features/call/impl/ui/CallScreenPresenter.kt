17c5534c3dbb1c06c3ee9ae910d2f36e56a8057d
Timo K. <toger5@hotmail.de>
2026-07-10 16:21:07 +0200

    User `theme=dark` url param for element call

--- features/call/impl/src/main/kotlin/io/element/android/features/call/impl/ui/CallScreenPresenter.kt
+++ features/call/impl/src/main/kotlin/io/element/android/features/call/impl/ui/CallScreenPresenter.kt
@@ -84,7 +84,7 @@ class CallScreenPresenter(
         var ignoreWebViewError by rememberSaveable { mutableStateOf(false) }
         var webViewError by remember { mutableStateOf<String?>(null) }
         val languageTag = languageTagProvider.provideLanguageTag()
-        val theme = if (ElementTheme.isLightTheme) "light" else "dark"
+        val theme = "dark"
 
         DisposableEffect(Unit) {
             coroutineScope.launch {
