8189b41ad1d3f07bd0bd30002b019aaca357dc87
Jorge Martín <jorgem@element.io>
2026-07-24 08:59:11 +0200

    Try making `HeaderFooterView` more robust
    Create `HeaderFooterLayout` to properly calculate the content size when the component is scrollable so it's at least the size of the available space.

---
+++ libraries/designsystem/src/main/kotlin/io/element/android/libraries/designsystem/atomic/pages/HeaderFooterLayout.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2026 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.libraries.designsystem.atomic.pages
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.movableContentOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.unit.Constraints
+import kotlin.math.max
+
+/**
+ * A layout with a [header], a [footer] and a [content] composables.
+ * The component can be scrollable or not, which will modify which layout is used internally to arrange the UI.
+ * In both cases, the [content] will use all the available space between the [header] and the [footer].
+ * The [footer] is always visible, the [header] and the [content] can be scrolled if [isScrollable] is true.
+ *
+ * @param header the header composable.
+ * @param footer the footer composable.
+ * @param isScrollable if the content should be scrollable.
+ * @param contentInsetsPadding padding values to apply to the content. This is useful to handle system insets.
+ * @param modifier Compose modifier.
+ * @param content the main content composable.
+ */
+@Composable
+fun HeaderFooterLayout(
+    header: @Composable () -> Unit,
+    footer: @Composable () -> Unit,
+    isScrollable: Boolean,
+    contentInsetsPadding: PaddingValues,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) {
+    val movableHeader = remember { movableContentOf(header) }
+    val movableContent = remember { movableContentOf(content) }
+    Column(modifier = modifier) {
+        if (isScrollable) {
+            var newHeight by remember { mutableIntStateOf(1) }
+            Box(
+                modifier = Modifier
+                .weight(1f, fill = true)
+                .padding(contentInsetsPadding)
+                .onSizeChanged {
+                    newHeight = it.height
+                }
+            ) {
+                Layout(
+                    content = {
+                        movableHeader()
+                        movableContent()
+                    },
+                    modifier = Modifier.verticalScroll(rememberScrollState()),
+                    measurePolicy = { measurables, constraints ->
+                        val actualConstraints = constraints.copy(minWidth = 0, minHeight = 0, maxHeight = newHeight)
+                        val headerPlaceable = measurables[0].measure(actualConstraints)
+
+                        val contentPlaceable = if (measurables.size > 1) {
+                            val availableContentHeight = max(1, actualConstraints.maxHeight - headerPlaceable.height)
+                            val contentConstraints = actualConstraints.copy(minHeight = availableContentHeight, maxHeight = Constraints.Infinity)
+                            measurables[1].measure(contentConstraints)
+                        } else {
+                            null
+                        }
+
+                        layout(actualConstraints.maxWidth, headerPlaceable.height + (contentPlaceable?.height ?: 0)) {
+                            var yPosition = 0
+
+                            headerPlaceable.placeRelative(0, yPosition)
+                            yPosition += headerPlaceable.height
+
+                            contentPlaceable?.placeRelative(0, yPosition)
+                        }
+                    }
+                )
+            }
+        } else {
+            movableHeader()
+            Box(Modifier.weight(1f, fill = true)) {
+                movableContent()
+            }
+        }
+
+        footer()
+    }
+}
