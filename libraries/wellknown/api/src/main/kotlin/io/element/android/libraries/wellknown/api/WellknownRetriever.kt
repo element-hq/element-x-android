25eca29cd2bc83656697ef6eb89f2a5bf2218f42
Jorge Martín <jorgem@element.io>
2026-08-10 14:22:39 +0200

    Rework `WellknownRetriever` so it uses the `GetUrlResolver`
    Also, allow instantiating it using a factory: with this we can always decide where to take the config from and how to do it
    
    Do the same for `ElementWellknownStore` so we can store and restore the values for different sources (endpoints) independently

--- libraries/wellknown/api/src/main/kotlin/io/element/android/libraries/wellknown/api/WellknownRetriever.kt
+++ libraries/wellknown/api/src/main/kotlin/io/element/android/libraries/wellknown/api/WellknownRetriever.kt
@@ -8,6 +8,15 @@
 
 package io.element.android.libraries.wellknown.api
 
+import io.element.android.libraries.matrix.api.GetUrlResolver
+
 interface WellknownRetriever {
-    suspend fun getElementWellKnown(baseUrl: String): WellknownRetrieverResult<ElementWellKnown>
+    suspend fun getElementWellKnown(
+        host: String,
+        source: EnterpriseRemoteConfigSource
+    ): WellknownRetrieverResult<ElementWellKnown>
+
+    interface Factory {
+        fun create(getUrlResolver: GetUrlResolver): WellknownRetriever
+    }
 }
