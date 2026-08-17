fb94fbe927e5e871b94c13bdbaa7f67c0f6dab7d
Jorge Martín <jorgem@element.io>
2026-08-17 12:15:58 +0200

    Fix API breaks:
    - `ClientBuildException.WellKnownLookupDisabled` should be handled now.
    - `Client.sendReply` returns a `Result<SendHandle>` now. We can ignore it like we do for `Client.send`.

--- libraries/matrix/impl/src/main/kotlin/io/element/android/libraries/matrix/impl/timeline/RustTimeline.kt
+++ libraries/matrix/impl/src/main/kotlin/io/element/android/libraries/matrix/impl/timeline/RustTimeline.kt
@@ -365,7 +365,7 @@ class RustTimeline(
         fromNotification: Boolean,
         msgType: MsgType,
     ): Result<Unit> = withContext(dispatcher) {
-        runCatchingExceptions {
+        runCatchingExceptions<Unit> {
             val msg = MessageEventContent.from(
                 body = body,
                 htmlBody = htmlBody,
