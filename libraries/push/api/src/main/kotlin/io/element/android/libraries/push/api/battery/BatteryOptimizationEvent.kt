217ccb75ed2a34cb7df7967d5b5f24b162468df7
Benoit Marty <benoit@matrix.org>
2026-08-25 10:32:34 +0200

    `BatteryOptimizationEvents` -> `BatteryOptimizationEvent`

--- libraries/push/api/src/main/kotlin/io/element/android/libraries/push/api/battery/BatteryOptimizationEvent.kt
+++ libraries/push/api/src/main/kotlin/io/element/android/libraries/push/api/battery/BatteryOptimizationEvent.kt
@@ -8,7 +8,7 @@
 
 package io.element.android.libraries.push.api.battery
 
-sealed interface BatteryOptimizationEvents {
-    data object Dismiss : BatteryOptimizationEvents
-    data object RequestDisableOptimizations : BatteryOptimizationEvents
+sealed interface BatteryOptimizationEvent {
+    data object Dismiss : BatteryOptimizationEvent
+    data object RequestDisableOptimizations : BatteryOptimizationEvent
 }
