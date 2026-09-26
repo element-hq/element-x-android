element-hq/element-x-android

commit 9b2577f1dd6ca2674803a97fff9a0f0b8c28d21e

Split settings: app settings / account settings

features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/PreferencesFlowNode.kt
@@ -26,11 +26,14 @@ import io.element.android.features.lockscreen.api.LockScreenEntryPoint
 import io.element.android.features.logout.api.LogoutEntryPoint
 import io.element.android.features.preferences.api.PreferencesEntryPoint
 import io.element.android.features.preferences.impl.about.AboutNode
-import io.element.android.features.preferences.impl.advanced.AdvancedSettingsNode
+import io.element.android.features.preferences.impl.account.PreferencesAccountNode
 import io.element.android.features.preferences.impl.analytics.AnalyticsSettingsNode
 import io.element.android.features.preferences.impl.blockedusers.BlockedUsersNode
 import io.element.android.features.preferences.impl.developer.DeveloperSettingsNode
 import io.element.android.features.preferences.impl.labs.LabsNode
+import io.element.android.features.preferences.impl.location.LocationSettingsNode
+import io.element.android.features.preferences.impl.media.MediaSettingsNode
+import io.element.android.features.preferences.impl.moderation.ModerationAndSafetyNode
 import io.element.android.features.preferences.impl.notifications.NotificationSettingsNode
 import io.element.android.features.preferences.impl.notifications.edit.EditDefaultNotificationSettingNode
 import io.element.android.features.preferences.impl.root.PreferencesRootNode
@@ -75,10 +78,16 @@ class PreferencesFlowNode(
         data object Root : NavTarget
 
         @Parcelize
+        data object AccountSettings : NavTarget
+
+        @Parcelize
         data object DeveloperSettings : NavTarget
 
         @Parcelize
-        data object AdvancedSettings : NavTarget
+        data object MediaSettings : NavTarget
+
+        @Parcelize
+        data object LocationSettings : NavTarget
 
         @Parcelize
         data object Labs : NavTarget
@@ -91,6 +100,9 @@ class PreferencesFlowNode(
 
         @Parcelize
         data object NotificationSettings : NavTarget
+
+        @Parcelize
+        data object ModerationAndSafety : NavTarget
 
         @Parcelize
         data object TroubleshootNotifications : NavTarget
@@ -133,14 +145,6 @@ class PreferencesFlowNode(
                         callback.navigateToAddAccount()
                     }
 
-                    override fun navigateToBugReport() {
-                        callback.navigateToBugReport()
-                    }
-
-                    override fun navigateToSecureBackup() {
-                        callback.navigateToSecureBackup()
-                    }
-
                     override fun navigateToAnalyticsSettings() {
                         backstack.push(NavTarget.AnalyticsSettings)
                     }
@@ -153,24 +157,48 @@ class PreferencesFlowNode(
                         backstack.push(NavTarget.DeveloperSettings)
                     }
 
-                    override fun navigateToNotificationSettings() {
-                        backstack.push(NavTarget.NotificationSettings)
-                    }
-
                     override fun navigateToLockScreenSettings() {
                         backstack.push(NavTarget.LockScreenSettings)
                     }
 
-                    override fun navigateToAdvancedSettings() {
-                        backstack.push(NavTarget.AdvancedSettings)
+                    override fun navigateToMediaSettings() {
+                        backstack.push(NavTarget.MediaSettings)
+                    }
+
+                    override fun navigateToLocationSettings() {
+                        backstack.push(NavTarget.LocationSettings)
                     }
 
                     override fun navigateToLabs() {
                         backstack.push(NavTarget.Labs)
                     }
 
+                    override fun navigateToAccountSettings() {
+                        backstack.push(NavTarget.AccountSettings)
+                    }
+                }
+                createNode<PreferencesRootNode>(buildContext, plugins = listOf(callback))
+            }
+            NavTarget.AccountSettings -> {
+                val callback = object : PreferencesAccountNode.Callback {
+                    override fun navigateToBugReport() {
+                        callback.navigateToBugReport()
+                    }
+
+                    override fun navigateToSecureBackup() {
+                        callback.navigateToSecureBackup()
+                    }
+
+                    override fun navigateToModerationAndSafety() {
+                        backstack.push(NavTarget.ModerationAndSafety)
+                    }
+
+                    override fun navigateToNotificationSettings() {
+                        backstack.push(NavTarget.NotificationSettings)
+                    }
+
                     override fun navigateToLinkNewDevice() {
-                        callback.navigateToLinkNewDevice()
+                        callback.navigateToAddAccount()
                     }
 
                     override fun navigateToUserProfile(matrixUser: MatrixUser) {
@@ -189,7 +217,7 @@ class PreferencesFlowNode(
                         backstack.push(NavTarget.AccountDeactivation)
                     }
                 }
-                createNode<PreferencesRootNode>(buildContext, plugins = listOf(callback))
+                createNode<PreferencesAccountNode>(buildContext, plugins = listOf(callback))
             }
             NavTarget.DeveloperSettings -> {
                 val developerSettingsCallback = object : DeveloperSettingsNode.Callback {
@@ -218,6 +246,12 @@ class PreferencesFlowNode(
                     }
                 }
                 createNode<LabsNode>(buildContext, listOf(callback))
+            }
+            NavTarget.ModerationAndSafety -> {
+                createNode<ModerationAndSafetyNode>(buildContext)
+            }
+            NavTarget.LocationSettings -> {
+                createNode<LocationSettingsNode>(buildContext)
             }
             NavTarget.About -> {
                 val callback = object : AboutNode.Callback {
@@ -309,8 +343,8 @@ class PreferencesFlowNode(
                 val input = EditDefaultNotificationSettingNode.Inputs(navTarget.isOneToOne)
                 createNode<EditDefaultNotificationSettingNode>(buildContext, plugins = listOf(input, callback))
             }
-            NavTarget.AdvancedSettings -> {
-                createNode<AdvancedSettingsNode>(buildContext)
+            NavTarget.MediaSettings -> {
+                createNode<MediaSettingsNode>(buildContext)
             }
             is NavTarget.UserProfile -> {
                 val inputs = EditUserProfileNode.Inputs(navTarget.matrixUser)

features/joinroom/impl/src/main/kotlin/io/element/android/features/joinroom/impl/JoinRoomView.kt
@@ -40,8 +40,8 @@ import androidx.compose.ui.unit.dp
 import io.element.android.compound.theme.ElementTheme
 import io.element.android.compound.tokens.generated.CompoundIcons
 import io.element.android.features.invite.api.InviteData
+import io.element.android.libraries.designsystem.atomic.atoms.CopiableTextAtom
 import io.element.android.libraries.designsystem.atomic.atoms.PlaceholderAtom
-import io.element.android.libraries.designsystem.atomic.atoms.RoomPreviewAliasAtom
 import io.element.android.libraries.designsystem.atomic.atoms.RoomPreviewDescriptionAtom
 import io.element.android.libraries.designsystem.atomic.atoms.RoomPreviewTitleAtom
 import io.element.android.libraries.designsystem.atomic.molecules.ButtonRowMolecule
@@ -514,7 +514,7 @@ private fun IncompleteContent(
         title = {
             when (roomIdOrAlias) {
                 is RoomIdOrAlias.Alias -> {
-                    RoomPreviewAliasAtom(roomIdOrAlias.identifier)
+                    CopiableTextAtom(roomIdOrAlias.identifier)
                 }
                 is RoomIdOrAlias.Id -> {
                     PlaceholderAtom(width = 200.dp, height = 22.dp)
@@ -567,7 +567,7 @@ private fun DefaultLoadedContent(
         },
         subtitle = {
             if (contentState.alias != null) {
-                RoomPreviewAliasAtom(contentState.alias.value)
+                CopiableTextAtom(contentState.alias.value)
             }
             if (contentState.details is LoadedDetails.Space) {
                 Spacer(Modifier.height(8.dp))

features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/account/PreferencesAccountNode.kt
@@ -1,0 +1,96 @@
+/*
+ * Copyright (c) 2026 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.features.preferences.impl.account
+
+import android.app.Activity
+import androidx.activity.compose.LocalActivity
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.bumble.appyx.core.modality.BuildContext
+import com.bumble.appyx.core.node.Node
+import com.bumble.appyx.core.plugin.Plugin
+import dev.zacsweers.metro.Assisted
+import dev.zacsweers.metro.AssistedInject
+import io.element.android.annotations.ContributesNode
+import io.element.android.compound.theme.ElementTheme
+import io.element.android.features.logout.api.direct.DirectLogoutEvent
+import io.element.android.features.logout.api.direct.DirectLogoutView
+import io.element.android.libraries.androidutils.browser.openUrlInChromeCustomTab
+import io.element.android.libraries.architecture.callback
+import io.element.android.libraries.di.SessionScope
+import io.element.android.libraries.matrix.api.user.MatrixUser
+
+@ContributesNode(SessionScope::class)
+@AssistedInject
+class PreferencesAccountNode(
+    @Assisted buildContext: BuildContext,
+    @Assisted plugins: List<Plugin>,
+    private val presenter: PreferencesAccountPresenter,
+    private val directLogoutView: DirectLogoutView,
+) : Node(buildContext, plugins = plugins) {
+    interface Callback : Plugin {
+        fun navigateToBugReport()
+        fun navigateToSecureBackup()
+        fun navigateToModerationAndSafety()
+        fun navigateToNotificationSettings()
+        fun navigateToLinkNewDevice()
+        fun navigateToUserProfile(matrixUser: MatrixUser)
+        fun navigateToBlockedUsers()
+        fun startSignOutFlow()
+        fun startAccountDeactivationFlow()
+    }
+
+    private val callback: Callback = callback()
+
+    private fun onManageAccountClick(
+        activity: Activity,
+        url: String?,
+        isDark: Boolean,
+    ) {
+        url?.let {
+            activity.openUrlInChromeCustomTab(
+                null,
+                darkTheme = isDark,
+                url = it
+            )
+        }
+    }
+
+    @Composable
+    override fun View(modifier: Modifier) {
+        val state = presenter.present()
+        val activity = requireNotNull(LocalActivity.current)
+        val isDark = ElementTheme.isLightTheme.not()
+        PreferencesAccountView(
+            state = state,
+            modifier = modifier,
+            onBackClick = this::navigateUp,
+            onAvatarClick = {
+                // TODO
+            },
+            onOpenRageShake = callback::navigateToBugReport,
+            onSecureBackupClick = callback::navigateToSecureBackup,
+            onModerationAndSafetyClick = callback::navigateToModerationAndSafety,
+            onLinkNewDeviceClick = callback::navigateToLinkNewDevice,
+            onManageAccountClick = { onManageAccountClick(activity, it, isDark) },
+            onOpenNotificationSettings = callback::navigateToNotificationSettings,
+            onEditProfileClick = callback::navigateToUserProfile,
+            onOpenBlockedUsers = callback::navigateToBlockedUsers,
+            onSignOutClick = {
+                if (state.directLogoutState.canDoDirectSignOut) {
+                    state.directLogoutState.eventSink(DirectLogoutEvent.Logout(ignoreSdkError = false))
+                } else {
+                    callback.startSignOutFlow()
+                }
+            },
+            onDeactivateClick = callback::startAccountDeactivationFlow
+        )
+
+        directLogoutView.Render(state = state.directLogoutState)
+    }
+}

features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/account/PreferencesAccountPresenter.kt
@@ -1,0 +1,111 @@
+/*
+ * Copyright (c) 2026 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.features.preferences.impl.account
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import dev.zacsweers.metro.Inject
+import io.element.android.features.enterprise.api.SessionEnterpriseService
+import io.element.android.features.logout.api.direct.DirectLogoutState
+import io.element.android.features.rageshake.api.RageshakeFeatureAvailability
+import io.element.android.libraries.architecture.Presenter
+import io.element.android.libraries.designsystem.utils.snackbar.SnackbarDispatcher
+import io.element.android.libraries.designsystem.utils.snackbar.collectSnackbarMessageAsState
+import io.element.android.libraries.featureflag.api.FeatureFlagService
+import io.element.android.libraries.featureflag.api.FeatureFlags
+import io.element.android.libraries.indicator.api.IndicatorService
+import io.element.android.libraries.matrix.api.MatrixClient
+import io.element.android.libraries.matrix.api.verification.SessionVerificationService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+@Inject
+class PreferencesAccountPresenter(
+    private val matrixClient: MatrixClient,
+    private val sessionVerificationService: SessionVerificationService,
+    private val snackbarDispatcher: SnackbarDispatcher,
+    private val indicatorService: IndicatorService,
+    private val directLogoutPresenter: Presenter<DirectLogoutState>,
+    private val rageshakeFeatureAvailability: RageshakeFeatureAvailability,
+    private val featureFlagService: FeatureFlagService,
+    private val sessionEnterpriseService: SessionEnterpriseService,
+) : Presenter<PreferencesAccountState> {
+    @Composable
+    override fun present(): PreferencesAccountState {
+        val matrixUser = matrixClient.userProfile.collectAsState()
+        LaunchedEffect(Unit) {
+            // Force a refresh of the profile
+            matrixClient.getUserProfile()
+        }
+        val showLinkNewDevice by remember {
+            featureFlagService.isFeatureEnabledFlow(FeatureFlags.QrCodeLogin)
+        }.collectAsState(initial = false)
+
+        val snackbarMessage by snackbarDispatcher.collectSnackbarMessageAsState()
+
+        // We should display the 'complete verification' option if the current session can be verified
+        val canVerifyUserSession by sessionVerificationService.needsSessionVerification.collectAsState(false)
+
+        val showSecureBackupIndicator by indicatorService.showSettingChatBackupIndicator()
+
+        val accountManagementUrl: MutableState<String?> = remember {
+            mutableStateOf(null)
+        }
+        var canDeactivateAccount by remember {
+            mutableStateOf(false)
+        }
+        val canReportBug by remember { rageshakeFeatureAvailability.isAvailable() }.collectAsState(false)
+        LaunchedEffect(Unit) {
+            canDeactivateAccount = matrixClient.canDeactivateAccount()
+        }
+
+        val nbOfBlockedUsers by produceState(initialValue = 0) {
+            matrixClient.ignoredUsersFlow
+                .onEach { value = it.size }
+                .launchIn(this)
+        }
+
+        val directLogoutState = directLogoutPresenter.present()
+
+        LaunchedEffect(Unit) {
+            initAccountManagementUrl(accountManagementUrl)
+        }
+
+        return PreferencesAccountState(
+            myUser = matrixUser.value,
+            showSecureBackup = !canVerifyUserSession,
+            showSecureBackupBadge = showSecureBackupIndicator,
+            accountManagementUrl = accountManagementUrl.value,
+            canReportBug = canReportBug,
+            showLinkNewDevice = showLinkNewDevice,
+            canDeactivateAccount = canDeactivateAccount,
+            nbOfBlockedUsers = nbOfBlockedUsers,
+            directLogoutState = directLogoutState,
+            snackbarMessage = snackbarMessage,
+        )
+    }
+
+    private fun CoroutineScope.initAccountManagementUrl(
+        accountManagementUrl: MutableState<String?>,
+    ) = launch {
+        accountManagementUrl.value = matrixClient.getAccountManagementUrl(null)
+            .getOrNull()
+            ?.let {
+                sessionEnterpriseService.tweakMasUrl(it)
+            }
+    }
+}

features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/account/PreferencesAccountState.kt
@@ -1,0 +1,27 @@
+/*
+ * Copyright (c) 2026 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.features.preferences.impl.account
+
+import io.element.android.features.logout.api.direct.DirectLogoutState
+import io.element.android.libraries.designsystem.utils.snackbar.SnackbarMessage
+import io.element.android.libraries.matrix.api.user.MatrixUser
+
+data class PreferencesAccountState(
+    val myUser: MatrixUser,
+    val showSecureBackup: Boolean,
+    val showSecureBackupBadge: Boolean,
+    val accountManagementUrl: String?,
+    val canReportBug: Boolean,
+    val showLinkNewDevice: Boolean,
+    val canDeactivateAccount: Boolean,
+    val nbOfBlockedUsers: Int,
+    val directLogoutState: DirectLogoutState,
+    val snackbarMessage: SnackbarMessage?,
+) {
+    val showBlockedUsersItem = nbOfBlockedUsers > 0
+}

features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/account/PreferencesAccountStatePreviewParam.kt
@@ -1,0 +1,75 @@
+/*
+ * Copyright (c) 2026 Element Creations Ltd.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial.
+ * Please see LICENSE files in the repository root for full details.
+ */
+
+package io.element.android.features.preferences.impl.account
+
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import io.element.android.features.logout.api.direct.DirectLogoutState
+import io.element.android.features.logout.api.direct.aDirectLogoutState
+import io.element.android.libraries.designsystem.utils.snackbar.SnackbarMessage
+import io.element.android.libraries.matrix.api.user.MatrixUser
+import io.element.android.libraries.matrix.ui.components.aMatrixUser
+import io.element.android.libraries.ui.strings.CommonStrings
+
+open class PreferencesAccountStatePreviewParam : PreviewParameterProvider<PreferencesAccountState> {
+    override val values: Sequence<PreferencesAccountState>
+        get() = sequenceOf(
+            // Nominal state, that a regular user will see if multi account is enabled
+            aPreferencesAccountState(
+                myUser = aMatrixUser(avatarUrl = "anAvatarUrl"),
+                showSecureBackup = true,
+                accountManagementUrl = "aUrl",
+                canReportBug = true,
+                showLinkNewDevice = true,
+                canDeactivateAccount = false,
+                nbOfBlockedUsers = 3,
+            ),
+            aPreferencesAccountState(
+                myUser = aMatrixUser(displayName = null),
+                showSecureBackup = true,
+                canDeactivateAccount = true,
+            ),
+            aPreferencesAccountState(
+                accountManagementUrl = "aUrl",
+                showSecureBackup = true,
+                showSecureBackupBadge = true,
+            ),
+            aPreferencesAccountState(
+                canReportBug = true,
+                nbOfBlockedUsers = 3,
+                snackbarMessage = SnackbarMessage(CommonStrings.common_verification_complete),
+            ),
+            aPreferencesAccountState(
+                showLinkNewDevice = true,
+                canDeactivateAccount = true,
+            ),
+        )
+}
+
+fun aPreferencesAccountState(
+    myUser: MatrixUser = aMatrixUser(),
+    showSecureBackup: Boolean = false,
+    showSecureBackupBadge: Boolean = false,
+    accountManagementUrl: String? = null,
+    canReportBug: Boolean = false,
+    showLinkNewDevice: Boolean = false,
+    canDeactivateAccount: Boolean = false,
+    nbOfBlockedUsers: Int = 0,
+    directLogoutState: DirectLogoutState = aDirectLogoutState(),
+    snackbarMessage: SnackbarMessage? = null,
+) = PreferencesAccountState(
+    myUser = myUser,
+    showSecureBackup = showSecureBackup,
+    showSecureBackupBadge = showSecureBackupBadge,
+    accountManagementUrl = accountManagementUrl,
+    canReportBug = canReportBug,
+    showLinkNewDevice = showLinkNewDevice,
+    canDeactivateAccount = canDeactivateAccount,
+    nbOfBlockedUsers = nbOfBlockedUsers,
+    directLogoutState = directLogoutState,
+    snackbarMessage = snackbarMessage,
+)

commit 83a01eb88043784cf83694913501858f6e81b0bf

Send "Link new device" to the link-new-device flow again
PreferencesFlowNode routed navigateToLinkNewDevice() to
callback.navigateToAddAccount(), so tapping "Link new device" under
Account settings opened the "Add an account" screen instead — the
opposite direction, signing an extra account into this device rather
than authorising a new one.

The correct destination was already implemented and simply unreachable:
LoggedInFlowNode handles navigateToLinkNewDevice() by pushing
NavTarget.LinkNewDevice. Call that instead.

Regression from 9b2577f1dd ("Split settings: app settings / account
settings"); the previous code called callback.navigateToLinkNewDevice().
It went unnoticed because FeatureFlags.QrCodeLogin defaults to false,
which hides the entry point on default builds.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_017y1etzTCPzowrNcPyRBdjm

features/preferences/impl/src/main/kotlin/io/element/android/features/preferences/impl/PreferencesFlowNode.kt
@@ -186,7 +186,7 @@ class PreferencesFlowNode(
                     }
 
                     override fun navigateToLinkNewDevice() {
-                        callback.navigateToAddAccount()
+                        callback.navigateToLinkNewDevice()
                     }
 
                     override fun navigateToUserProfile(matrixUser: MatrixUser) {